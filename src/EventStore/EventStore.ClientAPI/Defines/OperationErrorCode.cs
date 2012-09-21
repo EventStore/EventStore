03e56f508848117eb91e51dfc7dcea302543636b
Greg Young <gregoryyoung1@gmail.com>
2012-09-21 02:25:41 -0700

    server defines. rename "commands" to taskwrappers

--- src/EventStore/EventStore.ClientAPI/Defines/OperationErrorCode.cs
+++ src/EventStore/EventStore.ClientAPI/Defines/OperationErrorCode.cs
@@ -25,7 +25,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //  
-namespace EventStore.ClientAPI.Messages
+namespace EventStore.ClientAPI.Defines
 {
     public enum OperationErrorCode
     {
