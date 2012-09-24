e55cc15345a594d6e8c7ccd1ffd9a6841dae0a06
mr.matsekh@gmail.com <mr.matsekh@gmail.com>
2012-09-24 18:26:01 +0300

    made timeouts public for other projects

--- src/EventStore/EventStore.Core/Services/RequestManager/Managers/Timeouts.cs
+++ src/EventStore/EventStore.Core/Services/RequestManager/Managers/Timeouts.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace EventStore.Core.Services.RequestManager.Managers
 {
-    static class Timeouts
+    public static class Timeouts
     {
         public static readonly TimeSpan PrepareTimeout = TimeSpan.FromMilliseconds(2000);
         public static readonly TimeSpan CommitTimeout = TimeSpan.FromMilliseconds(2000);
