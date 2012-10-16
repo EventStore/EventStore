f351738691a75e64053c245657391457a60a6792
Andrii Nakryiko <andrii.nakryiko@gmail.com>
2012-10-16 13:26:15 +0300

    Fixed unit tests on TFChunk to account for TFChunk.CurrentChunkVersion.

--- src/EventStore/EventStore.Core.Tests/TransactionLog/when_writing_an_existing_chunked_transaction_file_with_checksum.cs
+++ src/EventStore/EventStore.Core.Tests/TransactionLog/when_writing_an_existing_chunked_transaction_file_with_checksum.cs
@@ -46,7 +46,7 @@ namespace EventStore.Core.Tests.TransactionLog
         public void a_record_can_be_written()
         {
             var filename = Path.Combine(PathName, "prefix.tf0");
-            var chunkHeader = new ChunkHeader(1, 10000, 0, 0, 0);
+            var chunkHeader = new ChunkHeader(TFChunk.CurrentChunkVersion, 10000, 0, 0, 0);
             var chunkBytes = chunkHeader.AsByteArray();
             var bytes = new byte[ChunkHeader.Size + 10000 + ChunkFooter.Size];
             Buffer.BlockCopy(chunkBytes, 0, bytes, 0, chunkBytes.Length);
