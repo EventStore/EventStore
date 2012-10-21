94b7776c8287b7a166f0b1e86a0272b51df575c7
Yuriy Solodkyy <yuriy@couldbedone.com>
2012-10-21 14:18:32 +0300

    Add multi-stream tags support in CheckpointTag and tests

---
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/checkpoint_tag/checkpoint_tag_by_stream_positions.cs
@@ -0,0 +1,120 @@
+// Copyright (c) 2012, Event Store LLP
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+// Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// Neither the name of the Event Store LLP nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+using System;
+using System.Collections.Generic;
+using EventStore.Projections.Core.Services.Processing;
+using NUnit.Framework;
+
+#pragma warning disable 1718 // allow a == a comparison
+
+namespace EventStore.Projections.Core.Tests.Services.checkpoint_tag
+{
+    [TestFixture]
+    public class checkpoint_tag_by_stream_positions
+    {
+        private readonly CheckpointTag _a1 = CheckpointTag.FromStreamPositions(
+            new Dictionary<string, int> {{"a", 1}}, 50);
+
+        private readonly CheckpointTag _b1 = CheckpointTag.FromStreamPositions(
+            new Dictionary<string, int> {{"b", 1}}, 100);
+
+        private readonly CheckpointTag _a1b1 =
+            CheckpointTag.FromStreamPositions(new Dictionary<string, int> {{"a", 1}, {"b", 1}}, 100);
+
+        private readonly CheckpointTag _a2b1 =
+            CheckpointTag.FromStreamPositions(new Dictionary<string, int> {{"a", 2}, {"b", 1}}, 110);
+
+        private readonly CheckpointTag _a1b2 =
+            CheckpointTag.FromStreamPositions(new Dictionary<string, int> {{"a", 1}, {"b", 2}}, 120);
+
+        private readonly CheckpointTag _a2b2 =
+            CheckpointTag.FromStreamPositions(new Dictionary<string, int> {{"a", 2}, {"b", 2}}, 120);
+
+        [Test]
+        public void equal_equals()
+        {
+            Assert.IsTrue(_a1.Equals(_a1));
+        }
+
+        [Test]
+        public void equal_operator()
+        {
+            Assert.IsTrue(_a2b1 == _a2b1);
+        }
+
+        [Test]
+        public void less_operator()
+        {
+            Assert.IsTrue(_a1 < _a1b1);
+            Assert.IsTrue(_a1 < _a1b2);
+            Assert.IsTrue(_a1 < _a2b2);
+            Assert.IsFalse(_a1b2 < _a1b2);
+            Assert.IsFalse(_a1b2 < _a1b1);
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1b2 < _a2b1; });
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1 < _b1; });
+        }
+
+        [Test]
+        public void less_or_equal_operator()
+        {
+            Assert.IsTrue(_a1 <= _a1b1);
+            Assert.IsTrue(_a1 <= _a1b2);
+            Assert.IsTrue(_a1 <= _a2b2);
+            Assert.IsTrue(_a1b2 <= _a1b2);
+            Assert.IsFalse(_a1b2 <= _a1b1);
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1b2 <= _a2b1; });
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1 <= _b1; });
+        }
+
+        [Test]
+        public void greater_operator()
+        {
+            Assert.IsFalse(_a1 > _a1b1);
+            Assert.IsFalse(_a1 > _a1b2);
+            Assert.IsFalse(_a1 > _a2b2);
+            Assert.IsFalse(_a1b2 > _a1b2);
+            Assert.IsTrue(_a1b2 > _a1b1);
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1b2 > _a2b1; });
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1 > _b1; });
+        }
+
+        [Test]
+        public void greater_or_equal_operator()
+        {
+            Assert.IsFalse(_a1 >= _a1b1);
+            Assert.IsFalse(_a1 >= _a1b2);
+            Assert.IsFalse(_a1 >= _a2b2);
+            Assert.IsTrue(_a1b2 >= _a1b2);
+            Assert.IsTrue(_a1b2 >= _a1b1);
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1b2 >= _a2b1; });
+            Assert.Throws<InvalidOperationException>(() => { var r = _a1 >= _b1; });
+        }
+    }
+#pragma warning restore 1718
+}
