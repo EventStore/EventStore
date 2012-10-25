0118e718c260dd5104d917518d30cd563ab533d2
Yuriy Solodkyy <yuriy@couldbedone.com>
2012-10-25 19:10:19 +0300

    Tests: CheckpointTag.UpdateStreamPositionUpdateStreamPosition

--- src/EventStore/EventStore.Projections.Core/Services/Processing/MultiStreamReaderEventDistributionPoint.cs
+++ src/EventStore/EventStore.Projections.Core/Services/Processing/MultiStreamReaderEventDistributionPoint.cs
@@ -156,12 +156,10 @@ namespace EventStore.Projections.Core.Services.Processing
             if (_pauseRequested || _paused)
                 throw new InvalidOperationException("Paused or pause requested");
 
-            var requested = false;
             foreach (var stream in _streams)
             {
                 if (_eventsRequested.Contains(stream))
                     continue;
-                requested = true;
                 _eventsRequested.Add(stream);
 
                 var readEventsForward = new ClientMessage.ReadStreamEventsForward(
