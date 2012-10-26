fc349a3dcdef7d1c7ad3b76bb468e6a6732cbc7f
Yuriy Solodkyy <yuriy@couldbedone.com>
2012-10-26 15:58:30 +0300

    Event ordered by prepare position when projection is catching-up with multi-stream-reader event distribution point

--- src/EventStore/EventStore.Projections.Core.Tests/Services/multi_stream_event_distribution_point/when_handling_read_completed_and_no_stream.cs
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/multi_stream_event_distribution_point/when_handling_read_completed_and_no_stream.cs
@@ -6,6 +6,7 @@ using EventStore.Core.Messages;
 using EventStore.Core.Services.Storage.ReaderIndex;
 using EventStore.Core.Services.TimerService;
 using EventStore.Core.TransactionLog.LogRecords;
+using EventStore.Projections.Core.Messages;
 using EventStore.Projections.Core.Services.Processing;
 using EventStore.Projections.Core.Tests.Services.core_projection;
 using NUnit.Framework;
@@ -92,10 +93,60 @@ namespace EventStore.Projections.Core.Tests.Services.multi_stream_event_distribu
         }
 
         [Test]
+        public void publishes_correct_committed_event_received_messages()
+        {
+            Assert.AreEqual(
+                3, _consumer.HandledMessages.OfType<ProjectionMessage.Projections.CommittedEventDistributed>().Count());
+            var first = _consumer.HandledMessages.OfType<ProjectionMessage.Projections.CommittedEventDistributed>().First();
+            var second =
+                _consumer.HandledMessages.OfType<ProjectionMessage.Projections.CommittedEventDistributed>().Skip(1).First();
+            var third =
+                _consumer.HandledMessages.OfType<ProjectionMessage.Projections.CommittedEventDistributed>().Skip(2).First();
+
+            Assert.IsNull(third.Data);
+            Assert.AreEqual(100, third.SafeTransactionFileReaderJoinPosition);
+
+            Assert.AreEqual("event_type1", first.Data.EventType);
+            Assert.AreEqual("event_type2", second.Data.EventType);
+            Assert.AreEqual(_firstEventId, first.Data.EventId);
+            Assert.AreEqual(_secondEventId, second.Data.EventId);
+            Assert.AreEqual(1, first.Data.Data[0]);
+            Assert.AreEqual(2, first.Data.Metadata[0]);
+            Assert.AreEqual(3, second.Data.Data[0]);
+            Assert.AreEqual(4, second.Data.Metadata[0]);
+            Assert.AreEqual("a", first.EventStreamId);
+            Assert.AreEqual("a", second.EventStreamId);
+            Assert.AreEqual(0, first.Position.PreparePosition);
+            Assert.AreEqual(0, second.Position.PreparePosition);
+            Assert.AreEqual(0, first.Position.CommitPosition);
+            Assert.AreEqual(0, second.Position.CommitPosition);
+            Assert.AreEqual(50, first.SafeTransactionFileReaderJoinPosition);
+            Assert.AreEqual(100, second.SafeTransactionFileReaderJoinPosition);
+        }
+
+
+        [Test]
         public void publishes_schedule()
         {
             Assert.AreEqual(1, _consumer.HandledMessages.OfType<TimerMessage.Schedule>().Count());
         }
+
+        [Test]
+        public void can_handle_following_read_events_completed()
+        {
+            _edp.Handle(
+                new ClientMessage.ReadStreamEventsForwardCompleted(
+                    _distibutionPointCorrelationId, "a",
+                    new[]
+                    {
+                        new EventLinkPair( 
+                            new EventRecord(
+                                3, 250, Guid.NewGuid(), Guid.NewGuid(), 250, 0, "a", ExpectedVersion.Any, DateTime.UtcNow,
+                                PrepareFlags.SingleWrite | PrepareFlags.TransactionBegin | PrepareFlags.TransactionEnd,
+                                "event_type", new byte[0], new byte[0]), null)
+                    }, RangeReadResult.Success, 11, 300, 4));
+        }
+
 
     }
 }
