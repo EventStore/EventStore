da1880c5454e91aced94228faff8aeac9955295d
Yuriy Solodkyy <yuriy@couldbedone.com>
2012-10-30 22:54:43 +0200

    Tests: for options ({ stateStreamName: "stream-name"});

---
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/core_projection/when_the_state_handler_with_configured_state_stream_does_process_an_event_the_projection_should.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text;
+using EventStore.Core.Data;
+using EventStore.Projections.Core.Messages;
+using NUnit.Framework;
+
+namespace EventStore.Projections.Core.Tests.Services.core_projection
+{
+    [TestFixture]
+    public class when_the_state_handler_with_configured_state_stream_does_process_an_event_the_projection_should : TestFixtureWithCoreProjection
+    {
+        protected override void Given()
+        {
+            _configureBuilderByQuerySource = source =>
+            {
+                source.FromAll();
+                source.AllEvents();
+                source.SetStateStreamNameOption("state-stream");
+            };
+            NoStream("state-stream");
+            NoStream("$projections-projection-checkpoint");
+        }
+
+        protected override void When()
+        {
+            //projection subscribes here
+            _coreProjection.Handle(
+                ProjectionMessage.Projections.CommittedEventReceived.Sample(Guid.Empty, new EventPosition(120, 110), "/event_category/1", -1, false,
+                                                                            new Event(
+                                                                                Guid.NewGuid(), "handle_this_type", false, Encoding.UTF8.GetBytes("data"),
+                                                                                Encoding.UTF8.GetBytes("metadata"))));
+        }
+
+        [Test]
+        public void write_the_new_state_snapshot()
+        {
+            Assert.AreEqual(1, _writeEventHandler.HandledMessages.Count);
+
+            var message = _writeEventHandler.HandledMessages[0];
+            var data = Encoding.UTF8.GetString(message.Events[0].Data);
+            Assert.AreEqual("data", data);
+            Assert.AreEqual("state-stream", message.EventStreamId);
+
+        }
+
+        [Test]
+        public void emit_a_state_updated_event()
+        {
+            Assert.AreEqual(1, _writeEventHandler.HandledMessages.Count);
+
+            var @event = _writeEventHandler.HandledMessages[0].Events[0];
+            Assert.AreEqual("StateUpdated", @event.EventType);
+        }
+    }
+}
