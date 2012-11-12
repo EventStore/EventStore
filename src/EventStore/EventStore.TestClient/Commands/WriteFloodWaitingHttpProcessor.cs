cc0b02486d29f411193305c5ebb12b123cf7358d
Taras Roshko <taras.roshko@gmail.com>
2012-11-12 17:53:23 +0200

    fixed loadfromxml in autoeventconverter, switched back to xml in testclient

--- src/EventStore/EventStore.TestClient/Commands/WriteFloodWaitingHttpProcessor.cs
+++ src/EventStore/EventStore.TestClient/Commands/WriteFloodWaitingHttpProcessor.cs
@@ -124,10 +124,10 @@ namespace EventStore.TestClient.Commands
                                                                "DATA" + new string('*', 256),
                                                                "METADATA" + new string('$', 100))
                             });
-                        var request = Codec.Json.To(write);
+                        var request = Codec.Xml.To(write);
                         client.Post(url, 
                                     request, 
-                                    Codec.Json.ContentType,
+                                    Codec.Xml.ContentType,
                                     succHandler, 
                                     exc => 
                                     {
