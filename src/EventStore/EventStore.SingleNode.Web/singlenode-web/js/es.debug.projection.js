18428d3fa3a98d9f6d33a958549414749ea42a26
Yuriy Solodkyy <yuriy@couldbedone.com>
2012-11-16 15:39:39 +0200

    Changed: browser based projection debugging experience improvement

---
+++ src/EventStore/EventStore.SingleNode.Web/singlenode-web/js/es.debug.projection.js
@@ -0,0 +1,138 @@
+
+(function () {
+
+    var projectionStatusUrl = "";
+
+    var sourceLoaded = false;
+    var eventsLoaded = false;
+    var eventsRaw = null;
+
+    $(function () {
+
+        $.templates("editSourceTemplate", "#editSourceTemplate");
+        projectionStatusUrl = location.hash.substr(1);
+
+        $('#update-button').click(function (ev) {
+            $.ajax(projectionStatusUrl + "/query", {
+                headers: {
+                    Accept: "application/json",
+                },
+
+                type: "PUT",
+                data: $("#source").val(),
+                success: successUpdateSource,
+                error: function (xhr) {
+                    var msg = es.util.formatError("Couldn't delete projection", xhr);
+                    alert(msg);
+                }
+            });
+        });
+
+        requestProjectionDebugState();
+        requestProjectionSource();
+    });
+
+    function requestProjectionDebugState() {
+        $.ajax(projectionStatusUrl + "/debug", {
+            headers: {
+                Accept: "application/json",
+            },
+            dataType: "text",
+            success: successDebugState,
+            error: errorDebugState
+        });
+
+        function successDebugState(data, status, xhr) {
+            $("#projection-debug-events").text(data);
+            eventsRaw = data;
+            eventsLoaded = true;
+            checkLoaded();
+        }
+
+        function errorDebugState(xhr, status) {
+            setTimeout(requestProjectionDebugState, 1000);
+        }
+    }
+
+    function requestProjectionSource() {
+        $.ajax(projectionStatusUrl + "/query", {
+            headers: {
+                Accept: "text/plain",
+            },
+            dataType: "text",
+            success: successSource,
+            error: errorSource
+        });
+
+        function successSource(data, status, xhr) {
+            $("#source").text(data);
+            sourceLoaded = true;
+            checkLoaded();
+        }
+
+        function errorSource(xhr, status) {
+            setTimeout(requestProjectionSource, 1000);
+        }
+    }
+
+    function successUpdateSource(data, status, xhr) {
+        window.location = "view-projection.htm#" + projectionStatusUrl;
+    }
+
+    function checkLoaded() {
+        if (sourceLoaded && eventsLoaded) {
+            readyForDebugging();
+        }
+    }
+
+    function readyForDebugging() {
+        var first = JSON.parse(eventsRaw)[0];
+
+        document.getElementById('script-placeholder').contentDocument.write(
+            '<div id="text"></div>' +
+                '<script src="/web/es/js/projections/v8/Prelude/Modules.js"><' + '/script>' +
+                '<script src="/web/es/js/projections/v8/Prelude/Projections.js"><' + '/script>' +
+                '<script src="/web/es/js/projections/es.projections.environment.js"><' + '/script>' +
+                '<script src="/web/es/js/projections/v8/Prelude/1Prelude.js"><' + '/script>' +
+                '<script src="/web/es/js/projections/es.projection.js"><' + '/script>' +
+                '<script src="/web/es/js/projections/es.api.js"><' + '/script>' +
+                '<script>window.processor = $initialize_hosted_projections(); processor.set_debugging();<' + '/script>' +
+                '<script src="' + projectionStatusUrl + '/query?1"><' + '/' + 'script>');
+
+
+        $.ajax(projectionStatusUrl + "/state?partition=" + first.Partition, {
+            headers: {
+                Accept: "application/json",
+            },
+            dataType: "text",
+            success: successPartitionState,
+            error: errorPartitionState
+        });
+
+        function successPartitionState(data, status, xhr) {
+            $("#projection-debug-state").text(data);
+            $('#run-button').removeAttr("disabled");
+
+            $('#run-button').click(function () {
+
+                document.getElementById('script-placeholder').contentWindow.processor.set_state(data);
+                document.getElementById('script-placeholder').contentWindow.processor.process_event(
+                    first.BodyRaw,
+                    first.StreamId,
+                    first.EventType,
+                    first.Category,
+                    first.SequenceNumber,
+                    first.MetadataRaw,
+                    first.LogPposition);
+            });
+
+        }
+
+        function errorPartitionState(xhr, status) {
+            alert("Failed to get state for partition: " + first.Partition);
+        }
+
+    }
+
+})();
+
