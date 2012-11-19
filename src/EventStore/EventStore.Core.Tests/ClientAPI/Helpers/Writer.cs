c6284267d16929c084a92ee03a952602c45ed3d2
Taras Roshko <taras.roshko@gmail.com>
2012-11-19 15:47:12 +0200

    tests for idempotency in transactions (both explicit and implicit stream creates)

--- src/EventStore/EventStore.Core.Tests/ClientAPI/Helpers/Writer.cs
+++ src/EventStore/EventStore.Core.Tests/ClientAPI/Helpers/Writer.cs
@@ -58,4 +58,44 @@ namespace EventStore.Core.Tests.ClientAPI.Helpers
             return this;
         }
     }
+
+    internal class TransactionalWriter
+    {
+        private readonly EventStoreConnection _store;
+        private readonly string _stream;
+
+        public TransactionalWriter(EventStoreConnection store, string stream)
+        {
+            _store = store;
+            _stream = stream;
+        }
+
+        public OngoingTransaction StartTransaction(int expectedVersion)
+        {
+            return new OngoingTransaction(_store, _store.StartTransaction(_stream, expectedVersion));
+        }
+    }
+
+    internal class OngoingTransaction
+    {
+        private readonly EventStoreConnection _store;
+        private readonly EventStoreTransaction _transaction;
+
+        public OngoingTransaction(EventStoreConnection store, EventStoreTransaction transaction)
+        {
+            _store = store;
+            _transaction = transaction;
+        }
+
+        public OngoingTransaction Write(params TestEvent[] events)
+        {
+            _store.TransactionalWrite(_transaction.TransactionId, _transaction.Stream, events);
+            return this;
+        }
+
+        public void Commit()
+        {
+            _store.CommitTransaction(_transaction.TransactionId, _transaction.Stream);
+        }
+    }
 }
