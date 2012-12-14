c2d9e1042e56b2a32d051fd0a85c14bcee70d1a4
Kostyantyn Pyatkivskyy <k.pyatkivskyy@gmail.com>
2012-12-14 02:26:39 +0200

    Add error function if failed.

--- src/EventStore/Scripts/get-mono-301p.sh
+++ src/EventStore/Scripts/get-mono-301p.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+function err() {
+  echo FAIL!
+  exit 1
+}
+
 curdir=`dirname $0`
 
 org="$curdir/mono/0001-ES-patch.patch"
 resolved=$(readlink -f $org)
 export patchtoapply="$resolved"
 
-$curdir/get-mono "mono-3.0.1"
+$curdir/get-mono "mono-3.0.1" || err
