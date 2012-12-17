b9d57e7f550355b5e5ff34b650cfea98602ee38f
Kostyantyn Pyatkivskyy <k.pyatkivskyy@gmail.com>
2012-12-17 17:41:44 +0200

    [tests] Ignore can_pass_though_millions_of_events test

--- src/EventStore/EventStore.Projections.Core.Tests/Services/projections_manager/v8/when_running_emitting_v8_projection.cs
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/projections_manager/v8/when_running_emitting_v8_projection.cs
@@ -75,7 +75,7 @@ namespace EventStore.Projections.Core.Tests.Services.projections_manager.v8
             Assert.AreEqual(@"{""a"":""b""}", Encoding.UTF8.GetString(emittedEvents[0].Data));
         }
 
-        [Test, Explicit, Category("LongRunning"), Category("v8")]
+        [Test, Explicit, Category("LongRunning"), Category("v8"), Ignore]
         public void can_pass_though_millions_of_events()
         {
             for (var i = 0; i < 100000000; i++)
