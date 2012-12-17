7308d278dcc0d12b5bb46a723159b5b70717b3e4
Kostyantyn Pyatkivskyy <k.pyatkivskyy@gmail.com>
2012-12-17 18:16:05 +0200

    [tests] Ignore Queue speed tests

--- src/EventStore/EventStore.Core.Tests/Bus/QueueSpeedTest.cs
+++ src/EventStore/EventStore.Core.Tests/Bus/QueueSpeedTest.cs
@@ -12,7 +12,7 @@ using NUnit.Framework;
 
 namespace EventStore.Core.Tests.Bus
 {
-    [TestFixture]
+    [TestFixture, Ignore]
     public class QueueSpeedTest
     {
         [Test, Category("LongRunning"), Explicit]
