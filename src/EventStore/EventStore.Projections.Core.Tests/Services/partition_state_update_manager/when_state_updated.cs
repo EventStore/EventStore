24b2fd8cdf981c0fe1f8e269c0b4eddff0ea9097
Yuriy Solodkyy <yuriy@couldbedone.com>
2012-12-21 13:11:54 +0200

    Added: PartitionStateUpdateManager.cs

---
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/partition_state_update_manager/when_state_updated.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) 2012, Event Store LLP
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+// Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// Neither the name of the Event Store LLP nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+using System.Text;
+using EventStore.Projections.Core.Services.Processing;
+using NUnit.Framework;
+
+namespace EventStore.Projections.Core.Tests.Services.partition_state_update_manager
+{
+    [TestFixture]
+    public class when_state_updated
+    {
+        private PartitionStateUpdateManager _updateManager;
+        private CheckpointTag _zero = CheckpointTag.FromPosition(100, 50);
+        private CheckpointTag _one = CheckpointTag.FromPosition(200, 150);
+        private CheckpointTag _two = CheckpointTag.FromPosition(300, 250);
+
+        [SetUp]
+        public void setup()
+        {
+            _updateManager = new PartitionStateUpdateManager(new ProjectionNamesBuilder("projection"));
+            _updateManager.StateUpdated(
+                "partition", "state", _zero, _one);
+        }
+
+        [Test]
+        public void handles_state_updated_for_the_same_partition()
+        {
+            _updateManager.StateUpdated(
+                "partition", "state", _one, _two);
+        }
+
+        [Test]
+        public void handles_state_updated_for_another_partition()
+        {
+            _updateManager.StateUpdated(
+                "partition", "state", _one, _two);
+        }
+
+        [Test]
+        public void emit_events_writes_single_state_updated_event()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            Assert.AreEqual(1, eventWriter.Writes.Count);
+            Assert.AreEqual(1, eventWriter.Writes[0].Length);
+        }
+
+        [Test]
+        public void emit_events_writes_correct_state_data()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            EmittedEvent @event = eventWriter.Writes[0][0];
+            Assert.AreEqual("state", Encoding.UTF8.GetString(@event.Data));
+        }
+
+        [Test]
+        public void emit_events_writes_event_with_correct_caused_by_tag()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            EmittedEvent @event = eventWriter.Writes[0][0];
+            Assert.AreEqual(_one, @event.CausedByTag);
+        }
+
+        [Test]
+        public void emit_events_writes_event_with_correct_expected_tag()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            EmittedEvent @event = eventWriter.Writes[0][0];
+            Assert.AreEqual(_zero, @event.ExpectedTag);
+        }
+
+    }
+}
---
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/partition_state_update_manager/when_two_states_were_updated.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+using System.Text;
+using EventStore.Projections.Core.Services.Processing;
+using NUnit.Framework;
+
+namespace EventStore.Projections.Core.Tests.Services.partition_state_update_manager
+{
+    [TestFixture]
+    public class when_two_states_were_updated
+    {
+        private PartitionStateUpdateManager _updateManager;
+        private CheckpointTag _zero = CheckpointTag.FromPosition(100, 50);
+        private CheckpointTag _one = CheckpointTag.FromPosition(200, 150);
+        private CheckpointTag _two = CheckpointTag.FromPosition(300, 250);
+        private CheckpointTag _three = CheckpointTag.FromPosition(400, 350);
+
+        [SetUp]
+        public void setup()
+        {
+            _updateManager = new PartitionStateUpdateManager(new ProjectionNamesBuilder("projection"));
+            _updateManager.StateUpdated("partition1", "state1", _zero, _one);
+            _updateManager.StateUpdated("partition2", "state2", _zero, _two);
+        }
+
+        [Test]
+        public void handles_state_updated_for_the_same_partition()
+        {
+            _updateManager.StateUpdated("partition1", "state", _two, _three);
+        }
+
+        [Test]
+        public void handles_state_updated_for_another_partition()
+        {
+            _updateManager.StateUpdated("partition3", "state", _two, _three);
+        }
+
+        [Test]
+        public void emit_events_writes_both_state_updated_event()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            Assert.AreEqual(2, (eventWriter.Writes.SelectMany(write => write)).Count());
+        }
+
+        [Test]
+        public void emit_events_writes_to_correct_streams()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            var events = eventWriter.Writes.SelectMany(write => write).ToArray();
+            Assert.IsTrue(events.Any((v => "$projections-projection-partition1-state" == v.StreamId)));
+            Assert.IsTrue(events.Any((v => "$projections-projection-partition2-state" == v.StreamId)));
+        }
+
+        [Test]
+        public void emit_events_writes_correct_state_data()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            var events = eventWriter.Writes.SelectMany(write => write).ToArray();
+            var event1 = events.Single(v => "$projections-projection-partition1-state" == v.StreamId);
+            var event2 = events.Single(v => "$projections-projection-partition2-state" == v.StreamId);
+
+            Assert.AreEqual("state1", Encoding.UTF8.GetString(event1.Data));
+            Assert.AreEqual("state2", Encoding.UTF8.GetString(event2.Data));
+        }
+
+        [Test]
+        public void emit_events_writes_event_with_correct_caused_by_tag()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            var events = eventWriter.Writes.SelectMany(write => write).ToArray();
+            var event1 = events.Single(v => "$projections-projection-partition1-state" == v.StreamId);
+            var event2 = events.Single(v => "$projections-projection-partition2-state" == v.StreamId);
+            Assert.AreEqual(_one, event1.CausedByTag);
+            Assert.AreEqual(_two, event2.CausedByTag);
+        }
+
+        [Test]
+        public void emit_events_writes_event_with_correct_expected_tag()
+        {
+            var eventWriter = new FakeEventWriter();
+            _updateManager.EmitEvents(eventWriter);
+            var events = eventWriter.Writes.SelectMany(write => write).ToArray();
+            var event1 = events.Single(v => "$projections-projection-partition1-state" == v.StreamId);
+            var event2 = events.Single(v => "$projections-projection-partition2-state" == v.StreamId);
+            Assert.AreEqual(_zero, event1.ExpectedTag);
+            Assert.AreEqual(_zero, event2.ExpectedTag);
+        }
+    }
+}
