98939cf5651068ce9642fb1188b5396c528bcb02
Kostyantyn Pyatkivskyy <k.pyatkivskyy@gmail.com>
2013-01-17 16:54:41 +0200

    Hashtag update in Version.cs

--- src/EventStore/EventStore.Common/Version/Version.cs
+++ src/EventStore/EventStore.Common/Version/Version.cs
@@ -1,9 +1,9 @@
-﻿namespace EventStore.Common.Version
+namespace EventStore.Common.Version
 {
     public static class EventStoreVersion
     {
         public const string Version = "1.0.0";
-        public const string Hashtag = "1f777721607ad73b1c2f8bbde97821b158c770ad";
+        public const string Hashtag = "06262f4c06d380e9a446002ec88f46c90a928403";
         public const string Branch = "master";
     }
 }
