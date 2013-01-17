286c0a3920bf0a4609d52b0aaa00cf64e5a05bb3
Kostyantyn Pyatkivskyy <k.pyatkivskyy@gmail.com>
2013-01-17 17:15:34 +0200

    Update hash.

--- src/EventStore/EventStore.Common/Version/Version.cs
+++ src/EventStore/EventStore.Common/Version/Version.cs
@@ -3,7 +3,7 @@ namespace EventStore.Common.Version
     public static class EventStoreVersion
     {
         public const string Version = "1.0.0";
-        public const string Hashtag = "06262f4c06d380e9a446002ec88f46c90a928403";
+        public const string Hashtag = "1c7b48c6fb1c115213239e16ac3844735ec5a88e";
         public const string Branch = "master";
     }
 }
