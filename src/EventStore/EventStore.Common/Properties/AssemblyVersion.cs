d01da273f3233214d671cd4c25067548f9da90f6
Yuriy Solodkyy <yuriy@couldbedone.com>
2013-01-18 21:09:08 +0200

    Update version info only in Release

--- src/EventStore/EventStore.Common/Properties/AssemblyVersion.cs
+++ src/EventStore/EventStore.Common/Properties/AssemblyVersion.cs
@@ -1,3 +1,3 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")] 
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")] 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.dev@468260c482e22c670b714116fed2b93830b496a8@Fri, 18 Jan 2013 20:59:09 +0200")] 
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.dev@d2ed681a28f4932df7bf3fcf2f0a7f5889ce11c2@Fri, 18 Jan 2013 21:01:06 +0200")]
