3f57d832d8d17c774e74c23e2c76e02577dee701
Andrii Nakryiko <andrii.nakryiko@gmail.com>
2013-01-18 15:13:08 +0200

    Fixed update-version scripting (especially in Windows).

--- src/EventStore/Scripts/version/update-version.sh
+++ src/EventStore/Scripts/version/update-version.sh
@@ -10,25 +10,18 @@ MSBuildProjectDirectory=$1
 
 if [ ! -e $MSBuildProjectDirectory/Properties/ESVersion.txt ] ; then err "No ESVersion.txt file found with current version!"; fi
 
-_es_version=`cat $MSBuildProjectDirectory/Properties/ESVersion.txt` || err "Read version"
-_es_branch=`git rev-parse --abbrev-ref HEAD` || err "Read branch"
-_es_hashtag=`git rev-parse HEAD` || err "Get hashtag"
-_es_timestamp=`git log HEAD -n1 --pretty="%aD"` || err "Get timestamp"
+_es_version=`cat $MSBuildProjectDirectory/Properties/ESVersion.txt`
+_es_branch=`git rev-parse --abbrev-ref HEAD`
+_es_log=`git log -n1 --pretty=format:"%H@%aD" HEAD`
 
-if [ ! -n "$_es_branch" ] ; then err "Empty branch variable"; fi;
-if [ ! -n "$_es_hashtag" ] ; then err "Empty hashtag variable"; fi;
-if [ ! -n "$_es_timestamp" ] ; then err "Empty timestamp variable"; fi;
-
-echo '[assembly: System.Reflection.AssemblyVersion("'"$_es_version.0"'")]' > /tmp/es-ver.tmp || err "Write es-ver.tmp"
-echo '[assembly: System.Reflection.AssemblyFileVersion("'"$_es_version.0"'")]' >> /tmp/es-ver.tmp || err "Write es-ver.tmp"
-echo '[assembly: System.Reflection.AssemblyInformationalVersion("'"$_es_version.$_es_branch@$_es_hashtag@$_es_timestamp"'")]' >> /tmp/es-ver.tmp || err "Write es-ver.tmp"
+echo '[assembly: System.Reflection.AssemblyVersion("'"$_es_version.0"'")]' > /tmp/es-ver.tmp
+echo '[assembly: System.Reflection.AssemblyFileVersion("'"$_es_version.0"'")]' >> /tmp/es-ver.tmp
+echo '[assembly: System.Reflection.AssemblyInformationalVersion("'"$_es_version.$_es_branch@$_es_log"'")]' >> /tmp/es-ver.tmp
 
 if diff /tmp/es-ver.tmp $MSBuildProjectDirectory/Properties/AssemblyVersion.cs >/dev/null ; then
    echo "Skip, version is the same"
 else
-   cp -f /tmp/es-ver.tmp $MSBuildProjectDirectory/Properties/AssemblyVersion.cs || err "Failed to copy AssemblyVersion.cs"
+   cp -f /tmp/es-ver.tmp $MSBuildProjectDirectory/Properties/AssemblyVersion.cs
 fi
 
 if [ -e "/tmp/es-ver.tmp" ] ; then rm /tmp/es-ver.tmp; fi
-
-echo "Done."
