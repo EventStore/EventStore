dd59e4a66d29217ba73e4ce2e3823720be9937cc
Andrii Nakryiko <andrii.nakryiko@gmail.com>
2013-01-21 13:57:56 +0200

    Fixed automatic versioning, it doesn't cause merge conflicts and works all the time.

--- src/EventStore/EventStore.Common/Properties/AssemblyVersion.cs
+++
@@ -1,3 +0,0 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.dev@d2ed681a28f4932df7bf3fcf2f0a7f5889ce11c2@Fri, 18 Jan 2013 21:01:06 +0200")]
--- src/EventStore/Scripts/version/update-version.cmd
+++ src/EventStore/Scripts/version/update-version.cmd
@@ -1,8 +1,8 @@
 @echo off   
 
-if not exist %1\Properties\ESVersion.txt goto VERSION_NOT_FOUND
+if not exist %1\..\EventStore.Common\Properties\ESVersion.txt goto VERSION_NOT_FOUND
 
-set /p _es_version=<%1\Properties\ESVersion.txt
+set /p _es_version=<%1\..\EventStore.Common\Properties\ESVersion.txt
 for /f "delims=" %%a in ('git rev-parse --abbrev-ref HEAD') do @set _es_branch=%%a
 for /f "delims=" %%a in ('call git log -n1 --pretty^=format:"%%H@%%aD" HEAD') do @set _es_log=%%a
 
@@ -10,8 +10,8 @@ echo [assembly: System.Reflection.AssemblyVersion("%_es_version: =%.0")] > %TEMP
 echo [assembly: System.Reflection.AssemblyFileVersion("%_es_version: =%.0")] >> %TEMP%\es-ver.tmp
 echo [assembly: System.Reflection.AssemblyInformationalVersion("%_es_version: =%.%_es_branch: =%@%_es_log%")] >> %TEMP%\es-ver.tmp
 
-fc %TEMP%\es-ver.tmp %1\Properties\AssemblyVersion.cs > nul && goto IDENTICAL
-copy /y %TEMP%\es-ver.tmp %1\Properties\AssemblyVersion.cs
+fc %TEMP%\es-ver.tmp %1\..\EventStore.Common\AssemblyVersion.cs > nul && goto IDENTICAL
+copy /y %TEMP%\es-ver.tmp %1\..\EventStore.Common\Properties\AssemblyVersion.cs
 :IDENTICAL
 del %TEMP%\es-ver.tmp 
 exit /b 0
