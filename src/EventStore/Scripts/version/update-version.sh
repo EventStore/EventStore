c049f43c818381c3a4626a08c79da26bca25e1ff
Andrii Nakryiko <andrii.nakryiko@gmail.com>
2013-01-21 15:36:21 +0200

    Made update-version scripts to use random temp files to allow multiple simultaneous builds.

--- src/EventStore/Scripts/version/update-version.sh
+++ src/EventStore/Scripts/version/update-version.sh
@@ -2,26 +2,27 @@
  
 function err() {
   echo Failed! ${1:-"Unknown error"}
-  if [ -e "/tmp/es-ver.tmp" ] ; then rm /tmp/es-ver.tmp; fi
+  if [ -e "$_es_tmpfile" ] ; then rm $_es_tmpfile; fi
   exit 1
 }
 
 MSBuildProjectDirectory=$1
+_es_tmpfile=`mktemp`
 
-if [ ! -e $MSBuildProjectDirectory/../EventStore.Common/Properties/ESVersion.txt ] ; then err "No ESVersion.txt file found with current version!"; fi
+if [ ! -e "$MSBuildProjectDirectory/../EventStore.Common/Properties/ESVersion.txt" ] ; then err "No ESVersion.txt file found with current version!"; fi
 
-_es_version=`cat $MSBuildProjectDirectory/../EventStore.Common/Properties/ESVersion.txt`
+_es_version=`cat "$MSBuildProjectDirectory/../EventStore.Common/Properties/ESVersion.txt"`
 _es_branch=`git rev-parse --abbrev-ref HEAD`
 _es_log=`git log -n1 --pretty=format:"%H@%aD" HEAD`
 
-echo '[assembly: System.Reflection.AssemblyVersion("'"$_es_version.0"'")]' > /tmp/es-ver.tmp
-echo '[assembly: System.Reflection.AssemblyFileVersion("'"$_es_version.0"'")]' >> /tmp/es-ver.tmp
-echo '[assembly: System.Reflection.AssemblyInformationalVersion("'"$_es_version.$_es_branch@$_es_log"'")]' >> /tmp/es-ver.tmp
+echo '[assembly: System.Reflection.AssemblyVersion("'"$_es_version.0"'")]' > "$_es_tmpfile"
+echo '[assembly: System.Reflection.AssemblyFileVersion("'"$_es_version.0"'")]' >> "$_es_tmpfile"
+echo '[assembly: System.Reflection.AssemblyInformationalVersion("'"$_es_version.$_es_branch@$_es_log"'")]' >> "$_es_tmpfile"
 
-if diff /tmp/es-ver.tmp $MSBuildProjectDirectory/../EventStore.Common/Properties/AssemblyVersion.cs >/dev/null ; then
+if diff "$_es_tmpfile" "$MSBuildProjectDirectory/../EventStore.Common/Properties/AssemblyVersion.cs" >/dev/null ; then
    echo "Skip, version is the same"
 else
-   cp -f /tmp/es-ver.tmp $MSBuildProjectDirectory/../EventStore.Common/Properties/AssemblyVersion.cs
+   cp -f "$_es_tmpfile" "$MSBuildProjectDirectory/../EventStore.Common/Properties/AssemblyVersion.cs"
 fi
 
-if [ -e "/tmp/es-ver.tmp" ] ; then rm /tmp/es-ver.tmp; fi
+if [ -e "$_es_tmpfile" ] ; then rm "$_es_tmpfile"; fi
