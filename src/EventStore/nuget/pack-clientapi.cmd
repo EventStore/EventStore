5abde4d47145dba2f4c5abcd01721effd4a50f00
James Nugent <james.nugent@bathasu.com>
2013-02-19 16:23:31 +0000

    NuGet package updated to build symbols package, also script added to get appropriate versions of source for ilmerged dependencies

--- src/EventStore/nuget/pack-clientapi.cmd
+++ src/EventStore/nuget/pack-clientapi.cmd
@@ -1,1 +1,7 @@
+@echo off
+pushd %~dp0
+IF [%1]==[] (
+	echo "No version specified - use pack-clientapi.cmd <version>"
+	exit /b1
+)
 ..\..\..\tools\nuget\nuget.exe pack -symbols -version %1 EventStore.Client.nuspec
