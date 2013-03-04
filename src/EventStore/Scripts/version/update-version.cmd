ddbc3d129e2360fdd3ed6f1db104b6dda19efc48
Andrii Nakryiko <andrii.nakryiko@gmail.com>
2013-03-04 19:09:26 +0200

    Separated EventStore and ClientApi versions.

--- src/EventStore/Scripts/version/update-version.cmd
+++ src/EventStore/Scripts/version/update-version.cmd
@@ -1,10 +1,13 @@
 @echo off   
 
-if not exist "%1\..\EventStore.Common\Properties\ESVersion.txt" goto VERSION_NOT_FOUND
+if not exist "%1\..\EventStore.Common\Properties\ESVersion.txt" goto ES_VERSION_NOT_FOUND
+if not exist "%1\..\EventStore.ClientAPI\Properties\ESCAVersion.txt" goto ESCA_VERSION_NOT_FOUND
 
 set _es_tmpfile="%TEMP%\es-ver-%RANDOM%-%TIME:~6,5%.tmp"
+set _esca_tmpfile="%TEMP%\esca-ver-%RANDOM%-%TIME:~6,5%.tmp"
 
 set /p _es_version=<"%1\..\EventStore.Common\Properties\ESVersion.txt"
+set /p _esca_version=<"%1\..\EventStore.ClientAPI\Properties\ESCAVersion.txt"
 for /f "delims=" %%a in ('git rev-parse --abbrev-ref HEAD') do @set _es_branch=%%a
 for /f "delims=" %%a in ('call git log -n1 --pretty^=format:"%%H@%%aD" HEAD') do @set _es_log=%%a
 
@@ -12,12 +15,24 @@ echo [assembly: System.Reflection.AssemblyVersion("%_es_version: =%.0")] > %_es_
 echo [assembly: System.Reflection.AssemblyFileVersion("%_es_version: =%.0")] >> %_es_tmpfile%
 echo [assembly: System.Reflection.AssemblyInformationalVersion("%_es_version: =%.%_es_branch: =%@%_es_log%")] >> %_es_tmpfile%
 
-fc %_es_tmpfile% "%1\..\EventStore.Common\Properties\AssemblyVersion.cs" && goto IDENTICAL
+echo [assembly: System.Reflection.AssemblyVersion("%_esca_version: =%.0")] > %_esca_tmpfile%
+echo [assembly: System.Reflection.AssemblyFileVersion("%_esca_version: =%.0")] >> %_esca_tmpfile%
+echo [assembly: System.Reflection.AssemblyInformationalVersion("%_esca_version: =%.%_es_branch: =%@%_es_log%")] >> %_esca_tmpfile%
+
+fc %_es_tmpfile% "%1\..\EventStore.Common\Properties\AssemblyVersion.cs" && goto IDENTICAL_ES
 copy /y %_es_tmpfile% "%1\..\EventStore.Common\Properties\AssemblyVersion.cs"
-:IDENTICAL
+:IDENTICAL_ES
+fc %_esca_tmpfile% "%1\..\EventStore.ClientAPI\Properties\AssemblyVersion.cs" && goto IDENTICAL_ESCA
+copy /y %_esca_tmpfile% "%1\..\EventStore.ClientAPI\Properties\AssemblyVersion.cs"
+:IDENTICAL_ESCA
 del %_es_tmpfile%
+del %_esca_tmpfile%
 exit /b 0
-:VERSION_NOT_FOUND
+
+:ES_VERSION_NOT_FOUND
 echo No ESVersion.txt file found with current version!
+exit /b 1
+:ESCA_VERSION_NOT_FOUND
+echo No ESCAVersion.txt file found with current version!
 exit /b 1
     
--- src/EventStore/Scripts/version/update-version.sh
+++ src/EventStore/Scripts/version/update-version.sh
@@ -2,16 +2,20 @@
  
 function err() {
   echo Failed! ${1:-"Unknown error"}
-  if [ -e "$_es_tmpfile" ] ; then rm $_es_tmpfile; fi
+  if [ -e "$_es_tmpfile" ] ; then rm "$_es_tmpfile"; fi
+  if [ -e "$_esca_tmpfile" ] ; then rm "$_esca_tmpfile"; fi
   exit 1
 }
 
 MSBuildProjectDirectory=$1
 _es_tmpfile=`mktemp`
+_esca_tmpfile=`mktemp`
 
 if [ ! -e "$MSBuildProjectDirectory/../EventStore.Common/Properties/ESVersion.txt" ] ; then err "No ESVersion.txt file found with current version!"; fi
+if [ ! -e "$MSBuildProjectDirectory/../EventStore.ClientAPI/Properties/ESCAVersion.txt" ] ; then err "No ESCAVersion.txt file found with current version!"; fi
 
 _es_version=`cat "$MSBuildProjectDirectory/../EventStore.Common/Properties/ESVersion.txt"`
+_esca_version=`cat "$MSBuildProjectDirectory/../EventStore.ClientAPI/Properties/ESCAVersion.txt"`
 _es_branch=`git rev-parse --abbrev-ref HEAD`
 _es_log=`git log -n1 --pretty=format:"%H@%aD" HEAD`
 
@@ -19,10 +23,21 @@ echo '[assembly: System.Reflection.AssemblyVersion("'"$_es_version.0"'")]' > "$_
 echo '[assembly: System.Reflection.AssemblyFileVersion("'"$_es_version.0"'")]' >> "$_es_tmpfile"
 echo '[assembly: System.Reflection.AssemblyInformationalVersion("'"$_es_version.$_es_branch@$_es_log"'")]' >> "$_es_tmpfile"
 
+echo '[assembly: System.Reflection.AssemblyVersion("'"$_esca_version.0"'")]' > "$_esca_tmpfile"
+echo '[assembly: System.Reflection.AssemblyFileVersion("'"$_esca_version.0"'")]' >> "$_esca_tmpfile"
+echo '[assembly: System.Reflection.AssemblyInformationalVersion("'"$_esca_version.$_es_branch@$_es_log"'")]' >> "$_esca_tmpfile"
+
 if diff "$_es_tmpfile" "$MSBuildProjectDirectory/../EventStore.Common/Properties/AssemblyVersion.cs" >/dev/null ; then
-   echo "Skip, version is the same"
+   echo "Skip, ES version is the same"
 else
    cp -f "$_es_tmpfile" "$MSBuildProjectDirectory/../EventStore.Common/Properties/AssemblyVersion.cs"
 fi
 
+if diff "$_esca_tmpfile" "$MSBuildProjectDirectory/../EventStore.ClientAPI/Properties/AssemblyVersion.cs" >/dev/null ; then
+   echo "Skip, ES ClientAPI version is the same"
+else
+   cp -f "$_esca_tmpfile" "$MSBuildProjectDirectory/../EventStore.ClientAPI/Properties/AssemblyVersion.cs"
+fi
+
 if [ -e "$_es_tmpfile" ] ; then rm "$_es_tmpfile"; fi
+if [ -e "$_esca_tmpfile" ] ; then rm "$_esca_tmpfile"; fi
