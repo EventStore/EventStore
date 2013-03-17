57eab613c52f7d3c9c6ca9662e98f1bd63de73ae
Yuriy Solodkyy <yuriy@couldbedone.com>
2013-03-17 20:36:47 +0200

    Fixed: platform toolset for vs2010

--- src/EventStore/Scripts/v8/configure-cpp.cmd
+++ src/EventStore/Scripts/v8/configure-cpp.cmd
@@ -2,9 +2,11 @@
     path %PATH%;%~dp0..\..\v8\third_party\python_26\;C:\Windows\Microsoft.NET\Framework\v4.0.30319\;c:\Program Files (x86)\Git\bin; || goto :error
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\vcvars32.bat" (
         set envconf="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\vcvars32.bat"
+        set platformtoolset=v110
     ) else (
         if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat" (
             set envconf="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+           set platformtoolset=v100
         ) else (
             echo "No visual Studio C++ build tools detected"
             goto :error
