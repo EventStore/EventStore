674a2ba88aaa3f55e7e6eba2069cc9fd3e5558ae
Kostyantyn Pyatkivskyy <k.pyatkivskyy@gmail.com>
2013-03-18 15:54:54 +0200

    PATH overriding updated to contain python 2,6 in the beginning.

--- src/EventStore/Scripts/v8/configure-cpp.cmd
+++ src/EventStore/Scripts/v8/configure-cpp.cmd
@@ -1,5 +1,5 @@
 @echo off
-    path %PATH%;%~dp0..\..\v8\third_party\python_26\;C:\Windows\Microsoft.NET\Framework\v4.0.30319\;c:\Program Files (x86)\Git\bin; || goto :error
+    path %~dp0..\..\v8\third_party\python_26\;C:\Windows\Microsoft.NET\Framework\v4.0.30319\;c:\Program Files (x86)\Git\bin;%PATH%; || goto :error
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\vcvars32.bat" (
         set envconf="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\vcvars32.bat"
         set platformtoolset=v110
