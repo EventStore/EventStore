9b846be144de101e4ab00b52e0e2ed07bb3e2d9d
Kostyantyn Pyatkivskyy <k.pyatkivskyy@gmail.com>
2013-03-18 15:14:52 +0200

    Specify VisualStudio version in config script to fix building on systems with both Visual Studio 2010 and 2012 installed.

--- src/EventStore/Scripts/v8/configure-cpp.cmd
+++ src/EventStore/Scripts/v8/configure-cpp.cmd
@@ -3,10 +3,12 @@
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\vcvars32.bat" (
         set envconf="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\vcvars32.bat"
         set platformtoolset=v110
+        set VisualStudioVersion=11.0
     ) else (
         if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat" (
             set envconf="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-           set platformtoolset=v100
+            set platformtoolset=v100
+            set VisualStudioVersion=10.0
         ) else (
             echo "No visual Studio C++ build tools detected"
             goto :error
