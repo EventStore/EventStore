2969efd7c0e57593a01a34d888111b95ec6587de
Andrii Nakryiko <andrii.nakryiko@gmail.com>
2013-04-23 18:56:24 +0300

    Fixed: added forgot files.

---
+++ src/EventStore/EventStore.ClientAPI/ClusterSettingsBuilder.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) 2012, Event Store LLP
+// All rights reserved.
+//  
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//  
+// Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// Neither the name of the Event Store LLP nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+using System;
+using EventStore.ClientAPI.Common.Utils;
+
+namespace EventStore.ClientAPI
+{
+    /// <summary>
+    /// Used to build a cluster settings (fluent API)
+    /// </summary>
+    public class ClusterSettingsBuilder
+    {
+        private string _clusterDns;
+        private int _maxDiscoverAttempts = Consts.DefaultMaxClusterDiscoverAttempts;
+        private int _managerExternalHttpPort = Consts.DefaultClusterManagerExternalHttpPort;
+
+        internal ClusterSettingsBuilder()
+        {
+        }
+
+        public ClusterSettingsBuilder SetClusterDns(string clusterDns)
+        {
+            Ensure.NotNullOrEmpty(clusterDns, "clusterDns");
+            _clusterDns = clusterDns;
+            return this;
+        }
+
+        public ClusterSettingsBuilder SetMaxDiscoverAttempts(int maxDiscoverAttempts)
+        {
+            if (maxDiscoverAttempts < -1)
+                throw new ArgumentOutOfRangeException("maxDiscoverAttempts", string.Format("maxDiscoverAttempts value is out of range: {0}. Allowed range: [-1, infinity].", maxDiscoverAttempts));
+            _maxDiscoverAttempts = maxDiscoverAttempts;
+            return this;
+        }
+
+        public ClusterSettingsBuilder SetManagerExternalHttpPort(int managerExternalHttpPort)
+        {
+            Ensure.Positive(managerExternalHttpPort, "managerExternalHttpPort");
+            _managerExternalHttpPort = managerExternalHttpPort;
+            return this;
+        }
+
+        public static implicit operator ClusterSettings(ClusterSettingsBuilder builder)
+        {
+            return new ClusterSettings(builder._clusterDns,
+                                       builder._maxDiscoverAttempts,
+                                       builder._managerExternalHttpPort);
+        }
+    }
+}
---
+++ src/EventStore/EventStore.ClientAPI/Core/StaticEndPointDiscoverer.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using EventStore.ClientAPI.Common.Utils;
+
+namespace EventStore.ClientAPI.Core
+{
+    internal class StaticEndPointDiscoverer: IEndPointDiscoverer
+    {
+        private readonly Task<IPEndPoint> _task;
+
+        public StaticEndPointDiscoverer(IPEndPoint endPoint)
+        {
+            Ensure.NotNull(endPoint, "endPoint");
+            _task = Task.Factory.StartNew(() => endPoint);
+        }
+
+        public Task<IPEndPoint> DiscoverAsync()
+        {
+            return _task;
+        }
+    }
+}
