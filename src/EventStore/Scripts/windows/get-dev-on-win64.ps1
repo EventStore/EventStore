240b3c3ea6f140a2579e65331909aca8820c974c
Yuriy Solodkyy <yuriy@couldbedone.com>
2013-05-01 13:00:43 +0300

    autobuild tools for windows

--- src/EventStore/Scripts/windows/get-dev-on-win64.ps1
+++ src/EventStore/Scripts/windows/get-dev-on-win64.ps1
@@ -1,3 +1,3 @@
 Set-Content $env:temp\get-event-store.cmd ((new-object net.webclient).DownloadString('https://github.com/EventStore/EventStore/raw/dev/src/EventStore/Scripts/windows/get-dev-on-win64.cmd'))
-$env:temp\get-event-store.cmd
+cmd /c $env:temp\get-event-store.cmd
 
