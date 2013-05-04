8fcdc892e8b1f831466bbc9fe86605a8e7221451
Yuriy Solodkyy <yuriy@couldbedone.com>
2013-05-04 14:46:38 +0300

    Changed: improved readability of the test

--- src/EventStore/EventStore.Projections.Core.Tests/Services/event_reader/multi_stream_reader/catching_up/when_reordering_happens_in_event_by_type_index.cs
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/event_reader/multi_stream_reader/catching_up/when_reordering_happens_in_event_by_type_index.cs
@@ -64,8 +64,7 @@ namespace EventStore.Projections.Core.Tests.Services.event_reader.multi_stream_r
             _tfPos2 = ExistingEvent("test-stream", "type1", "{}", "{Data: 2}");
             _tfPos3 = ExistingEvent("test-stream", "type2", "{}", "{Data: 3}");
 
-            // simulate index-ny-type system projection
-            ExistingEvent("$et-type1", "$>", TFPosToMetadata(_tfPos1), "0@test-stream");
+            NoStream("$et-type1");
             NoStream("$et-type2");
 
             _subscriptionId = Guid.NewGuid();
@@ -94,13 +93,19 @@ namespace EventStore.Projections.Core.Tests.Services.event_reader.multi_stream_r
                 new ReaderSubscriptionManagement.Subscribe(
                     _subscriptionId, fromZeroPosition, _readerStrategy, _readerSubscriptionOptions);
 
-            // simulate index-ny-type system projection (the second event write is delayed - awaiting for ACK from the previous write)
+            // simulate index-by-type system projection
+            yield return
+                new ClientMessage.WriteEvents(
+                    Guid.NewGuid(), new NoopEnvelope(), false, "$et-type1", ExpectedVersion.Any,
+                    new Event(Guid.NewGuid(), "$>", false, "0@test-stream", TFPosToMetadata(_tfPos1)), user: null);
+
+            // simulate index-by-type system projection (the second event write is delayed - awaiting for ACK from the previous write)
             yield return
                 new ClientMessage.WriteEvents(
                     Guid.NewGuid(), new NoopEnvelope(), false, "$et-type2", ExpectedVersion.Any,
                     new Event(Guid.NewGuid(), "$>", false, "2@test-stream", TFPosToMetadata(_tfPos3)), user: null);
 
-            // simulate index-ny-type system projection (ACK received - writing the next event)
+            // simulate index-by-type system projection (ACK received - writing the next event)
             yield return
                 new ClientMessage.WriteEvents(
                     Guid.NewGuid(), new NoopEnvelope(), false, "$et-type1", ExpectedVersion.Any,
