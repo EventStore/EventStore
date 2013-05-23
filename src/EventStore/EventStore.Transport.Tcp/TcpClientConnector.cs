6f3467688bc5497963139773e40862359b44d0dd
Andrii Nakryiko <andrii.nakryiko@gmail.com>
2013-05-23 18:35:35 +0300

    Changed: switched to lockless impl of TcpConnection.

--- src/EventStore/EventStore.Transport.Tcp/TcpClientConnector.cs
+++ src/EventStore/EventStore.Transport.Tcp/TcpClientConnector.cs
@@ -58,7 +58,7 @@ namespace EventStore.Transport.Tcp
                                         bool verbose = true)
         {
             Ensure.NotNull(remoteEndPoint, "remoteEndPoint");
-            return TcpConnection.CreateConnectingTcpConnection(connectionId, remoteEndPoint, this, onConnectionEstablished, onConnectionFailed, verbose);
+            return TcpConnectionLockless.CreateConnectingTcpConnection(connectionId, remoteEndPoint, this, onConnectionEstablished, onConnectionFailed, verbose);
         }
 
         internal void InitConnect(IPEndPoint serverEndPoint,
