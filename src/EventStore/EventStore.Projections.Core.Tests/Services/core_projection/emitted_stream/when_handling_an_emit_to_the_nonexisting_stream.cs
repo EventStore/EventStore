898bcbfc99710dcd4e9593875d69e1edad481bba
Yuriy Solodkyy <yuriy@couldbedone.com>
2013-06-08 15:30:53 +0300

    Added: tests on configuring ACL by emitted stream

--- src/EventStore/EventStore.Projections.Core.Tests/Services/core_projection/emitted_stream/when_handling_an_emit_to_the_nonexisting_stream.cs
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/core_projection/emitted_stream/when_handling_an_emit_to_the_nonexisting_stream.cs
@@ -1,41 +1,14 @@
-﻿// Copyright (c) 2012, Event Store LLP
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-// 
-// Redistributions of source code must retain the above copyright notice,
-// this list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-// Neither the name of the Event Store LLP nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
-
-using System;
+﻿using System;
 using System.Linq;
 using EventStore.Core.Messages;
+using EventStore.Core.Services;
 using EventStore.Projections.Core.Services.Processing;
 using NUnit.Framework;
 
 namespace EventStore.Projections.Core.Tests.Services.core_projection.emitted_stream
 {
     [TestFixture]
-    public class when_handling_an_emit_the_started_in_recovery_stream : TestFixtureWithExistingEvents
+    public class when_handling_an_emit_to_the_nonexisting_stream : TestFixtureWithExistingEvents
     {
         private EmittedStream _stream;
         private TestCheckpointManagerMessageHandler _readyHandler;
@@ -43,7 +16,8 @@ namespace EventStore.Projections.Core.Tests.Services.core_projection.emitted_str
         protected override void Given()
         {
             AllWritesQueueUp();
-            ExistingEvent("test_stream", "type", @"{""c"": 100, ""p"": 50}", "data");
+            AllWritesToSucceed("$$test_stream");
+            NoOtherStreams();
         }
 
         [SetUp]
@@ -69,7 +43,7 @@ namespace EventStore.Projections.Core.Tests.Services.core_projection.emitted_str
         }
 
         [Test]
-        public void does_not_publish_already_published_events()
+        public void publishes_already_published_events()
         {
             _stream.EmitEvents(
                 new[]
@@ -77,7 +51,11 @@ namespace EventStore.Projections.Core.Tests.Services.core_projection.emitted_str
                         new EmittedDataEvent(
                     "test_stream", Guid.NewGuid(), "type", "data", null, CheckpointTag.FromPosition(100, 50), null)
                     });
-            Assert.AreEqual(0, _consumer.HandledMessages.OfType<ClientMessage.WriteEvents>().Count());
+            Assert.AreEqual(
+                1,
+                _consumer.HandledMessages.OfType<ClientMessage.WriteEvents>()
+                         .ExceptOfEventType(SystemEventTypes.StreamMetadata)
+                         .Count());
         }
 
         [Test]
@@ -89,7 +67,11 @@ namespace EventStore.Projections.Core.Tests.Services.core_projection.emitted_str
                         new EmittedDataEvent(
                     "test_stream", Guid.NewGuid(), "type", "data", null, CheckpointTag.FromPosition(200, 150), null)
                     });
-            Assert.AreEqual(1, _consumer.HandledMessages.OfType<ClientMessage.WriteEvents>().Count());
+            Assert.AreEqual(
+                1,
+                _consumer.HandledMessages.OfType<ClientMessage.WriteEvents>()
+                         .ExceptOfEventType(SystemEventTypes.StreamMetadata)
+                         .Count());
         }
 
         [Test]
