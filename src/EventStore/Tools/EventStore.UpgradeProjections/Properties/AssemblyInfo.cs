3c67a41b85e413943f054b19e8849dea94d65a49
James Nugent <james.nugent@bathasu.com>
2013-08-01 12:26:41 +0100

    Projections upgrade tool
    	- switched to NuGet Client API
    	- added README file

--- src/EventStore/Tools/EventStore.UpgradeProjections/Properties/AssemblyInfo.cs
+++ src/EventStore/Tools/EventStore.UpgradeProjections/Properties/AssemblyInfo.cs
@@ -7,12 +7,15 @@
 // 
 // Redistributions of source code must retain the above copyright notice,
 // this list of conditions and the following disclaimer.
+//
 // Redistributions in binary form must reproduce the above copyright
 // notice, this list of conditions and the following disclaimer in the
 // documentation and/or other materials provided with the distribution.
+//
 // Neither the name of the Event Store LLP nor the names of its
 // contributors may be used to endorse or promote products derived from
 // this software without specific prior written permission
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -24,40 +27,22 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("EventStore.UpgradeProjections")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Event Store Projections Upgrade Tool (v1.x -> 2.0.0)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Event Store LLP")]
 [assembly: AssemblyProduct("EventStore.UpgradeProjections")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013 Event Store LLP")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e2c2f8eb-2acd-45ea-9e78-7e940ef056a8")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- src/EventStore/Tools/EventStore.UpgradeProjections/UpgradeProjections.cs
+++ src/EventStore/Tools/EventStore.UpgradeProjections/UpgradeProjections.cs
@@ -7,12 +7,15 @@
 // 
 // Redistributions of source code must retain the above copyright notice,
 // this list of conditions and the following disclaimer.
+//
 // Redistributions in binary form must reproduce the above copyright
 // notice, this list of conditions and the following disclaimer in the
 // documentation and/or other materials provided with the distribution.
+//
 // Neither the name of the Event Store LLP nor the names of its
 // contributors may be used to endorse or promote products derived from
 // this software without specific prior written permission
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -24,11 +27,10 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+
 using System;
 using System.Linq;
 using System.Net;
-using EventStore.ClientAPI.Exceptions;
 using NDesk.Options;
 
 namespace EventStore.UpgradeProjections
