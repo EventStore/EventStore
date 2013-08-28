bd9260b98828d88fea4e243c18d34b81d928bea3
Greg Young <gregoryyoung1@gmail.com>
2013-08-29 00:06:19 +0300

    add beginnings of esquery repl tool Y

---
+++ src/EventStore/esquery/CommandProcessor.cs
@@ -0,0 +1,300 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Threading;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace esquery
+{
+    class CommandProcessor
+    {
+        static string GetFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return null;
+            if (s.IndexOf(' ') < 0) return null;
+            return s.Substring(0, s.IndexOf(' '));
+        }
+
+        static List<string> EatFirstN(int n, string s)
+        {
+            var ret = new List<string>();
+            var current = 0;
+            for (int i = 0; i < n;i++)
+            {
+                var next = s.IndexOf(' ', current);
+                if(next < 0) { return null; }
+                ret.Add(s.Substring(current, next - current));
+                current = next + 1;
+            }
+            //get whatever else is on the line.
+            ret.Add(s.Substring(current, s.Length - current));
+            return ret;
+        }
+
+        public static object Process(string command, State state)
+        {
+            var c = GetFirst(command);
+            if (c == null) return new HelpCommandResult();
+            try
+            {
+                switch (c)
+                {
+                    case "a":
+                    case "append":
+                        var append = EatFirstN(3, command);
+                        if (append.Count != 4) return new InvalidCommandResult(command);
+                        return Append(append[1], append[2], append[3]);
+                    case "h":
+                    case "help":
+                        return new HelpCommandResult();
+                    case "q":
+                    case "query":
+                        var query = EatFirstN(1, command);
+                        if (query.Count != 2) return new InvalidCommandResult(command);
+                        return CreateAndRunQuery(new Uri("http://127.0.0.1:2113"), query[1], state.Args.Credentials);
+                    default:
+                        return new InvalidCommandResult(command);
+                }
+            }
+            catch
+            {
+                return new InvalidCommandResult(command);
+            }
+        }
+
+        private static Uri PostQuery(Uri baseUri, string query, NetworkCredential credential)
+        {
+            var request = WebRequest.Create("http://127.0.0.1:2113/projections/transient?enabled=yes");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = query.Length;
+            //TODO take from args
+            request.Credentials = credential;
+            using (var sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write(query);
+            }
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                var s = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                
+                if(response.StatusCode != HttpStatusCode.Created)
+                {
+                    throw new Exception("Query Failed with Status Code: " + response.StatusCode + "\n" + s);
+                }
+                return new Uri(response.Headers["Location"]);
+            }
+        }
+
+        private static QueryInformation CheckQueryStatus(Uri toCheck, NetworkCredential credential)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(toCheck);
+            request.Method = "GET";
+            request.Accept = "application/json";
+            //TODO take from args
+            request.Credentials = credential;
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                var s = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                JObject json = JObject.Parse(s);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception("Query Polling Failed with Status Code: " + response.StatusCode + "\n" + s);
+                }
+                var faulted = json["status"].Value<string>().StartsWith( "Faulted");
+                var completed = json["status"].Value<string>().StartsWith("Completed");   
+                var faultReason = json["stateReason"].Value<string>();
+                var resulturi = new Uri(json["resultStreamUrl"].Value<string>());
+                var progress = json["progress"].Value<decimal>();
+               
+                return new QueryInformation() {
+                    Faulted = faulted, 
+                    FaultReason = faultReason, 
+                    ResultUri = resulturi, 
+                    Progress=progress, 
+                    Completed=completed
+                };
+            }
+        }
+
+        private static Uri GetNamedLink(JObject feed, string name)
+        {
+            JToken token;
+            var links = feed["links"];
+            if (links == null) return null;
+            foreach (var item in links)
+            {
+                if (item["relation"].Value<string>() == name)
+                {
+                    return new Uri(item["uri"].Value<string>());
+                }
+            }
+            return null;
+        }
+
+        private static Uri GetLast(Uri head, NetworkCredential credential)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(head);
+            request.Credentials = credential;
+            request.Accept = "application/vnd.eventstore.atom+json";
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+
+                var json = JObject.Parse(new StreamReader(response.GetResponseStream()).ReadToEnd());
+                var last = GetNamedLink(json, "last");
+                return last ?? GetNamedLink(json, "self");
+            }
+        }
+
+        static Uri ReadResults(Uri uri, NetworkCredential credential)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(uri);
+            request.Credentials = credential;
+            request.Accept = "application/vnd.eventstore.atom+json";
+            using (var response = request.GetResponse())
+            {
+                var json = JObject.Parse(new StreamReader(response.GetResponseStream()).ReadToEnd());
+
+                foreach (var item in json["entries"])
+                {
+                    Console.WriteLine(item["data"].ToString());
+                }
+                return GetNamedLink(json, "previous") ?? uri;
+            }
+        }
+
+        private static void WriteItem(SyndicationItem item)
+        {
+            //TODO Print Result
+            Console.WriteLine(item.Content);
+        }
+
+        private static object CreateAndRunQuery(Uri baseUri, string query, NetworkCredential credential)
+        {
+            try
+            {
+                var watch = new Stopwatch();
+                watch.Start();
+                var toCheck = PostQuery(baseUri, query, credential);
+                var queryInformation = new QueryInformation();
+                while (!queryInformation.Completed)
+                {
+                    queryInformation = CheckQueryStatus(toCheck, credential);
+                    if (queryInformation.Faulted)
+                    {
+                        throw new Exception("Query Faulted.\n" + queryInformation.FaultReason);
+                    }
+                    Console.Write("\r{0}", queryInformation.Progress.ToString("f2") + "%");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine("\rQuery Completed in: " + watch.Elapsed);
+                var last = GetLast(queryInformation.ResultUri, credential);
+                last = new Uri(last.OriginalString + "?embed=body");
+                var next = ReadResults(last, credential);
+                return new QueryResult() {Query = query};
+            }
+            catch(Exception ex)
+            {
+                return new ErrorResult(ex);
+            }
+        }
+
+        private static AppendResult Append(string stream, string eventType, string data)
+        {
+            var message = "[{'eventType':'" + eventType + "', 'eventId' :'" + Guid.NewGuid() + "', 'data' : " + data +"}]";
+            var request = WebRequest.Create("http://127.0.0.1:2113/streams/" + stream);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = message.Length;
+            using (var sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write(message);
+            }
+            using (var response = (HttpWebResponse) request.GetResponse())
+            {
+                return new AppendResult() {StatusCode = response.StatusCode};
+            }
+        }
+
+        private class AppendResult
+        {
+            public HttpStatusCode StatusCode;
+
+            public override string ToString()
+            {
+                if (StatusCode == HttpStatusCode.Created)
+                {
+                    return "Succeeded.";
+                }
+                return StatusCode.ToString();
+            }
+        }
+
+        private class QueryResult
+        {
+            public string Query;
+
+            public override string ToString()
+            {
+                return "Query Completed";
+            }
+        }
+    }
+
+    class QueryInformation
+    {
+        public bool Faulted;
+        public string FaultReason;
+        public decimal Progress;
+        public Uri ResultUri;
+        public bool Completed;
+    }
+
+    class ErrorResult 
+    {
+        private Exception _exception;
+
+        public ErrorResult(Exception exception)
+        {
+            _exception = exception;
+        }
+
+        public override string ToString()
+        {
+            return "An error has occured\n" + _exception.Message;
+        }
+    }
+
+    class HelpCommandResult
+    {
+        public override string ToString()
+        {
+            return "esquery help:\n" + 
+                   "\th/help: prints help" + 
+                   "\tq/query {js query} executes a query.\n" + 
+                   "\ta/append {stream} {js object}: appends to a stream.\n";
+        }
+    }
+
+    class InvalidCommandResult
+    {
+        private string _command;
+
+        public InvalidCommandResult(string command)
+        {
+            _command = command;
+        }
+
+        public override string ToString()
+        {
+            return "Invalid command: '" + _command + "'";
+        }
+    }
+}
---
+++ src/EventStore/esquery/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace esquery
+{
+    class Program
+    {
+        static void Loop(Func<State,State> func, State initial)
+        {
+            var state = initial;
+            while (true)
+            {
+                state = func(state);
+                if (state.Exit) break;
+            }
+        }
+
+        static bool IsPiped()
+        {
+            try
+            {
+                var nothing = Console.KeyAvailable;
+                return false;
+            }
+            catch (InvalidOperationException expected)
+            {
+                return true;
+            }
+        }
+        static Tuple<object, State> Eval(Tuple<string, State> data)
+        {
+            var str = data.Item1;
+            if(str == null) { data.Item2.Exit = true;}
+            var builder = data.Item2.Current;
+            if (!string.IsNullOrEmpty(str))
+            {
+                builder.AppendLine(str);
+                return new Tuple<object, State>(null, data.Item2);
+            }
+            
+            var command = builder.ToString();
+            builder.Clear();
+            return new Tuple<object, State>(CommandProcessor.Process(command, data.Item2), data.Item2);
+        }
+
+        static Tuple<string, State> Read(State state)
+        {
+            var piped = IsPiped();
+            if(state.Current.Length == 0)
+                Console.Write("es:> ");
+            var read = Console.ReadLine();
+            if(piped && read != null)
+                Console.WriteLine(read);
+            if (read == null && piped) return new Tuple<string, State>(null, state);
+            return new Tuple<string, State>(read, state);
+        }
+
+        static Tuple<object, State> Print(Tuple<object,State> data)
+        {
+            if(data.Item1 != null)
+                  Console.WriteLine("\n" + data.Item1);
+            return data;
+        }
+        
+        private static Args ReadArgs(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                Console.WriteLine("Server set to: " + args[0]);
+                return new Args(false, args[0], new NetworkCredential("admin", "changeit"));
+            }
+            Console.WriteLine("No server set defaulting to http://127.0.0.1:2113/");
+            return new Args(true, "http://127.0.0.1:2113/", new NetworkCredential("admin", "changeit"));
+        }
+
+        static void Main(string[] args)
+        {
+            Loop(s =>Print(Eval(Read(s))).Item2, new State() { Args = ReadArgs(args) });
+        }
+    }
+
+    class State
+    {
+        public StringBuilder Current = new StringBuilder();
+        public Args Args;
+        public bool Exit;
+    }
+
+    class Args
+    {
+        public readonly bool KeepRunning;
+        public NetworkCredential Credentials;
+
+        public Args(bool keepRunning, string http, NetworkCredential credentials)
+        {
+            KeepRunning = keepRunning;
+            Credentials = credentials;
+        }
+    }
+
+}
