140d954efebf3bef4286f3d3cf86ff141917a8a3
Yuriy Solodkyy <yuriy@couldbedone.com>
2013-09-17 12:24:21 +0300

    Added: the simplest balancing based on scheduled task size

---
+++ src/EventStore/EventStore.Projections.Core.Tests/Services/parallel_processing_load_balancer/when_accounting_measured_tasks.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace EventStore.Projections.Core.Tests.Services.parallel_processing_load_balancer
+{
+    [TestFixture]
+    public class when_accounting_measured_tasks : specification_with_parallel_processing_load_balancer
+    {
+        private int _task1ScheduledOn;
+        private int _task2ScheduledOn;
+
+        protected override void Given()
+        {
+            _balancer.ScheduleTask("task1", OnScheduled);
+            _balancer.ScheduleTask("task2", OnScheduled);
+        }
+
+        protected override void When()
+        {
+            _balancer.AccountMeasured("task1", 1000);
+            _balancer.AccountMeasured("task1", 100);
+        }
+
+        private void OnScheduled(string task, int worker)
+        {
+            switch (task)
+            {
+                case "task1":
+                {
+                    _task1ScheduledOn = worker;
+                    break;
+                }
+                case "task2":
+                {
+                    _task2ScheduledOn = worker;
+                    break;
+                }
+                default:
+                    Assert.Inconclusive();
+                    break;
+            }
+        }
+
+        [Test]
+        public void schedules_on_least_loaded_worker()
+        {
+            var scheduledOn = -1;
+            _balancer.ScheduleTask("task3", (s, on) => { scheduledOn = on; });
+
+            Assert.AreEqual(_task2ScheduledOn, scheduledOn);
+        }
+
+        [Test]
+        public void schedules_nearest_tasks_on_least_loaded_worker()
+        {
+            var scheduled3On = -1;
+            var scheduled4On = -1;
+            _balancer.ScheduleTask("task3", (s, on) => { scheduled3On = on; });
+            _balancer.ScheduleTask("task4", (s, on) => { scheduled4On = on; });
+
+            Assert.AreEqual(_task2ScheduledOn, scheduled3On);
+            Assert.AreEqual(_task2ScheduledOn, scheduled4On);
+        }
+    }
+}
--- src/EventStore/EventStore.Projections.Core/Services/Processing/ParallelProcessingLoadBalancer.cs
+++ src/EventStore/EventStore.Projections.Core/Services/Processing/ParallelProcessingLoadBalancer.cs
@@ -27,22 +27,43 @@
 // 
 
 using System;
+using System.Collections.Generic;
 
 namespace EventStore.Projections.Core.Services.Processing
 {
     public class ParallelProcessingLoadBalancer
     {
-        private class State
+        private class WorkerState
         {
+            private readonly int Worker ;
             public int UnmeasuredTasksScheduled;
+            public int MeasuredTasksScheduled;
             public long ScheduledSize;
+
+            public WorkerState(int worker)
+            {
+                Worker = worker;
+            }
+        }
+
+        private class TaskState
+        {
+            private readonly object Task;
+            internal readonly int Worker;
+
+            public TaskState(object task, int worker)
+            {
+                Task = task;
+                Worker = worker;
+            }
         }
 
         private readonly int _workers;
         private readonly long _maxScheduledSizePerWorker;
         private readonly int _maxUnmeasuredTasksPerWorker;
 
-        private readonly State[] _state;
+        private readonly WorkerState[] _workerState;
+        private readonly Dictionary<object, TaskState> _tasks = new Dictionary<object, TaskState>();
 
         public ParallelProcessingLoadBalancer(
             int workers, long maxScheduledSizePerWorker, int maxUnmeasuredTasksPerWorker)
@@ -55,15 +76,19 @@ namespace EventStore.Projections.Core.Services.Processing
             _maxScheduledSizePerWorker = maxScheduledSizePerWorker;
             _maxUnmeasuredTasksPerWorker = maxUnmeasuredTasksPerWorker;
 
-            _state = new State[workers];
-            for (int index = 0; index < _state.Length; index++)
-                _state[index] = new State();
+            _workerState = new WorkerState[workers];
+            for (int index = 0; index < _workerState.Length; index++)
+                _workerState[index] = new WorkerState(index);
 
         }
 
         public void AccountMeasured(object task, int size)
         {
-            throw new NotImplementedException();
+            var taskState = _tasks[task];
+            var workerState = _workerState[taskState.Worker];
+            workerState.MeasuredTasksScheduled++;
+            workerState.UnmeasuredTasksScheduled--;
+            workerState.ScheduledSize += size;
         }
 
         public void AccountCompleted(object task)
@@ -74,7 +99,8 @@ namespace EventStore.Projections.Core.Services.Processing
         public void ScheduleTask<T>(T task, Action<T, int> scheduled)
         {
             var index = FindLeastLoaded();
-            _state[index].UnmeasuredTasksScheduled ++;
+            _workerState[index].UnmeasuredTasksScheduled ++;
+            _tasks.Add(task, new TaskState(task, index));
             scheduled(task, index);
         }
 
@@ -82,7 +108,7 @@ namespace EventStore.Projections.Core.Services.Processing
         {
             var bestIndex = -1;
             var best = long.MaxValue;
-            for (var i = 0; i < _state.Length; i++)
+            for (var i = 0; i < _workerState.Length; i++)
             {
                 var current = EstimateWorkerLoad(i);
                 if (current < best)
@@ -96,7 +122,7 @@ namespace EventStore.Projections.Core.Services.Processing
 
         private long EstimateWorkerLoad(int i)
         {
-            return _state[i].UnmeasuredTasksScheduled*10 + _state[i].ScheduledSize;
+            return _workerState[i].UnmeasuredTasksScheduled*10 + _workerState[i].ScheduledSize;
         }
     }
 }
