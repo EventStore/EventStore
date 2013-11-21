b3428bcec452c205640e579b96871e2abdcb0f1b
James Nugent <james.nugent@bathasu.com>
2013-11-21 14:52:57 +0000

    Add help to cmd file

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 @echo off
 
 if '%1'=='/?' goto help
+if '%1'=='/help' goto help
+if '%1'=='--help' goto help
 if '%1'=='-help' goto help
 if '%1'=='-h' goto help
 
@@ -10,3 +12,6 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\tools\powershel
 exit /B %errorlevel%
 
 :help
+
+echo Usage:
+echo build.cmd build-type configuration platform version specific-vs-version force-network defines
