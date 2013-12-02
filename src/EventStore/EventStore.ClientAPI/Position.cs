f808fda248bff87afcdac7c247b9d6d65b8a1549
James Nugent <james.nugent@bathasu.com>
2013-12-02 15:06:11 +0000

    Add XML documentation to Position

--- src/EventStore/EventStore.ClientAPI/Position.cs
+++ src/EventStore/EventStore.ClientAPI/Position.cs
@@ -26,47 +26,114 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //  
 
+using System;
+
 namespace EventStore.ClientAPI
 {
+    /// <summary>
+    /// A structure referring to a potential logical record position
+    /// in the Event Store transaction file.
+    /// </summary>
     public struct Position
     {
+        /// <summary>
+        /// Position representing the start of the transaction file
+        /// </summary>
         public static readonly Position Start = new Position(0, 0);
+        /// <summary>
+        /// Position representing the end of the transaction file
+        /// </summary>
         public static readonly Position End = new Position(-1, -1);
 
+        /// <summary>
+        /// The commit position of the record
+        /// </summary>
         public readonly long CommitPosition;
+        /// <summary>
+        /// The prepare position of the record.
+        /// </summary>
         public readonly long PreparePosition;
 
+        /// <summary>
+        /// Constructs a position with the given commit and prepare positions.
+        /// It is not guaranteed that the position is actually the start of a
+        /// record in the transaction file.
+        /// 
+        /// The commit position cannot be less than the prepare position.
+        /// </summary>
+        /// <param name="commitPosition">The commit position of the record.</param>
+        /// <param name="preparePosition">The prepare position of the record.</param>
         public Position(long commitPosition, long preparePosition)
         {
+            if (commitPosition < preparePosition)
+                throw new ArgumentException("The commit position cannot be less than the prepare position", "commitPosition");
+
             CommitPosition = commitPosition;
             PreparePosition = preparePosition;
         }
 
+        /// <summary>
+        /// Compares whether p1 &lt; p2.
+        /// </summary>
+        /// <param name="p1">A <see cref="Position" />.</param>
+        /// <param name="p2">A <see cref="Position" />.</param>
+        /// <returns>True if p1 &lt; p2.</returns>
         public static bool operator <(Position p1, Position p2)
         {
             return p1.CommitPosition < p2.CommitPosition || (p1.CommitPosition == p2.CommitPosition && p1.PreparePosition < p2.PreparePosition);
         }
 
+        
+        /// <summary>
+        /// Compares whether p1 &gt; p2.
+        /// </summary>
+        /// <param name="p1">A <see cref="Position" />.</param>
+        /// <param name="p2">A <see cref="Position" />.</param>
+        /// <returns>True if p1 &gt; p2.</returns>
         public static bool operator >(Position p1, Position p2)
         {
             return p1.CommitPosition > p2.CommitPosition || (p1.CommitPosition == p2.CommitPosition && p1.PreparePosition > p2.PreparePosition);
         }
 
+        /// <summary>
+        /// Compares whether p1 &gt;= p2.
+        /// </summary>
+        /// <param name="p1">A <see cref="Position" />.</param>
+        /// <param name="p2">A <see cref="Position" />.</param>
+        /// <returns>True if p1 &gt;= p2.</returns>
         public static bool operator >=(Position p1, Position p2)
         {
             return p1 > p2 || p1 == p2;
         }
 
+        /// <summary>
+        /// Compares whether p1 &lt;= p2.
+        /// </summary>
+        /// <param name="p1">A <see cref="Position" />.</param>
+        /// <param name="p2">A <see cref="Position" />.</param>
+        /// <returns>True if p1 &lt;= p2.</returns>
         public static bool operator <=(Position p1, Position p2)
         {
             return p1 < p2 || p1 == p2;
         }
 
+        /// <summary>
+        /// Compares p1 and p2 for equality.
+        /// </summary>
+        /// <param name="p1">A <see cref="Position" />.</param>
+        /// <param name="p2">A <see cref="Position" />.</param>
+        /// <returns>True if p1 is equal to p2.</returns>
         public static bool operator ==(Position p1, Position p2)
         {
             return p1.CommitPosition == p2.CommitPosition && p1.PreparePosition == p2.PreparePosition;
         }
 
+        /// <summary>
+        /// Compares p1 and p2 for equality.
+        /// </summary>
+        /// <param name="p1">A <see cref="Position" />.</param>
+        /// <param name="p2">A <see cref="Position" />.</param>
+        /// <returns>True if p1 is not equal to p2.</returns>
         public static bool operator !=(Position p1, Position p2)
         {
             return !(p1 == p2);
@@ -77,6 +144,12 @@ namespace EventStore.ClientAPI
             return other is Position && Equals((Position)other);
         }
 
+        /// <summary>
+        /// Compares this instance of <see cref="Position" /> for equality
+        /// with another instance.
+        /// </summary>
+        /// <param name="other">A <see cref="Position" /></param>
+        /// <returns>True if this instance is equal to the other instance.</returns>
         public bool Equals(Position other)
         {
             return this == other;
