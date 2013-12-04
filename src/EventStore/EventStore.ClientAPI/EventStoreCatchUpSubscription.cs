f4c93d66ba3a9390ed5b6551e905e909f8b36c9b
James Nugent <james.nugent@bathasu.com>
2013-12-04 16:24:52 +0000

    Make catch-up subscriptions reconnect on failure

--- src/EventStore/EventStore.ClientAPI/EventStoreCatchUpSubscription.cs
+++ src/EventStore/EventStore.ClientAPI/EventStoreCatchUpSubscription.cs
@@ -162,15 +162,51 @@ namespace EventStore.ClientAPI
 
         internal void Start()
         {
+            if (Verbose) Log.Debug("Catch-up Subscription to {0}: starting...", IsSubscribedToAll ? "<all>" : StreamId);
+            RunSubscription();
+        }
+
+        /// <summary>
+        /// Attempts to stop the subscription.
+        /// </summary>
+        /// <param name="timeout">The amount of time within which the subscription should stop.</param>
+        /// <exception cref="TimeoutException">Thrown if the subscription fails to stop within it's timeout period.</exception>
+        public void Stop(TimeSpan timeout)
+        {
+            if (Verbose) Log.Debug("Catch-up Subscription to {0}: requesting stop...", IsSubscribedToAll ? "<all>" : StreamId);
+
+            if (Verbose) Log.Debug("Catch-up Subscription to {0}: unhooking from connection.Connected.");
+            _connection.Connected -= OnReconnect;
+
+            _stop = true;
+            EnqueueSubscriptionDropNotification(SubscriptionDropReason.UserInitiated, null);
+            if (!_stopped.Wait(timeout))
+                throw new TimeoutException(string.Format("Couldn't stop {0} in time.", GetType().Name));
+        }
+
+        private void OnReconnect(object sender, ClientConnectionEventArgs clientConnectionEventArgs)
+        {
+            if (Verbose) Log.Debug("Catch-up Subscription to {0}: recovering after reconnection.");
+            if (Verbose) Log.Debug("Catch-up Subscription to {0}: unhooking from connection.Connected.");
+            _connection.Connected -= OnReconnect;
+            RunSubscription();
+        }
+
+        private void RunSubscription()
+        {
             ThreadPool.QueueUserWorkItem(_ =>
             {
-                if (Verbose) Log.Debug("Catch-up Subscription to {0}: starting...", IsSubscribedToAll ? "<all>" : StreamId);
+                if (Verbose) Log.Debug("Catch-up Subscription to {0}: running...", IsSubscribedToAll ? "<all>" : StreamId);
 
                 _stopped.Reset();
                 try
                 {
-                    if (Verbose) Log.Debug("Catch-up Subscription to {0}: pulling events...", IsSubscribedToAll ? "<all>" : StreamId);
-                    ReadEventsTill(_connection, _resolveLinkTos, _userCredentials, null, null);
+                    if (!_stop)
+                    {
+                        if (Verbose) Log.Debug("Catch-up Subscription to {0}: pulling events...", IsSubscribedToAll ? "<all>" : StreamId);
+                        ReadEventsTill(_connection, _resolveLinkTos, _userCredentials, null, null);
+                    }
+
                     if (!_stop)
                     {
                         if (Verbose) Log.Debug("Catch-up Subscription to {0}: subscribing...", IsSubscribedToAll ? "<all>" : StreamId);
@@ -195,28 +231,18 @@ namespace EventStore.ClientAPI
                 }
 
                 if (Verbose) Log.Debug("Catch-up Subscription to {0}: processing live events...", IsSubscribedToAll ? "<all>" : StreamId);
-                if (_liveProcessingStarted != null) 
-                    _liveProcessingStarted(this); 
+
+                if (_liveProcessingStarted != null)
+                    _liveProcessingStarted(this);
+
+                if (Verbose) Log.Debug("Catch-up Subscription to {0}: hooking to connection.Connected");
+                _connection.Connected += OnReconnect;
+
                 _allowProcessing = true;
                 EnsureProcessingPushQueue();
             });
         }
-
-        /// <summary>
-        /// Attempts to stop the subscription.
-        /// </summary>
-        /// <param name="timeout">The timeout within which the subscription should stop.</param>
-        /// <exception cref="TimeoutException">Thrown if the subscription fails to stop within the timeout.</exception>
-        public void Stop(TimeSpan timeout)
-        {
-            if (Verbose) Log.Debug("Catch-up Subscription to {0}: requesting stop...", IsSubscribedToAll ? "<all>" : StreamId);
-
-            _stop = true;
-            EnqueueSubscriptionDropNotification(SubscriptionDropReason.UserInitiated, null);
-            if (!_stopped.Wait(timeout))
-                throw new TimeoutException(string.Format("Couldn't stop {0} in time.", GetType().Name));
-        }
-
+        
         private void EnqueuePushedEvent(EventStoreSubscription subscription, ResolvedEvent e)
         {
             if (Verbose)
@@ -232,6 +258,7 @@ namespace EventStore.ClientAPI
             }
 
             _liveQueue.Enqueue(e);
+
             if (_allowProcessing)
                 EnsureProcessingPushQueue();
         }
@@ -270,6 +297,7 @@ namespace EventStore.ClientAPI
                     {
                         if (_dropData == null) throw new Exception("Drop reason not specified.");
                         DropSubscription(_dropData.Reason, _dropData.Error);
+                        Interlocked.CompareExchange(ref _isProcessing, 0, 1);
                         return;
                     }
 
