f2659c4e479027e2f50040ca273f2ad1c2845fe9
James Nugent <james.nugent@bathasu.com>
2014-04-01 16:17:38 +0100

    Fix client connecting via gossip seeds
    - This fixes the issue reported on the list in the "connection to cluster
      using gossip seeds fails (client api)" thread.
    - ClusterSettingsBuilder now forces you to choose first between DNS or
      Gossip Seed configuration and then presents only the correct
      configuration points.
    - This is a breaking API change!

--- src/EventStore/EventStore.ClientAPI/Core/IEndPointDiscoverer.cs
+++ src/EventStore/EventStore.ClientAPI/Core/IEndPointDiscoverer.cs
@@ -1,29 +1,8 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Threading.Tasks;
 
 namespace EventStore.ClientAPI.Core
 {
-    internal struct NodeEndPoints
-    {
-        public readonly IPEndPoint TcpEndPoint;
-        public readonly IPEndPoint SecureTcpEndPoint;
-
-        public NodeEndPoints(IPEndPoint tcpEndPoint, IPEndPoint secureTcpEndPoint)
-        {
-            if ((tcpEndPoint ?? secureTcpEndPoint) == null) throw new ArgumentException("Both endpoints are null.");
-            TcpEndPoint = tcpEndPoint;
-            SecureTcpEndPoint = secureTcpEndPoint;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("[{0}, {1}]",
-                                 TcpEndPoint == null ? "n/a" : TcpEndPoint.ToString(),
-                                 SecureTcpEndPoint == null ? "n/a" : SecureTcpEndPoint.ToString());
-        }
-    }
-
     internal interface IEndPointDiscoverer
     {
         Task<NodeEndPoints> DiscoverAsync(IPEndPoint failedTcpEndPoint);
