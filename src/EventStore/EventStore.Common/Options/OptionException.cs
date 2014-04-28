32408a72ba902ceccca7882d9c0018d1ea6f13e2
pgermishuys <pieter@pieterg.com>
2014-04-28 10:53:10 +0200

    Removed OptsHelper tests. Added OptionException stand-alone type. Removed source dependencies from old Options

---
+++ src/EventStore/EventStore.Common/Options/OptionException.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+
+namespace EventStore.Common.Options
+{
+    [Serializable]
+    public class OptionException : Exception
+    {
+        private string option;
+
+        public OptionException()
+        {
+        }
+
+        public OptionException(string message, string optionName)
+            : base(message)
+        {
+            this.option = optionName;
+        }
+
+        public OptionException(string message, string optionName, Exception innerException)
+            : base(message, innerException)
+        {
+            this.option = optionName;
+        }
+
+        protected OptionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            this.option = info.GetString("OptionName");
+        }
+
+        public string OptionName
+        {
+            get { return this.option; }
+        }
+
+        [SecurityPermission(SecurityAction.LinkDemand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("OptionName", option);
+        }
+    }
+}
