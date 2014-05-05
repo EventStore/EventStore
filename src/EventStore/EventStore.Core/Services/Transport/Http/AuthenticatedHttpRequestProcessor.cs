ec46edc077789c07da4910e5102e93a2806a1b85
Yuriy Solodkyy <yuriy@couldbedone.com>
2014-05-05 15:06:17 +0300

    Revert "Fixed: incorrect timeout handling in http"
    This reverts commit 94c06830038f95d406eddf114d0589ddcaf2abcf.

--- src/EventStore/EventStore.Core/Services/Transport/Http/AuthenticatedHttpRequestProcessor.cs
+++ src/EventStore/EventStore.Core/Services/Transport/Http/AuthenticatedHttpRequestProcessor.cs
@@ -33,24 +33,14 @@ namespace EventStore.Core.Services.Transport.Http
                 return;
             try
             {
-                var utcNow = DateTime.UtcNow;
                 while (_pending.Count > 0)
                 {
                     var req = _pending.FindMin();
-                    if (req.Item1 <= utcNow || !req.Item2.IsProcessing)
+                    if (req.Item1 <= DateTime.UtcNow || req.Item2.IsProcessing)
                     {
                         req = _pending.DeleteMin();
-
-                        //NOTE: if new element appear before old min it should also be removed
-                        // so we only check whether it is still active
-                        if (req.Item2.IsProcessing)
-                            req.Item2.ReplyStatus(
-                                HttpStatusCode.RequestTimeout,
-                                "Server was unable to handle request in time",
-                                e =>
-                                    Log.Debug(
-                                        "Error occurred while closing timed out connection (http service core): {0}.",
-                                        e.Message));
+                        req.Item2.ReplyStatus(HttpStatusCode.RequestTimeout, "Server was unable to handle request in time",
+                                              e => Log.Debug("Error occurred while closing timed out connection (http service core): {0}.", e.Message));
                     }
                     else 
                         break;
