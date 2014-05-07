b75bd6413749e3e5ad2467800e4fba5868471eea
Greg Young <gregoryyoung1@gmail.com>
2014-05-07 15:14:52 +0300

    get auto.sh working again (now operates in debug)

--- run_tests.sh
+++ run_tests.sh
@@ -34,7 +34,7 @@ if [[ $MONOPATH == "" ]]; then
     MONOPATH="/opt/mono"
 fi
 
-LD_LIBRARY_PATH=bin/eventstore/$CONFIGURATION/anycpu/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/eventstore.tests/$CONFIGURATION/anycpu/EventStore.Core.Tests.dll $EXCLUDE -xml=inter
+LD_LIBRARY_PATH=bin/eventstore/$CONFIGURATION/anycpu/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/eventstore.tests/$CONFIGURATION/anycpu/EventStore.Core.Tests.dll $EXCLUDE -xml=inter 
 rc=$?
 xsltproc tools/nunit-2.6.3/results.xslt inter
 rm inter
