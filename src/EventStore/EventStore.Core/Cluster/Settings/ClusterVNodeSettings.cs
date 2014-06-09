00ddc96f6689b99d127884ef26c94207c18477c1
James Nugent <james.nugent@bathasu.com>
2014-06-09 15:54:23 +0100

    Show timeouts on ClusterVNodeSettings ToString

--- src/EventStore/EventStore.Core/Cluster/Settings/ClusterVNodeSettings.cs
+++ src/EventStore/EventStore.Core/Cluster/Settings/ClusterVNodeSettings.cs
@@ -9,71 +9,71 @@ using EventStore.Core.Services.Monitoring;
 
 namespace EventStore.Core.Cluster.Settings
 {
-	public class ClusterVNodeSettings
-	{
-		public readonly VNodeInfo NodeInfo;
-		public readonly string[] HttpPrefixes;
-		public readonly bool EnableTrustedAuth;
-		public readonly X509Certificate2 Certificate;
-		public readonly int WorkerThreads;
+    public class ClusterVNodeSettings
+    {
+        public readonly VNodeInfo NodeInfo;
+        public readonly string[] HttpPrefixes;
+        public readonly bool EnableTrustedAuth;
+        public readonly X509Certificate2 Certificate;
+        public readonly int WorkerThreads;
 
-		public readonly bool DiscoverViaDns;
-		public readonly string ClusterDns;
-		public readonly IPEndPoint[] GossipSeeds;
+        public readonly bool DiscoverViaDns;
+        public readonly string ClusterDns;
+        public readonly IPEndPoint[] GossipSeeds;
 
-		public readonly TimeSpan MinFlushDelay;
+        public readonly TimeSpan MinFlushDelay;
 
-		public readonly int ClusterNodeCount;
-		public readonly int PrepareAckCount;
-		public readonly int CommitAckCount;
-		public readonly TimeSpan PrepareTimeout;
-		public readonly TimeSpan CommitTimeout;
+        public readonly int ClusterNodeCount;
+        public readonly int PrepareAckCount;
+        public readonly int CommitAckCount;
+        public readonly TimeSpan PrepareTimeout;
+        public readonly TimeSpan CommitTimeout;
 
-	    public readonly int NodePriority;
+        public readonly int NodePriority;
 
-		public readonly bool UseSsl;
-		public readonly string SslTargetHost;
-		public readonly bool SslValidateServer;
+        public readonly bool UseSsl;
+        public readonly string SslTargetHost;
+        public readonly bool SslValidateServer;
 
-		public readonly TimeSpan StatsPeriod;
-		public readonly StatsStorage StatsStorage;
+        public readonly TimeSpan StatsPeriod;
+        public readonly StatsStorage StatsStorage;
 
-		public readonly IAuthenticationProviderFactory AuthenticationProviderFactory;
-	    public readonly bool DisableScavengeMerging;
-	    public bool AdminOnPublic;
-	    public bool StatsOnPublic;
+        public readonly IAuthenticationProviderFactory AuthenticationProviderFactory;
+        public readonly bool DisableScavengeMerging;
+        public bool AdminOnPublic;
+        public bool StatsOnPublic;
         public bool GossipOnPublic;
         public readonly TimeSpan GossipInterval;
         public readonly TimeSpan GossipAllowedTimeDifference;
         public readonly TimeSpan GossipTimeout;
 
-	    public ClusterVNodeSettings(Guid instanceId, int debugIndex,
-									IPEndPoint internalTcpEndPoint,
-									IPEndPoint internalSecureTcpEndPoint,
-									IPEndPoint externalTcpEndPoint,
-									IPEndPoint externalSecureTcpEndPoint,
-									IPEndPoint internalHttpEndPoint,
-									IPEndPoint externalHttpEndPoint,
-									string[] httpPrefixes,
-									bool enableTrustedAuth,
-									X509Certificate2 certificate,
-									int workerThreads,
-									bool discoverViaDns,
-									string clusterDns,
-									IPEndPoint[] gossipSeeds,
-									TimeSpan minFlushDelay,
-									int clusterNodeCount,
-									int prepareAckCount,
-									int commitAckCount,
-									TimeSpan prepareTimeout,
-									TimeSpan commitTimeout,
-									bool useSsl,
-									string sslTargetHost,
-									bool sslValidateServer,
-									TimeSpan statsPeriod,
-									StatsStorage statsStorage,
-									int nodePriority,
-									IAuthenticationProviderFactory authenticationProviderFactory,
+        public ClusterVNodeSettings(Guid instanceId, int debugIndex,
+                                    IPEndPoint internalTcpEndPoint,
+                                    IPEndPoint internalSecureTcpEndPoint,
+                                    IPEndPoint externalTcpEndPoint,
+                                    IPEndPoint externalSecureTcpEndPoint,
+                                    IPEndPoint internalHttpEndPoint,
+                                    IPEndPoint externalHttpEndPoint,
+                                    string[] httpPrefixes,
+                                    bool enableTrustedAuth,
+                                    X509Certificate2 certificate,
+                                    int workerThreads,
+                                    bool discoverViaDns,
+                                    string clusterDns,
+                                    IPEndPoint[] gossipSeeds,
+                                    TimeSpan minFlushDelay,
+                                    int clusterNodeCount,
+                                    int prepareAckCount,
+                                    int commitAckCount,
+                                    TimeSpan prepareTimeout,
+                                    TimeSpan commitTimeout,
+                                    bool useSsl,
+                                    string sslTargetHost,
+                                    bool sslValidateServer,
+                                    TimeSpan statsPeriod,
+                                    StatsStorage statsStorage,
+                                    int nodePriority,
+                                    IAuthenticationProviderFactory authenticationProviderFactory,
                                     bool disableScavengeMerging,
                                     bool adminOnPublic,
                                     bool statsOnPublic,
@@ -81,110 +81,113 @@ namespace EventStore.Core.Cluster.Settings
                                     TimeSpan gossipInterval,
                                     TimeSpan gossipAllowedTimeDifference,
                                     TimeSpan gossipTimeout)
-		{
-			Ensure.NotEmptyGuid(instanceId, "instanceId");
-			Ensure.NotNull(internalTcpEndPoint, "internalTcpEndPoint");
-			Ensure.NotNull(externalTcpEndPoint, "externalTcpEndPoint");
-			Ensure.NotNull(internalHttpEndPoint, "internalHttpEndPoint");
-			Ensure.NotNull(externalHttpEndPoint, "externalHttpEndPoint");
-			Ensure.NotNull(httpPrefixes, "httpPrefixes");
-			if (internalSecureTcpEndPoint != null || externalSecureTcpEndPoint != null)
-				Ensure.NotNull(certificate, "certificate");
-			Ensure.Positive(workerThreads, "workerThreads");
-			Ensure.NotNull(clusterDns, "clusterDns");
-			Ensure.NotNull(gossipSeeds, "gossipSeeds");
-			Ensure.Positive(clusterNodeCount, "clusterNodeCount");
-			Ensure.Positive(prepareAckCount, "prepareAckCount");
-			Ensure.Positive(commitAckCount, "commitAckCount");
-			
-			if (discoverViaDns && string.IsNullOrWhiteSpace(clusterDns))
-				throw new ArgumentException("Either DNS Discovery must be disabled (and seeds specified), or a cluster DNS name must be provided.");
+        {
+            Ensure.NotEmptyGuid(instanceId, "instanceId");
+            Ensure.NotNull(internalTcpEndPoint, "internalTcpEndPoint");
+            Ensure.NotNull(externalTcpEndPoint, "externalTcpEndPoint");
+            Ensure.NotNull(internalHttpEndPoint, "internalHttpEndPoint");
+            Ensure.NotNull(externalHttpEndPoint, "externalHttpEndPoint");
+            Ensure.NotNull(httpPrefixes, "httpPrefixes");
+            if (internalSecureTcpEndPoint != null || externalSecureTcpEndPoint != null)
+                Ensure.NotNull(certificate, "certificate");
+            Ensure.Positive(workerThreads, "workerThreads");
+            Ensure.NotNull(clusterDns, "clusterDns");
+            Ensure.NotNull(gossipSeeds, "gossipSeeds");
+            Ensure.Positive(clusterNodeCount, "clusterNodeCount");
+            Ensure.Positive(prepareAckCount, "prepareAckCount");
+            Ensure.Positive(commitAckCount, "commitAckCount");
 
-			if (useSsl)
-				Ensure.NotNull(sslTargetHost, "sslTargetHost");
+            if (discoverViaDns && string.IsNullOrWhiteSpace(clusterDns))
+                throw new ArgumentException("Either DNS Discovery must be disabled (and seeds specified), or a cluster DNS name must be provided.");
 
-			NodeInfo = new VNodeInfo(instanceId, debugIndex,
-									 internalTcpEndPoint, internalSecureTcpEndPoint,
-									 externalTcpEndPoint, externalSecureTcpEndPoint,
-									 internalHttpEndPoint, externalHttpEndPoint);
-			HttpPrefixes = httpPrefixes;
-			EnableTrustedAuth = enableTrustedAuth;
-			Certificate = certificate;
-			WorkerThreads = workerThreads;
+            if (useSsl)
+                Ensure.NotNull(sslTargetHost, "sslTargetHost");
 
-			DiscoverViaDns = discoverViaDns;
-			ClusterDns = clusterDns;
-			GossipSeeds = gossipSeeds;
+            NodeInfo = new VNodeInfo(instanceId, debugIndex,
+                                     internalTcpEndPoint, internalSecureTcpEndPoint,
+                                     externalTcpEndPoint, externalSecureTcpEndPoint,
+                                     internalHttpEndPoint, externalHttpEndPoint);
+            HttpPrefixes = httpPrefixes;
+            EnableTrustedAuth = enableTrustedAuth;
+            Certificate = certificate;
+            WorkerThreads = workerThreads;
 
-			ClusterNodeCount = clusterNodeCount;
-			MinFlushDelay = minFlushDelay;
-			PrepareAckCount = prepareAckCount;
-			CommitAckCount = commitAckCount;
-			PrepareTimeout = prepareTimeout;
-			CommitTimeout = commitTimeout;
+            DiscoverViaDns = discoverViaDns;
+            ClusterDns = clusterDns;
+            GossipSeeds = gossipSeeds;
 
-			UseSsl = useSsl;
-			SslTargetHost = sslTargetHost;
-			SslValidateServer = sslValidateServer;
+            ClusterNodeCount = clusterNodeCount;
+            MinFlushDelay = minFlushDelay;
+            PrepareAckCount = prepareAckCount;
+            CommitAckCount = commitAckCount;
+            PrepareTimeout = prepareTimeout;
+            CommitTimeout = commitTimeout;
 
-			StatsPeriod = statsPeriod;
-			StatsStorage = statsStorage;
+            UseSsl = useSsl;
+            SslTargetHost = sslTargetHost;
+            SslValidateServer = sslValidateServer;
 
-			AuthenticationProviderFactory = authenticationProviderFactory;
+            StatsPeriod = statsPeriod;
+            StatsStorage = statsStorage;
 
-			NodePriority = nodePriority;
-	        DisableScavengeMerging = disableScavengeMerging;
-	        AdminOnPublic = adminOnPublic;
-	        StatsOnPublic = statsOnPublic;
-	        GossipOnPublic = gossipOnPublic;
-	        GossipInterval = gossipInterval;
-	        GossipAllowedTimeDifference = gossipAllowedTimeDifference;
-	        GossipTimeout = gossipTimeout;
-		}
+            AuthenticationProviderFactory = authenticationProviderFactory;
 
-		public override string ToString()
-		{
-			return string.Format("InstanceId: {0}\n"
-								 + "InternalTcp: {1}\n"
-								 + "InternalSecureTcp: {2}\n"
-								 + "ExternalTcp: {3}\n"
-								 + "ExternalSecureTcp: {4}\n"
-								 + "InternalHttp: {5}\n"
-								 + "ExternalHttp: {6}\n"
-								 + "HttpPrefixes: {7}\n"
-								 + "EnableTrustedAuth: {8}\n"
-								 + "Certificate: {9}\n"
-								 + "WorkerThreads: {10}\n"
-								 + "DiscoverViaDns: {11}\n"
-								 + "ClusterDns: {12}\n"
-								 + "GossipSeeds: {13}\n"
-								 + "ClusterNodeCount: {14}\n"
-								 + "MinFlushDelay: {15}\n"
-								 + "PrepareAckCount: {16}\n"
-								 + "CommitAckCount: {17}\n"
-								 + "PrepareTimeout: {18}\n"
-								 + "CommitTimeout: {19}\n"
-								 + "UseSsl: {20}\n"
-								 + "SslTargetHost: {21}\n"
-								 + "SslValidateServer: {22}\n"
-								 + "StatsPeriod: {23}\n"
-								 + "StatsStorage: {24}\n"
-								 + "AuthenticationProviderFactory Type: {25}\n"
-								 + "NodePriority: {26}",
-								 NodeInfo.InstanceId,
-								 NodeInfo.InternalTcp, NodeInfo.InternalSecureTcp,
-								 NodeInfo.ExternalTcp, NodeInfo.ExternalSecureTcp,
-								 NodeInfo.InternalHttp, NodeInfo.ExternalHttp,
-								 string.Join(", ", HttpPrefixes),
-								 EnableTrustedAuth,
-								 Certificate == null ? "n/a" : Certificate.ToString(true),
-								 WorkerThreads, DiscoverViaDns, ClusterDns,
-								 string.Join(",", GossipSeeds.Select(x => x.ToString())),
-								 ClusterNodeCount, MinFlushDelay,
-								 PrepareAckCount, CommitAckCount, PrepareTimeout, CommitTimeout,
-								 UseSsl, SslTargetHost, SslValidateServer,
-								 StatsPeriod, StatsStorage, AuthenticationProviderFactory.GetType(),
-								 NodePriority);
-		}
-	}
+            NodePriority = nodePriority;
+            DisableScavengeMerging = disableScavengeMerging;
+            AdminOnPublic = adminOnPublic;
+            StatsOnPublic = statsOnPublic;
+            GossipOnPublic = gossipOnPublic;
+            GossipInterval = gossipInterval;
+            GossipAllowedTimeDifference = gossipAllowedTimeDifference;
+            GossipTimeout = gossipTimeout;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("InstanceId: {0}\n"
+                                 + "InternalTcp: {1}\n"
+                                 + "InternalSecureTcp: {2}\n"
+                                 + "ExternalTcp: {3}\n"
+                                 + "ExternalSecureTcp: {4}\n"
+                                 + "InternalHttp: {5}\n"
+                                 + "ExternalHttp: {6}\n"
+                                 + "HttpPrefixes: {7}\n"
+                                 + "EnableTrustedAuth: {8}\n"
+                                 + "Certificate: {9}\n"
+                                 + "WorkerThreads: {10}\n"
+                                 + "DiscoverViaDns: {11}\n"
+                                 + "ClusterDns: {12}\n"
+                                 + "GossipSeeds: {13}\n"
+                                 + "ClusterNodeCount: {14}\n"
+                                 + "MinFlushDelay: {15}\n"
+                                 + "PrepareAckCount: {16}\n"
+                                 + "CommitAckCount: {17}\n"
+                                 + "PrepareTimeout: {18}\n"
+                                 + "CommitTimeout: {19}\n"
+                                 + "UseSsl: {20}\n"
+                                 + "SslTargetHost: {21}\n"
+                                 + "SslValidateServer: {22}\n"
+                                 + "StatsPeriod: {23}\n"
+                                 + "StatsStorage: {24}\n"
+                                 + "AuthenticationProviderFactory Type: {25}\n"
+                                 + "NodePriority: {26}"
+                                 + "GossipInterval: {27}\n"
+                                 + "GossipAllowedTimeDifference: {28}\n"
+                                 + "GossipTimeout: {29}\n",
+                                 NodeInfo.InstanceId,
+                                 NodeInfo.InternalTcp, NodeInfo.InternalSecureTcp,
+                                 NodeInfo.ExternalTcp, NodeInfo.ExternalSecureTcp,
+                                 NodeInfo.InternalHttp, NodeInfo.ExternalHttp,
+                                 string.Join(", ", HttpPrefixes),
+                                 EnableTrustedAuth,
+                                 Certificate == null ? "n/a" : Certificate.ToString(true),
+                                 WorkerThreads, DiscoverViaDns, ClusterDns,
+                                 string.Join(",", GossipSeeds.Select(x => x.ToString())),
+                                 ClusterNodeCount, MinFlushDelay,
+                                 PrepareAckCount, CommitAckCount, PrepareTimeout, CommitTimeout,
+                                 UseSsl, SslTargetHost, SslValidateServer,
+                                 StatsPeriod, StatsStorage, AuthenticationProviderFactory.GetType(),
+                                 NodePriority, GossipInterval, GossipAllowedTimeDifference, GossipTimeout);
+        }
+    }
 }
