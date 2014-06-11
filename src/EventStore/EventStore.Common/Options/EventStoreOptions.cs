a2ad7c890bc45482ebdb8b0ffd1f0c4411b0607b
pgermishuys <pieter@pieterg.com>
2014-06-11 05:52:57 +0200

    Updated environment variable constant to be consistent with other sources description

--- src/EventStore/EventStore.Common/Options/EventStoreOptions.cs
+++ src/EventStore/EventStore.Common/Options/EventStoreOptions.cs
@@ -14,7 +14,7 @@ namespace EventStore.Common.Options
     public class EventStoreOptions
     {
         private const string DEFAULT_OPTION_SOURCE = "<DEFAULT>";
-        private const string FROM_ENVIRONMENT_VARIABLE = "environment variable";
+        private const string FROM_ENVIRONMENT_VARIABLE = "from environment variable";
         private const string FROM_COMMAND_LINE = "from commandline";
         private const string FROM_CONFIG_FILE = "from config";
         private static List<Tuple<string, OptionSource>> parsedOptions;
