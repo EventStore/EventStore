4235ff2caa8a254f461171ff2a4b4a437993b998
unknown <pieter@pieterg.com>
2014-07-04 15:47:59 +0200

    Migrated tests from json to yaml

--- src/EventStore/EventStore.Core.Tests/Common/EventStoreOptionsTests/when_parsing/with_arguments_and_config_files.cs
+++ src/EventStore/EventStore.Core.Tests/Common/EventStoreOptionsTests/when_parsing/with_arguments_and_config_files.cs
@@ -15,7 +15,7 @@ namespace EventStore.Core.Tests.Common.EventStoreOptionsTests.when_parsing
         [Test]
         public void should_use_the_argument_over_the_config_file_value()
         {
-            var args = new string[] { "-config", "TestConfigs/test_config.json", "-log", "~/customLogsDirectory" };
+            var args = new string[] { "-config", "TestConfigs/test_config.yaml", "-log", "~/customLogsDirectory" };
             var testArgs = EventStoreOptions.Parse<TestArgs>(args, Opts.EnvPrefix);
             Assert.AreEqual("~/customLogsDirectory", testArgs.Log);
         }
