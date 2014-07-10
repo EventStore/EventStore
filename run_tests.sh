802d4dd608d0d0400fbdcec3ec5ecc7233ca3a28
James Nugent <james.nugent@bathasu.com>
2014-07-10 17:52:51 +0100

    Update run_tests.sh with new paths
    Upshot of moving the build output is that this no longer requires
    configuration information to be able to run the tests following a build,
    the output directory is the same for all configurations.

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-while getopts "c:m:x:p" option
+while getopts "m:x:p" option
 do
     case $option in
-        c)
-            CONFIGURATION=$OPTARG
-            ;;
         m)
             MONOPATH=$OPTARG 
             ;;   
@@ -15,9 +12,8 @@ do
             RUNPROJECTIONS="TRUE"
             ;;
         ?)
-            echo "Usage: run_tests.sh [-c Configuration] [-x ExcludeCategories] [-m /path/to/mono] [-p]"
+            echo "Usage: run_tests.sh [-x ExcludeCategories] [-m /path/to/mono] [-p]"
             echo "Defaults:"
-            echo "   Configuration: release"
             echo "   Mono Path: /opt/mono"
             echo "   Exclude: None"
             echo "   projections: false"
@@ -26,15 +22,11 @@ do
     esac
 done
 
-if [[ $CONFIGURATION == "" ]]; then
-    CONFIGURATION="debug"
-fi
-
 if [[ $MONOPATH == "" ]]; then
     MONOPATH="/opt/mono"
 fi
 
-LD_LIBRARY_PATH=bin/eventstore/$CONFIGURATION/anycpu/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/eventstore.tests/$CONFIGURATION/anycpu/EventStore.Core.Tests.dll $EXCLUDE -xml=inter 
+LD_LIBRARY_PATH=bin/tests:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/tests/EventStore.Core.Tests.dll $EXCLUDE -xml=inter 
 rc=$?
 xsltproc tools/nunit-2.6.3/results.xslt inter
 rm inter
@@ -43,5 +35,5 @@ if [[ $rc != 0 ]] ; then
 fi
 
 if [[ $RUNPROJECTIONS == "TRUE" ]]; then
-    LD_LIBRARY_PATH=bin/eventstore/$CONFIGURATION/anycpu/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/eventstore.tests/$CONFIGURATION/anycpu/EventStore.Projections.Core.Tests.dll $EXCLUDE
+    LD_LIBRARY_PATH=bin/tests/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/tests/EventStore.Projections.Core.Tests.dll $EXCLUDE
 fi
