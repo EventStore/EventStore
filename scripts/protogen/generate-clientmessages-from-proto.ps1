452b928f14352f9fcecbe9933207e365e7fc8d1a
James Nugent <james@jen20.com>
2014-07-13 23:14:02 +0100

    Rewrite Protogen scripts for client API messages
    The protogen scripts were broken during the recent source location
    refactoring, this moves them into the top level scripts directory from the
    source directory.
    
    It also replaces the batch implementation with a slightly more robust
    Powershell implementation which can be safely run from outside the script
    location.

---
+++ scripts/protogen/generate-clientmessages-from-proto.ps1
@@ -0,0 +1,25 @@
+﻿# Setup and Configuration
+$baseDirectory = Resolve-Path (Join-Path $PSScriptRoot "..\..\")
+$srcDirectory = Join-Path $baseDirectory "src"
+$toolsDirectory = Join-Path $baseDirectory "tools"
+$protocPath = Join-Path $toolsDirectory (Join-Path "protoc" "protoc.exe")
+$protogenPath = Join-Path $toolsDirectory (Join-Path "ProtoGen" "protogen.exe")
+
+try {
+    Push-Location $baseDirectory
+
+    $protoPath = Join-Path $srcDirectory (Join-Path "Protos" "ClientAPI")
+    $clientProtoFile = Join-Path $protoPath "ClientMessageDtos.proto"
+    $tempDescriptorsFile = Join-Path $protoPath "ClientMessageDtos.TempDescriptors"
+
+    Start-Process -NoNewWindow -Wait -FilePath $protocPath -ArgumentList @("--descriptor_set_out=$tempDescriptorsFile", "--proto_path=$protoPath", "$clientProtoFile")
+    
+    $clientApiClassPath = Join-Path $srcDirectory (Join-Path "EventStore.ClientAPI" (Join-Path "Messages" "ClientMessage.cs"))
+    Start-Process -NoNewWindow -Wait -FilePath $protogenPath -ArgumentList @("-p:fixCase", "-i:$tempDescriptorsFile", "-ns:EventStore.ClientAPI.Messages", "-o:$clientApiClassPath", '-p:umbrella="ClientMessage"', '-p:umbrellaVisibility="internal"')
+
+    $serverClassPath = Join-Path $srcDirectory (Join-Path "EventStore.Core" (Join-Path "Messages" "TcpClientMessageDto.cs"))
+    Start-Process -NoNewWindow -Wait -FilePath $protogenPath -ArgumentList @("-p:fixCase", "-i:$tempDescriptorsFile", "-ns:EventStore.Core.Messages", "-o:$serverClassPath", '-p:umbrella="TcpClientMessageDto"', '-p:umbrellaVisibility="public"')
+} finally {
+    Remove-Item -Force $tempDescriptorsFile
+    Pop-Location
+}
---
+++ scripts/protogen/generate-clientmessages-from-proto.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR=$SCRIPTDIR/../../
+
+MONO_PATH=/opt/mono/bin/mono
+PROTOC_PATH=$BASEDIR/tools/protoc/protoc
+PROTOGEN_PATH=$BASEDIR/tools/ProtoGen/protogen.exe
+
+$PROTOC_PATH --descriptor_set_out=ClientMessageDtos.TempDescriptors --proto_path=$BASEDIR/src/Protos/ClientAPI $BASEDIR/src/Protos/ClientAPI/ClientMessageDtos.proto
+$MONO_PATH $PROTOGEN_PATH -p:fixCase -i:ClientMessageDtos.TempDescriptors -ns:EventStore.ClientAPI.Messages -o:$BASEDIR/src/EventStore.ClientAPI/Messages/ClientMessage.cs -p:umbrella="ClientMessage" -p:umbrellaVisibility="internal"
+$MONO_PATH $PROTOGEN_PATH -p:fixCase -i:ClientMessageDtos.TempDescriptors -ns:EventStore.Core.Messages -o:$BASEDIR/src/EventStore.Core/Messages/TcpClientMessageDto.cs -p:umbrella="TcpClientMessageDto" -p:umbrellaVisibility="public"
+rm -f ClientMessageDtos.TempDescriptors
