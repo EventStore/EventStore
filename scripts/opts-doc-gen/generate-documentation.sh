a27cfd721c10d685b977fdf0fee22a28614e585d
pgermishuys <pieter@pieterg.com>
2014-07-14 23:09:32 +0200

    spiked out the bash script to generate the documentation

--- scripts/opts-doc-gen/generate-documentation.sh
+++ scripts/opts-doc-gen/generate-documentation.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR=$SCRIPTDIR/../..
+
+MONO_PATH=/opt/mono/bin/mono
+DOCUMENTATIONOUTPUT_PATH=$BASEDIR/tools/documentation-generation/documentation.md
+DOCUMENTATIONGEN_PATH=$BASEDIR/tools/documentation-generation/EventStore.Documentation.exe
+
+EVENTSTORESINGLENODE_PATH=$BASEDIR/bin/singlenode
+EVENTSTORECLUSTERNODE_PATH=$BASEDIR/bin/clusternode
+
+$MONO_PATH $DOCUMENTATIONGEN_PATH -b:EVENTSTORESINGLENODE_PATH -b:EVENTSTORECLUSTERNODE_PATH -o:DOCUMENTATIONOUTPUT_PATH
