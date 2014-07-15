ed17a5fb53e836e41f26ac1dfba689659151d493
pgermishuys <pieter@pieterg.com>
2014-07-15 06:04:27 +0200

    Group the options by their argument groups

--- etc/EventStore.Documentation/DocumentGenerator.cs
+++ etc/EventStore.Documentation/DocumentGenerator.cs
@@ -31,21 +31,33 @@ namespace EventStore.Documentation
                         var optionConstructor = optionType.GetConstructor(new Type[] { });
                         var options = optionConstructor.Invoke(null);
                         var optionDocumentation = String.Format("###{0}{1}", options.GetType().Name, Environment.NewLine);
-                        optionDocumentation += String.Format("| Parameter | Environment *(all prefixed with EVENTSTORE_)* | Yaml | Description | Default |{0}", Environment.NewLine);
-                        optionDocumentation += String.Format("| --------- | --------------------------------------------- | ---- | ----------- | ------- |{0}", Environment.NewLine);
+                        optionDocumentation += String.Format("| Group  | Parameter | Environment *(all prefixed with EVENTSTORE_)* | Yaml | Description | Default |{0}", Environment.NewLine);
+                        optionDocumentation += String.Format("| ------ | --------- | --------------------------------------------- | ---- | ----------- | ------- |{0}", Environment.NewLine);
                         var properties = options.GetType().GetProperties();
                         var argumentsDefinition = new CommandLineArgumentsDefinition(optionType);
-                        foreach (var property in properties)
+                        var currentGroup = String.Empty;
+                        foreach (var property in properties.OrderBy(x=>x.Attr<ArgDescription>().Group))
                         {
                             var parameterRow = String.Empty;
+                            var groupColumn = "|";
+                            if (currentGroup != property.Attr<ArgDescription>().Group)
+                            {
+                                currentGroup = property.Attr<ArgDescription>().Group;
+                                groupColumn += "**" + currentGroup + "**";
+                            }
+
+                            parameterRow += groupColumn;
+
                             var parameterDefinition = argumentsDefinition.Arguments.First(x => ((PropertyInfo)x.Source).Name == property.Name);
                             var parameterUsageFormat = "-{0} <br/>";
                             var parameterUsage = String.Empty;
+
                             foreach (var alias in parameterDefinition.Aliases.Reverse())
                             {
                                 parameterUsage += String.Format(parameterUsageFormat, alias);
                                 parameterUsageFormat = "--{0}=VALUE";
                             }
+
                             parameterRow += String.Format("|{0}", parameterUsage);
                             parameterRow += String.Format("|{0}", EnvironmentVariableNameProvider.GetName("EVENTSTORE_", property.Name.ToUpper()));
                             parameterRow += String.Format("|{0}", property.Name);
