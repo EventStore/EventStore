9d06f77b3c082406bf6cf54c0f67fe0f33523728
Greg Young <gregoryyoung1@gmail.com>
2014-07-17 14:41:21 +0300

    bring in competing consumers on client api side + messaging protocols

--- src/EventStore.ClientAPI/Core/EventStoreConnectionLogicHandler.cs
+++ src/EventStore.ClientAPI/Core/EventStoreConnectionLogicHandler.cs
@@ -1,4 +1,31 @@
-﻿using System;
+﻿// Copyright (c) 2012, Event Store LLP
+// All rights reserved.
+//  
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//  
+// Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// Neither the name of the Event Store LLP nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+using System;
 using System.Diagnostics;
 using System.Net;
 using System.Threading;
@@ -55,6 +82,7 @@ namespace EventStore.ClientAPI.Core
 
             _queue.RegisterHandler<StartOperationMessage>(msg => StartOperation(msg.Operation, msg.MaxRetries, msg.Timeout));
             _queue.RegisterHandler<StartSubscriptionMessage>(StartSubscription);
+            _queue.RegisterHandler<StartPersistentSubscriptionMessage>(StartSubscription);
 
             _queue.RegisterHandler<EstablishTcpConnectionMessage>(msg => EstablishTcpConnection(msg.EndPoints));
             _queue.RegisterHandler<TcpConnectionEstablishedMessage>(msg => TcpConnectionEstablished(msg.Connection));
@@ -396,7 +424,33 @@ namespace EventStore.ClientAPI.Core
                     break;
                 case ConnectionState.Connecting:
                 case ConnectionState.Connected:
-                    var operation = new SubscriptionOperation(_settings.Log, msg.Source, msg.StreamId, msg.ResolveLinkTos, 
+                    var operation = new VolatileSubscriptionOperation(_settings.Log, msg.Source, msg.StreamId, msg.ResolveLinkTos, 
+                                                              msg.UserCredentials, msg.EventAppeared, msg.SubscriptionDropped, 
+                                                              _settings.VerboseLogging, () => _connection);
+                    LogDebug("StartSubscription {4} {0}, {1}, {2}, {3}.", operation.GetType().Name, operation, msg.MaxRetries, msg.Timeout, _state == ConnectionState.Connected ? "fire" : "enqueue");
+                    var subscription = new SubscriptionItem(operation, msg.MaxRetries, msg.Timeout);
+                    if (_state == ConnectionState.Connecting)
+                        _subscriptions.EnqueueSubscription(subscription);
+                    else
+                        _subscriptions.StartSubscription(subscription, _connection);
+                    break;
+                case ConnectionState.Closed:
+                    msg.Source.SetException(new ObjectDisposedException(_esConnection.ConnectionName));
+                    break;
+                default: throw new Exception(string.Format("Unknown state: {0}.", _state));
+            }
+        }
+
+        private void StartSubscription(StartPersistentSubscriptionMessage msg)
+        {
+            switch (_state)
+            {
+                case ConnectionState.Init:
+                    msg.Source.SetException(new InvalidOperationException(string.Format("EventStoreConnection '{0}' is not active.", _esConnection.ConnectionName)));
+                    break;
+                case ConnectionState.Connecting:
+                case ConnectionState.Connected:
+                    var operation = new PersistentSubscriptionOperation(_settings.Log, msg.Source, msg.SubscriptionId, msg.BufferSize, msg.StreamId, msg.ResolveLinkTos, 
                                                               msg.UserCredentials, msg.EventAppeared, msg.SubscriptionDropped, 
                                                               _settings.VerboseLogging, () => _connection);
                     LogDebug("StartSubscription {4} {0}, {1}, {2}, {3}.", operation.GetType().Name, operation, msg.MaxRetries, msg.Timeout, _state == ConnectionState.Connected ? "fire" : "enqueue");
--- src/EventStore.ClientAPI/Core/Messages.cs
+++ src/EventStore.ClientAPI/Core/Messages.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿// Copyright (c) 2012, Event Store LLP
+// All rights reserved.
+//  
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//  
+// Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// Neither the name of the Event Store LLP nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+
+using System;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using EventStore.ClientAPI.ClientOperations;
@@ -125,6 +153,59 @@ namespace EventStore.ClientAPI.Core
             SubscriptionDropped = subscriptionDropped;
             MaxRetries = maxRetries;
             Timeout = timeout;
+        }
+    }
+    
+    internal class StartPersistentSubscriptionMessage : Message
+    {
+        public readonly TaskCompletionSource<PersistentEventStoreSubscription> Source;
+
+        public readonly string SubscriptionId;
+        public readonly string StreamId;
+        public readonly bool ResolveLinkTos;
+        public readonly int BufferSize;
+        public readonly UserCredentials UserCredentials;
+        public readonly Action<PersistentEventStoreSubscription, ResolvedEvent> EventAppeared;
+        public readonly Action<PersistentEventStoreSubscription, SubscriptionDropReason, Exception> SubscriptionDropped;
+           
+        public readonly int MaxRetries;
+        public readonly TimeSpan Timeout;
+
+        public StartPersistentSubscriptionMessage(TaskCompletionSource<PersistentEventStoreSubscription> source, string subscriptionId, string streamId, bool resolveLinkTos, int bufferSize, UserCredentials userCredentials, Action<PersistentEventStoreSubscription, ResolvedEvent> eventAppeared, Action<PersistentEventStoreSubscription, SubscriptionDropReason, Exception> subscriptionDropped, int maxRetries, TimeSpan timeout)
+        {
+            Ensure.NotNull(source, "source");
+            Ensure.NotNull(eventAppeared, "eventAppeared");
+            Ensure.NotNullOrEmpty(subscriptionId, "subscriptionId");
+            Ensure.Nonnegative(bufferSize, "bufferSize");
+
+            SubscriptionId = subscriptionId;
+            BufferSize = bufferSize;
+            Source = source;
+            StreamId = streamId;
+            ResolveLinkTos = resolveLinkTos;
+            UserCredentials = userCredentials;
+            EventAppeared = eventAppeared;
+            SubscriptionDropped = subscriptionDropped;
+            MaxRetries = maxRetries;
+            Timeout = timeout;
+        }
+    }
+
+    internal class NotifyEventsProcessed : Message
+    {
+        public readonly Guid SubscriptionId;
+        public readonly int FreeSlots;
+        public readonly Guid[] ProcessedEventIds;
+
+        public NotifyEventsProcessed(Guid subscriptionId, int freeSlots, Guid[] processedEventIds)
+        {
+            Ensure.NotEmptyGuid(subscriptionId, "subscriptionId");
+            Ensure.Nonnegative(freeSlots, "freeSlots");
+            Ensure.NotNull(processedEventIds, "processedEventIds");
+
+            SubscriptionId = subscriptionId;
+            FreeSlots = freeSlots;
+            ProcessedEventIds = processedEventIds;
         }
     }
 
