a183a63b554a061b95f4ee6eb9816baa0a2b2b77
James Nugent <james@jen20.com>
2014-07-19 13:15:28 +0100

    Remove unused NuGet package script
    This has been replaced by package-clientapi.ps1 and is no longer required.

--- scripts/nuget-clientapi/pack-clientapi.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd %~dp0
-IF [%1]==[] (
-	echo "No version specified - use pack-clientapi.cmd <version>"
-	exit /b1
-)
-..\..\tools\nuget\nuget.exe pack -symbols -version %1 EventStore.Client.nuspec
