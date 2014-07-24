f2092fa22a6502296e6f81202dcb14e77c2e26d7
pgermishuys <pieter@pieterg.com>
2014-07-24 22:55:04 +0200

    1. Refactoring the event store arguments to support more flexible configuration parsing 2. Moved exception handling into EventStoreOptions.Parse

---
+++ src/EventStore.Common/Utils/IPAddressConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace EventStore.Common.Utils
+{
+    public class IPAddressConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            var valueAsString = value as string;
+            if (valueAsString != null)
+            {
+                return IPAddress.Parse(valueAsString);
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
---
+++ src/EventStore.Common/Utils/IPEndPointArrayConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace EventStore.Common.Utils
+{
+    public class IPEndPointArrayConverter : ArrayConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            var values = value as IEnumerable;
+            if (values != null)
+            {
+                var ipEndPointList = new List<IPEndPoint>();
+                foreach (var val in values)
+                {
+                    ipEndPointList.Add((IPEndPoint)new IPEndPointConverter().ConvertFrom(val));
+                }
+                return ipEndPointList.ToArray();
+            }
+            var valueAsString = value as string;
+            if (valueAsString != null)
+            {
+                var ipEndPointList = valueAsString.Split(new[] { "," }, StringSplitOptions.None).Select(x => new IPEndPointConverter().ConvertFrom(x));
+                return ipEndPointList.ToArray();
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
