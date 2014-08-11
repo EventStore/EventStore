a283e9a38c3a44e895b0f8d893db5edffe0e2d32
thefringeninja <joao.paul@braganca.name>
2014-08-11 16:33:09 +0200

    Initial project

--- src/EventStore.ClientAPI.Embedded/Properties/AssemblyInfo.cs
+++ src/EventStore.ClientAPI.Embedded/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("EventStore.ClientAPI")]
@@ -11,12 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+//NOTE: Do not change these, they will be modified during the build by TeamCity.
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 
-[assembly: InternalsVisibleTo("EventStore.Core.Tests")]
-[assembly: InternalsVisibleTo("EventStore.Projections.Core.Tests")]
-[assembly: InternalsVisibleTo("EventStore.ClientAPI.Embedded")]
-
 [assembly: ComVisible(false)]
-[assembly: Guid("0bffd734-18c5-4575-9321-324ed1a3bbfb")]
+[assembly: Guid("cc60985b-ddf1-4b09-9d21-12421389003a")]
