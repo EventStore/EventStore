213e989d8aa8e5410c2ac390e46745b524edf1fb
pgermishuys <pieter@pieterg.com>
2014-08-11 23:25:23 +0200

    started the refresh of the options to better support merging and displaying the option source

---
+++ src/EventStore.Common/Options/EnvironmentVariableProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventStore.Common.Options
+{
+    public class EnvironmentVariableProvider : IOptionSourceProvider
+    {
+        private List<OptionSource> _parsedOptions;
+        public OptionSource[] Parse<TOptions>(string environmentPrefix) where TOptions : class
+        {
+            _parsedOptions = new List<OptionSource>();
+            foreach (var property in typeof(TOptions).GetProperties())
+            {
+                var environmentVariableName = EnvironmentVariableNameProvider.GetName(environmentPrefix, property.Name);
+                var environmentVariableValue = Environment.GetEnvironmentVariable(environmentVariableName);
+                if (!String.IsNullOrEmpty(environmentVariableValue))
+                {
+                    _parsedOptions.Add(new OptionSource("Environment Variable", property.Name, environmentVariableValue));
+                }
+            }
+            return _parsedOptions.ToArray();
+
+        }
+        public OptionSource[] GetEffectiveOptions()
+        {
+            return _parsedOptions.ToArray() ?? new OptionSource[] { };
+        }
+    }
+
+    public class EnvironmentVariableNameProvider
+    {
+        public static string GetName(string environmentPrefix, string name)
+        {
+            var regex = new System.Text.RegularExpressions.Regex(@"(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z])(?=[A-Z])|(?<=[A-Za-z])(?=[^A-Za-z])");
+            var convertedName = regex.Replace(name, "_");
+            return environmentPrefix + convertedName.ToUpper();
+        }
+    }
+}
---
+++ src/EventStore.Common/Options/OptionsSourceMerger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventStore.Common.Options
+{
+    public static class OptionsSourceMerger
+    {
+        public static OptionSource[] SequentialMerge(params OptionSource[][] optionsSources)
+        {
+            var effectiveOptionsSource = new Dictionary<string, OptionSource>();
+            foreach (var optionSources in optionsSources.Where(x => x != null))
+            {
+                foreach (var optionSource in optionSources)
+                {
+                    if (effectiveOptionsSource.ContainsKey(optionSource.Name))
+                    {
+                        effectiveOptionsSource[optionSource.Name] = optionSource;
+                    }
+                    else
+                    {
+                        effectiveOptionsSource.Add(optionSource.Name, optionSource);
+                    }
+                }
+            }
+            return effectiveOptionsSource.Select(x => x.Value).ToArray();
+        }
+    }
+}
