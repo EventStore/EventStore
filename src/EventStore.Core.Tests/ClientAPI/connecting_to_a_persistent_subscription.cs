fdd547f35387f8e40c3d62d0938b2d6e2bdbbcb3
Greg Young <gregoryyoung1@gmail.com>
2014-08-13 12:08:48 -0700

    tests to verify authorization works

--- src/EventStore.Core.Tests/ClientAPI/connecting_to_a_persistent_subscription.cs
+++ src/EventStore.Core.Tests/ClientAPI/connecting_to_a_persistent_subscription.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Text;
 using EventStore.ClientAPI;
+using EventStore.ClientAPI.Exceptions;
+using EventStore.ClientAPI.SystemData;
 using NUnit.Framework;
 
 namespace EventStore.Core.Tests.ClientAPI
@@ -9,21 +10,17 @@ namespace EventStore.Core.Tests.ClientAPI
     public class connect_to_non_existing_persistent_subscription_with_permissions : SpecificationWithMiniNode
     {
         private EventStorePersistentSubscription _sub;
-        private SubscriptionDropReason _reason;
-        private Exception _ex;
         private Exception _caught;
 
         protected override void When()
         {
             try
             {
-                _sub = _conn.ConnectToPersistentSubscription("nonexisting",
-                    "foo",
+                _sub = _conn.ConnectToPersistentSubscription("foo",
+                    "nonexisting",
                     (sub, e) => Console.Write("appeared"),
                     (sub, reason, ex) =>
                     {
-                        _reason = reason;
-                        _ex = ex;
                     });
                 throw new Exception("should have thrown");
             }
@@ -43,6 +40,70 @@ namespace EventStore.Core.Tests.ClientAPI
         public void the_exception_is_an_argument_exception()
         {
             Assert.IsInstanceOf<ArgumentException>(_caught.InnerException);
+        }
+    }
+
+    [TestFixture, Category("LongRunning")]
+    public class connect_to_existing_persistent_subscription_with_permissions : SpecificationWithMiniNode
+    {
+        private EventStorePersistentSubscription _sub;
+        private SubscriptionDropReason _reason;
+        private Exception _ex;
+        private readonly string _stream = Guid.NewGuid().ToString();
+
+        protected override void When()
+        {
+            _conn.CreatePersistentSubscriptionAsync("agroupname", _stream , true).Wait();
+            _sub = _conn.ConnectToPersistentSubscription(_stream,
+                "agroupname",
+                (sub, e) => Console.Write("appeared"),
+                (sub, reason, ex) =>
+                {
+                    _reason = reason;
+                    _ex = ex;
+                });
+        }
+
+        [Test]
+        public void the_subscription_suceeds()
+        {
+            Assert.IsNotNull(_sub);
+        }
+    }
+
+    [TestFixture, Category("LongRunning")]
+    public class connect_to_existing_persistent_subscription_without_permissions : SpecificationWithMiniNode
+    {
+        private SubscriptionDropReason _reason;
+        private Exception _ex;
+        private readonly string _stream = "$" + Guid.NewGuid();
+
+        protected override void When()
+        {
+            _conn.CreatePersistentSubscriptionAsync(_stream, "agroupname", true,
+                new UserCredentials("admin", "changeit")).Wait();
+        }
+
+        [Test]
+        public void the_subscription_fails_to_connect()
+        {
+            try
+            {
+                _conn.ConnectToPersistentSubscription("agroupname", 
+                    _stream,
+                    (sub, e) => Console.Write("appeared"),
+                    (sub, reason, ex) =>
+                    {
+                        _reason = reason;
+                        _ex = ex;
+                    });
+                throw new Exception("should have thrown.");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<AggregateException>(ex);
+                Assert.IsInstanceOf<AccessDeniedException>(ex.InnerException);
+            }
         }
     }
 }
