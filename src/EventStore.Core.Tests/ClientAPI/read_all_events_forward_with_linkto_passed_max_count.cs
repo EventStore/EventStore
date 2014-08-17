9b743604fa6f2248663d8d4bf728f9dc45ccccec
Greg Young <gregoryyoung1@gmail.com>
2014-08-17 14:22:42 -0700

    add some tests around maxcount with linktos

---
+++ src/EventStore.Core.Tests/ClientAPI/read_all_events_forward_with_linkto_passed_max_count.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using EventStore.ClientAPI;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.ClientAPI
+{
+    [TestFixture]
+    public class read_all_events_forward_with_linkto_passed_max_count : SpecificationWithLinkToToMaxCountDeletedEvents
+    {
+        private StreamEventsSlice _read;
+
+        protected override void When()
+        {
+            _read = _conn.ReadStreamEventsForwardAsync(LinkedStreamName, 0, 1, true).Result;
+        }
+
+        [Test]
+        public void one_event_is_read()
+        {
+            Assert.AreEqual(1, _read.Events.Length);
+        }
+    }
+}
