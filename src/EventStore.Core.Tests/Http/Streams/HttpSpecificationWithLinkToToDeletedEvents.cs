0aa61f61b681f11e7f2d12ca75746a9a544b7205
Greg Young <gregoryyoung1@gmail.com>
2014-08-17 14:22:42 -0700

    add some tests around maxcount with linktos

---
+++ src/EventStore.Core.Tests/Http/Streams/HttpSpecificationWithLinkToToDeletedEvents.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Common;
+using EventStore.ClientAPI.SystemData;
+using EventStore.Core.Tests.ClientAPI.Helpers;
+
+namespace EventStore.Core.Tests.Http.Streams
+{
+    public abstract class HttpSpecificationWithLinkToToDeletedEvents : HttpBehaviorSpecification
+    {
+        protected string LinkedStreamName;
+        protected string DeletedStreamName;
+
+        protected override void Given()
+        {
+            var creds = new UserCredentials("admin", "changeit");
+            LinkedStreamName = Guid.NewGuid().ToString();
+            DeletedStreamName = Guid.NewGuid().ToString();
+            using (var conn = TestConnection.Create(_node.TcpEndPoint))
+            {
+                conn.ConnectAsync().Wait();
+                conn.AppendToStreamAsync(DeletedStreamName, ExpectedVersion.Any, creds,
+                    new EventData(Guid.NewGuid(), "testing", true, Encoding.UTF8.GetBytes("{'foo' : 4}"), new byte[0]))
+                    .Wait();
+                conn.AppendToStreamAsync(LinkedStreamName, ExpectedVersion.Any, creds,
+                    new EventData(Guid.NewGuid(), SystemEventTypes.LinkTo, false,
+                        Encoding.UTF8.GetBytes("0@" + DeletedStreamName), new byte[0])).Wait();
+                conn.DeleteStreamAsync(DeletedStreamName, ExpectedVersion.Any).Wait();
+            }
+        }
+    }
+}
