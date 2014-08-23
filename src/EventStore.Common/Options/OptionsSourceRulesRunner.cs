53696ffcdc191e54f87204afab152b1efe2058cb
pgermishuys <pieter@pieterg.com>
2014-08-23 22:27:57 +0200

    Added a test to ensure that we are correctly suggesting the incorrectly cased options

--- src/EventStore.Common/Options/OptionsSourceRulesRunner.cs
+++ src/EventStore.Common/Options/OptionsSourceRulesRunner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventStore.Common.Log;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +15,44 @@ namespace EventStore.Common.Options
         private static void UnknownOptionRules(string[] validOptions, OptionSource[] optionSources)
         {
             if (optionSources == null) return;
-            var possibleInvalidOptions = optionSources.Select(option => option.Name).Except(validOptions);
+            var possibleInvalidOptions = optionSources.Select(option => option.Name)
+                                                      .Except(validOptions);
             if (possibleInvalidOptions.Any())
             {
-                var message = "The following options are not valid." + Environment.NewLine;
-                message += String.Join(",", possibleInvalidOptions);
+                var invalidOptions = possibleInvalidOptions
+                                        .Select(invalidOption => optionSources.First(x => x.Name == invalidOption))
+                                        .ToArray();
+
+                var message = InformAboutUnknownOptions(validOptions, optionSources, invalidOptions);
+                message += InformAboutPossibleCasingIssues(validOptions, optionSources, invalidOptions);
+
                 throw new OptionException(message, String.Empty);
             }
+        }
+        private static string InformAboutUnknownOptions(string[] validOptions, OptionSource[] optionSources, OptionSource[] invalidOptions)
+        {
+            var message = "The following unknown options have been specified." + Environment.NewLine;
+            message += String.Join(", ", invalidOptions.Select(x => x.Name).ToArray()) + Environment.NewLine;
+            return message;
+        }
+        private static string InformAboutPossibleCasingIssues(string[] validOptions, OptionSource[] optionSources, OptionSource[] invalidOptions)
+        {
+            var possibleIncorrectlyCasedOptions = invalidOptions.Select(option => option.Name.ToLower())
+                                                        .Intersect(validOptions.Select(option => option.ToLower()));
+
+            if (possibleIncorrectlyCasedOptions.Any())
+            {
+                var incorrectlyCasedOptions = possibleIncorrectlyCasedOptions.Select(option =>
+                                                new
+                                                {
+                                                    Correct = optionSources.First(x => x.Name.ToLower() == option).Name,
+                                                    Incorrect = invalidOptions.First(x => x.Name.ToLower() == option).Name
+                                                });
+                string message = "The following options are incorrectly cased." + Environment.NewLine;
+                message += String.Join(", ", incorrectlyCasedOptions.Select(x => x.Incorrect + " should be " + x.Correct).ToArray()) + Environment.NewLine;
+                return message;
+            }
+            return String.Empty;
         }
     }
 }
---
+++ src/EventStore.Core.Tests/Common/EventStoreOptionsTests/when_parsing/with_incorrectly_cased_option_in_config.cs
@@ -0,0 +1,23 @@
+﻿using EventStore.Common.Options;
+using EventStore.Core.Util;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventStore.Core.Tests.Common.EventStoreOptionsTests.when_parsing
+{
+    [TestFixture]
+    public class with_incorrectly_cased_option_in_config
+    {
+        [Test]
+        public void should_inform_the_user_of_the_incorrectly_cased_option()
+        {
+            var args = new string[] { "-config", "TestConfigs/test_config_with_incorrectly_cased_option.yaml" };
+            var optionException = Assert.Throws<OptionException>(() => { EventStoreOptions.Parse<TestArgs>(args, Opts.EnvPrefix); });
+            Assert.That(optionException.Message.Contains("log should be Log"));
+            Assert.That(optionException.Message.Contains("runProjections should be RunProjections"));
+        }
+    }
+}
