33a24a4b2e8efc2eff87bbc51ca7aac21d90b266
Greg Young <gregoryyoung1@gmail.com>
2014-08-31 04:56:21 -0700

    remove ununsed class update docs

--- src/EventStore.ClientAPI/PersistentSubscriptionSettings.cs
+++ src/EventStore.ClientAPI/PersistentSubscriptionSettings.cs
@@ -1,13 +1,13 @@
 namespace EventStore.ClientAPI
 {
     /// <summary>
-    /// Represents the settings for a <see cref="PersistentSubscription"></see> you should
+    /// Represents the settings for a <see cref="PersistentEventStoreSubscription"></see> you should
     /// normally not use this class directly but instead use a <see cref="PersistentSubscriptionSettingsBuilder"></see>
     /// </summary>
     public class PersistentSubscriptionSettings
     {
         /// <summary>
-        /// Whether or not the <see cref="PersistentSubscription"></see> should resolve linkTo events to their linked events.
+        /// Whether or not the <see cref="PersistentEventStoreSubscription"></see> should resolve linkTo events to their linked events.
         /// </summary>
         public readonly bool ResolveLinkTos;
 
