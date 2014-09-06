96783466aa61e68aea7ea513d7862c03477c2933
Greg Young <gregoryyoung1@gmail.com>
2014-09-06 11:55:15 -0700

    add boundedqueue

---
+++ src/EventStore.Core/DataStructures/BoundedQueue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace EventStore.Core.DataStructures
+{
+    public class BoundedQueue<T> 
+    {
+        private readonly int _capacity;
+        private Queue<T> _queue;
+
+        public BoundedQueue(int capacity)
+        {
+            _queue = new Queue<T>(capacity);
+            _capacity = capacity;
+        }
+
+        public void Enqueue(T obj)
+        {
+            if (_queue.Count >= _capacity) Dequeue();
+            _queue.Enqueue(obj);
+        }
+
+        private T Dequeue()
+        {
+            return _queue.Dequeue();
+        }
+    }
+}
