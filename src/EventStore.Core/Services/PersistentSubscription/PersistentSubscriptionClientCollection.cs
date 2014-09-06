b370a9a82381301284e91122ec644a43aab8be28
Greg Young <gregoryyoung1@gmail.com>
2014-09-06 13:59:31 -0700

    move a bunch of logic out to client connection add feature of roundrobin dispatch to clientcollection

---
+++ src/EventStore.Core/Services/PersistentSubscription/PersistentSubscriptionClientCollection.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventStore.Core.Data;
+
+namespace EventStore.Core.Services.PersistentSubscription
+{
+    internal class PersistentSubscriptionClientCollection
+    {
+        //TODO this is likely faster with a list etc as the counts are very small
+        private readonly Dictionary<Guid, PersistentSubscriptionClient> _hash = new Dictionary<Guid, PersistentSubscriptionClient>();
+        private readonly Queue<PersistentSubscriptionClient> _queue = new Queue<PersistentSubscriptionClient>();
+
+
+        public int Count { get { return _hash.Count; } }
+
+        public void AddClient(PersistentSubscriptionClient client)
+        {
+            _hash.Add(client.CorrelationId, client);
+            _queue.Enqueue(client);
+        }
+
+        public bool PushMessageToClient(ResolvedEvent ev)
+        {
+            for (var i = 0; i < _queue.Count; i++)
+            {
+                var current = _queue.Dequeue();
+                try
+                {
+                    if (current.CanSend())
+                    {
+                        current.Push(ev);
+                        break;
+                    }
+                }
+                finally
+                {
+                    _queue.Enqueue(current);
+                }
+            }
+        }
+
+        public IEnumerable<ResolvedEvent> RemoveClientByConnectionId(Guid connectionId)
+        {
+            var clients = _hash.Values.Where(x => x.ConnectionId == connectionId).ToList();
+            return clients.SelectMany(client => RemoveClientByCorrelationId(client.CorrelationId, false));
+        }
+
+        public void ShutdownAll()
+        {
+            while (_queue.Count > 0)
+            {
+                var client = _queue.Dequeue();
+                RemoveClientByCorrelationId(client.CorrelationId, true);
+            }
+        }
+
+        public IEnumerable<ResolvedEvent> RemoveClientByCorrelationId(Guid correlationId, bool sendDropNotification)
+        {
+            PersistentSubscriptionClient client;
+            if (!_hash.TryGetValue(correlationId, out client)) return new ResolvedEvent[0];
+            _hash.Remove(client.CorrelationId);
+            for(var i=0;i<_queue.Count;i++)
+            {
+                var current = _queue.Dequeue();
+                if (current == client) break;
+                _queue.Enqueue(current);
+            }
+            if (sendDropNotification)
+            {
+                client.SendDropNotification();
+            }
+            return client.GetUnconfirmedEvents();
+        }
+
+        public IEnumerable<PersistentSubscriptionClient> GetAll()
+        {
+            return _hash.Values;
+        }
+
+        public void AcknowledgeMessagesProcessed(Guid correlationId, Guid[] processedEventIds)
+        {
+            PersistentSubscriptionClient client;
+            if (!_hash.TryGetValue(correlationId, out client)) return;
+            client.ConfirmProcessing(processedEventIds);
+        }
+
+        public void NotAcknowledgeMessagesProcessed(Guid correlationId, Guid[] processedEventIds)
+        {
+            PersistentSubscriptionClient client;
+            if (!_hash.TryGetValue(correlationId, out client)) return;
+            client.DenyProcessing(processedEventIds);
+        }
+    }
+}
