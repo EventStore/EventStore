7f85469f5136a8650e6b08c01f11cbfdf7fd515c
Greg Young <gregoryyoung1@gmail.com>
2014-10-02 17:11:35 -0700

    fix stats test

--- src/EventStore.Core.Tests/Http/PersistentSubscription/statistics.cs
+++ src/EventStore.Core.Tests/Http/PersistentSubscription/statistics.cs
@@ -285,7 +285,7 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
 
 
     [TestFixture, Category("LongRunning")]
-    public class subscription_stats_for_stream_have_right_data : SpecificationWithPersistentSubscriptionAndConnections
+    public class subscription_stats_for_stream_have_summary_data : SpecificationWithPersistentSubscriptionAndConnections
     {
         private readonly PersistentSubscriptionSettings _settings = PersistentSubscriptionSettingsBuilder.Create()
                                                     .DoNotResolveLinkTos()
@@ -331,15 +331,40 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
         }
 
         [Test]
-        public void the_event_stream_is_correct()
+        public void the_first_event_stream_is_correct()
         {
             Assert.AreEqual(_streamName, _json[0]["eventStreamId"].Value<string>());
         }
 
         [Test]
-        public void the_groupname_is_correct()
+        public void the_first_groupname_is_correct()
         {
             Assert.AreEqual(_groupName, _json[0]["groupName"].Value<string>());
+        }
+
+        [Test]
+        public void the_first_event_stream_detail_uri_is_correct()
+        {
+            Assert.AreEqual(string.Format("http://{0}/subscriptions/{1}/{2}", _node.HttpEndPoint, _streamName, _groupName), _json[0]["detailUri"].Value<string>());
+        }
+
+        [Test]
+        public void the_second_event_stream_detail_uri_is_correct()
+        {
+
+            Assert.AreEqual(string.Format("http://{0}/subscriptions/{1}/{2}", _node.HttpEndPoint, _streamName, "secondgroup"), _json[1]["detailUri"].Value<string>());
+        }
+
+        [Test]
+        public void the_first_parked_message_queue_uri_is_correct()
+        {
+            Assert.AreEqual(string.Format("http://{0}/streams/$persistentsubscription-{1}::{2}-parked", _node.HttpEndPoint, _streamName, _groupName), _json[0]["parkedMessageUri"].Value<string>());
+        }
+
+        [Test]
+        public void the_second_parked_message_queue_uri_is_correct()
+        {
+            Assert.AreEqual(string.Format("http://{0}/streams/$persistentsubscription-{1}::{2}-parked", _node.HttpEndPoint, _streamName, "secondgroup"), _json[1]["parkedMessageUri"].Value<string>());
         }
 
         [Test]
@@ -351,33 +376,7 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
         [Test]
         public void there_are_two_connections()
         {
-            Assert.AreEqual(2, _json[0]["connections"].Count());
-        }
-
-        [Test]
-        public void the_first_connection_has_endpoint()
-        {
-            Assert.IsNotNull(_json[0]["connections"][0]["from"]);
-        }
-
-
-        [Test]
-        public void the_second_connection_has_endpoint()
-        {
-            Assert.IsNotNull(_json[0]["connections"][1]["from"]);
-        }
-
-        [Test]
-        public void the_first_connection_has_user()
-        {
-            Assert.AreEqual("anonymous", _json[0]["connections"][0]["username"].Value<string>());
-        }
-
-
-        [Test]
-        public void the_second_connection_has_user()
-        {
-            Assert.AreEqual("admin", _json[0]["connections"][1]["username"].Value<string>());
+            Assert.AreEqual(2, _json[0]["connectionCount"].Value<int>());
         }
 
         [Test]
@@ -392,53 +391,11 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
             Assert.AreEqual("secondgroup", _json[1]["groupName"].Value<string>());
         }
 
-        [Test]
-        public void the_second_subscription_status_is_push()
-        {
-            Assert.AreEqual("Idle", _json[1]["status"].Value<string>());
-        }
 
         [Test]
         public void second_subscription_there_are_three_connections()
         {
-            Assert.AreEqual(3, _json[1]["connections"].Count());
-        }
-
-        [Test]
-        public void the_second_subscription_first_connection_has_endpoint()
-        {
-            Assert.IsNotNull(_json[1]["connections"][0]["from"]);
-        }
-
-
-        [Test]
-        public void the_second_subscription_second_connection_has_endpoint()
-        {
-            Assert.IsNotNull(_json[1]["connections"][1]["from"]);
-        }
-
-        [Test]
-        public void the_second_subscription_third_connection_has_endpoint()
-        {
-            Assert.IsNotNull(_json[1]["connections"][2]["from"]);
-        }
-        [Test]
-        public void the_second_subscription_first_connection_has_user()
-        {
-            Assert.AreEqual("anonymous", _json[1]["connections"][0]["username"].Value<string>());
-        }
-
-
-        [Test]
-        public void the_second_subscription_second_connection_has_user()
-        {
-            Assert.AreEqual("admin", _json[1]["connections"][1]["username"].Value<string>());
-        }
-
-        [Test]
-        public void the_second_subscription_third_connection_has_user()
-        {
-            Assert.AreEqual("admin", _json[1]["connections"][2]["username"].Value<string>());
+            Assert.AreEqual(3, _json[1]["connectionCount"].Value<int>());
         }
     }
 
