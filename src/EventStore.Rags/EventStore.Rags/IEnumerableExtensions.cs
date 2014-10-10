7842b3add3f7b0de8d8a0e8988ec8c8fbd66f720
Greg Young <gregoryyoung1@gmail.com>
2014-10-10 16:49:17 -0700

    add Dump

---
+++ src/EventStore.Rags/EventStore.Rags/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace EventStore.Rags
+{
+    public static class IEnumerableExtensions
+    {
+        public static string Dump(this IEnumerable<OptionSource> items)
+        {
+            var ret = new StringBuilder();
+            foreach (var item in items)
+            {
+                ret.AppendFormat("{0}:{1} = '{2}'", item.Source, item.Name, item.Value);
+            }
+            return ret.ToString();
+        }
+    }
+}
