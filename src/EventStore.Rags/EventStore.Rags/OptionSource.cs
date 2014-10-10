4de097ab40398d944e3c49a9e6dfb7f6bcb5167e
Greg Young <gregoryyoung1@gmail.com>
2014-10-10 16:42:06 -0700

    add option Exception and YAML

--- src/EventStore.Rags/EventStore.Rags/OptionSource.cs
+++ src/EventStore.Rags/EventStore.Rags/OptionSource.cs
@@ -1,7 +1,15 @@
 ﻿namespace EventStore.Rags
 {
-    public class OptionSource
+    public struct OptionSource
     {
-         
+        public string Source;
+        public string Name;
+        public string Value;
+        public OptionSource(string source, string name, string value)
+        {
+            this.Source = source;
+            this.Name = name;
+            this.Value = value;
+        }
     }
 }
