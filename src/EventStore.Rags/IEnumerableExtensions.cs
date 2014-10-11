5cc4e4f282706649cb7dc8e1879eaaf17a4d5127
Greg Young <gregoryyoung1@gmail.com>
2014-10-10 19:35:05 -0700

    simplify

--- src/EventStore.Rags/IEnumerableExtensions.cs
+++ src/EventStore.Rags/IEnumerableExtensions.cs
@@ -10,26 +10,11 @@ namespace EventStore.Rags
         public static T ApplyTo<T>(this IEnumerable<OptionSource> source) where T:class,new()
         {
             return OptionApplicator.Get<T>(source);
-        } 
+        }
 
-        public static IEnumerable<OptionSource> MergeOptions(this IEnumerable<IEnumerable<OptionSource>> sources, Func<OptionSource, OptionSource, bool> compare)
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> source)
         {
-            return MergeWithPreference(sources, x=> x.Name,compare);
-        } 
-
-        public static IEnumerable<T> MergeWithPreference<T>(this IEnumerable<IEnumerable<T>> sources, Func<T, string> keyFunc, Func<T, T, bool> compare)
-        {
-            var maps = sources.Select(s => s.ToDictionary(keyFunc)).ToList();
-            var current = new Dictionary<string, T>();
-            foreach (var kv in maps.SelectMany(m => m))
-            {
-                T val;
-                if (!current.TryGetValue(kv.Key, out val) || compare(val, kv.Value))
-                {
-                    current[kv.Key] = kv.Value;
-                }
-            }
-            return current.Values;
+            return source.SelectMany(x => x);
         }
     }
 }
--- src/EventStore.RagsPlayground/Program.cs
+++ src/EventStore.RagsPlayground/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using EventStore.Rags;
 
@@ -17,26 +18,41 @@ namespace RagsPlayground
             MyFirstCount = 5;
             MyFirstOption = "greg";
         }
+
+        public override string ToString()
+        {
+            return string.Format("MyFirstOption: {0}, MyFirstCount: {1}", MyFirstOption, MyFirstCount);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var sources = GetConfig(args);
-            var merged = sources.MergeOptions((existing, potential) => true); //last one in wins but can write your own function
-            merged.ApplyTo<SomeOptionType>();
+            var resolvedConfig = GetConfig(args)
+                .Flatten()
+                .ToLookup(x => x.Name)
+                .Select(ResolvePrecedence);
+                //.ApplyTo<SomeOptionType>();
+            foreach (var item in resolvedConfig)
+            {
+                Console.WriteLine("{0} : {1}={2}", item.Source, item.Name, item.Value);
+            }
 
-            //wouldnt this be nicer as
-            //GetConfig |> Merge |> ApplyTo<SomeOptionType>
+        }
+
+        private static OptionSource ResolvePrecedence(IGrouping<string, OptionSource> optionSources)
+        {
+            //go through and pick one based on your rules!
+            return optionSources.FirstOrDefault();
         }
 
         private static IEnumerable<IEnumerable<OptionSource>> GetConfig(string [] args)
         {
-            var commandline = CommandLine.Parse<SomeOptionType>(args);
+            //var commandline = CommandLine.Parse<SomeOptionType>(args);
             yield return
                 EnvironmentVariables.Parse<SomeOptionType>(x => NameTranslators.PrefixEnvironmentVariables(x, "ES"));
-            yield return
-                Yaml.FromFile(@"C:\shitbird.yaml", "foo"); //can get config file from commandline if it exists or help etc
+            //yield return
+            //    Yaml.FromFile(@"C:\shitbird.yaml", "foo"); //can get config file from commandline if it exists or help etc
             yield return
                 TypeDefaultOptions.Get<SomeOptionType>();
             yield return
