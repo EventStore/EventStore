4ce29f46a87c3712ae130fafef3fdcb50bc8ec20
Greg Young <gregoryyoung1@gmail.com>
2014-10-10 17:11:16 -0700

    fix directory structure

---
+++ src/EventStore.Rags/ArgDescriptionAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace EventStore.Rags
+{
+    public class ArgDescriptionAttribute
+    {
+        public string Description { get; private set; }
+        public string Group { get; private set; }
+
+        public ArgDescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
+        public ArgDescriptionAttribute(string description, string group)
+        {
+            Description = description;
+            Group = @group;
+        }
+    }
+}
--- src/EventStore.RagsPlayground/Program.cs
+++ src/EventStore.RagsPlayground/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventStore.Rags;
 
 namespace RagsPlayground
 {
+    class SomeOptions
+    {
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            //Read Environment Variables
+            EnvironmentVariables.Parse<>()
         }
     }
 }
