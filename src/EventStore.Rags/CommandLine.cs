9f12729098cdefebc563dac883db03f645e0569c
Greg Young <gregoryyoung1@gmail.com>
2014-10-10 18:27:29 -0700

    add command line and start playing to figure out where powerargs is doing stuff.

---
+++ src/EventStore.Rags/CommandLine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PowerArgs;
+
+namespace EventStore.Rags
+{
+    public class CommandLine
+    {
+        private List<OptionSource> _parsedOptions;
+        public static IEnumerable<OptionSource> Parse<TOptions>(string[] args) where TOptions : class
+        {
+            //TODO GFY drop lower here
+            //From poking around we actually want to use argParser here not all of powerargs, it needs a definition and a context
+            var ret = new List<OptionSource>();
+            var definition = new CommandLineArgumentsDefinition(typeof(TOptions));
+            var options = (TOptions)Args.Parse(definition, args).Value;
+            var changedOptions = definition.Arguments.Where(x => x.RevivedValue != null).ToList();
+            foreach (var changedOption in changedOptions)
+            {
+                var optionName = ((System.Reflection.PropertyInfo)changedOption.Source).Name;
+                ret.Add(new OptionSource("Command Line", optionName, changedOption.Source));
+            }
+            return ret;
+        }
+    }
+}
