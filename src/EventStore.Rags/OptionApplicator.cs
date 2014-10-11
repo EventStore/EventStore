6b4f717d753107b6621453570c2edd761452b562
pgermishuys <pieter@pieterg.com>
2014-10-11 12:44:44 +0200

    taking a stab at implementing the Apply

--- src/EventStore.Rags/OptionApplicator.cs
+++ src/EventStore.Rags/OptionApplicator.cs
@@ -1,12 +1,40 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EventStore.Rags
 {
     public class OptionApplicator
     {
-        public static T Get<T>(IEnumerable<OptionSource> source) where T:class,new()
+        public static T Get<T>(IEnumerable<OptionSource> source) where T : class,new()
         {
-            return new T();
+            var revived = new T();
+            var properties = revived.GetType().GetProperties();
+            foreach (var option in source)
+            {
+                var property = properties.FirstOrDefault(x => x.Name == option.Name);
+                if (property == null) continue;
+                if (option.IsTyped)
+                {
+                    property.SetValue(revived, option.Value, null);
+                }
+                else if (option.Value != null)
+                {
+                    object revivedValue = null;
+                    //Let's make use of the ArgRevivers to do the heavy lifting of parsing the type, 
+                    //so if it's an array, join it with a comma
+                    if (option.Value.GetType().IsArray)
+                    {
+                        var commaJoined = string.Join(",", ((string[])option.Value));
+                        revivedValue = ArgRevivers.Revive(property.PropertyType, option.Name, commaJoined);
+                    }
+                    else
+                    {
+                        revivedValue = ArgRevivers.Revive(property.PropertyType, option.Name, option.Value.ToString());
+                    }
+                    property.SetValue(revived, revivedValue, null);
+                }
+            }
+            return revived;
         }
     }
 }
