5f9b9154c537440c67fb1a5d0f8085cc7a218f40
San <sean.farrow@seanfarrow.co.uk>
2014-11-18 13:35:16 +0000

    Finish the user enable/disable methods and add the ability to delete a user.

--- src/EventStore.ClientAPI/UsersClient.cs
+++ src/EventStore.ClientAPI/UsersClient.cs
@@ -20,14 +20,113 @@ namespace EventStore.ClientAPI
             _client = new HttpAsyncClient(log);
         }
 
-        internal Task Enable(IPEndPoint _httpEndPoint, string login, UserCredentials userCredentials)
+public Task Enable(IPEndPoint endPoint, string login, UserCredentials userCredentials)
         {
-            throw new NotImplementedException();
+            return SendPost(endPoint.ToHttpUrl("/users/{login}/command/enable", login), string.Empty, userCredentials, HttpStatusCode.OK);
+    }
+
+public  Task Disable(IPEndPoint endPoint, string login, UserCredentials userCredentials)
+        {
+            return SendPost(endPoint.ToHttpUrl("/users/{login}/command/disable", login), string.Empty, userCredentials, HttpStatusCode.OK);
         }
 
-        internal Task Disable(IPEndPoint _httpEndPoint, string login, UserCredentials userCredentials)
-        {
-            throw new NotImplementedException();
-        }
+        public Task Delete(IPEndPoint endPoint, string login, UserCredentials userCredentials)
+{
+    return SendDelete(endPoint.ToHttpUrl("/users/{login}", login), userCredentials, HttpStatusCode.OK);
+}
+
+        private Task<string> SendGet(string url, UserCredentials userCredentials, int expectedCode)
+{
+    var source = new TaskCompletionSource<string>();
+    _client.Get(url,
+                userCredentials,
+                _operationTimeout,
+                response =>
+                {
+                    if (response.HttpStatusCode == expectedCode)
+                        source.SetResult(response.Body);
+                    else
+                        source.SetException(new UserCommandFailedException(
+                                                    string.Format("Server returned {0} ({1}) for GET on {2}",
+                                                                  response.HttpStatusCode,
+                                                                  response.StatusDescription,
+                                                                  url)));
+                },
+                source.SetException);
+
+    return source.Task;
+}
+
+private Task<string> SendDelete(string url, UserCredentials userCredentials, int expectedCode)
+{
+    var source = new TaskCompletionSource<string>();
+    _client.Delete(url,
+                   userCredentials,
+                   _operationTimeout,
+                   response =>
+                   {
+                       if (response.HttpStatusCode == expectedCode)
+                           source.SetResult(response.Body);
+                       else
+                           source.SetException(new UserCommandFailedException(
+                                                       string.Format("Server returned {0} ({1}) for DELETE on {2}",
+                                                                     response.HttpStatusCode,
+                                                                     response.StatusDescription,
+                                                                     url)));
+                   },
+                   source.SetException);
+
+    return source.Task;
+}
+
+private Task SendPut(string url, string content, UserCredentials userCredentials, int expectedCode)
+{
+    var source = new TaskCompletionSource<object>();
+    _client.Put(url,
+                content,
+                "application/json",
+                userCredentials,
+                _operationTimeout,
+                response =>
+                {
+                    if (response.HttpStatusCode == expectedCode)
+                        source.SetResult(null);
+                    else
+                        source.SetException(new UserCommandFailedException(
+                                                    string.Format("Server returned {0} ({1}) for PUT on {2}",
+                                                                  response.HttpStatusCode,
+                                                                  response.StatusDescription,
+                                                                  url)));
+                },
+                source.SetException);
+
+    return source.Task;
+}
+
+private Task SendPost(string url, string content, UserCredentials userCredentials, int expectedCode)
+{
+    var source = new TaskCompletionSource<object>();
+    _client.Post(url,
+                 content,
+                 "application/json",
+                 _operationTimeout,
+                 userCredentials,
+                 response =>
+                 {
+                     if (response.HttpStatusCode == expectedCode)
+                         source.SetResult(null);
+                     else if (response.HttpStatusCode == 409)
+                         source.SetException(new UserCommandConflictException(response.StatusDescription));
+                     else
+                         source.SetException(new UserCommandFailedException(
+                                                     string.Format("Server returned {0} ({1}) for POST on {2}",
+                                                                   response.HttpStatusCode,
+                                                                   response.StatusDescription,
+                                                                   url)));
+                 },
+                 source.SetException);
+
+    return source.Task;
+}
     }
 }
