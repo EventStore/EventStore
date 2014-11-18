d6bf600b0c8895b7a48ab5ba3eb3db8137a19e65
San <sean.farrow@seanfarrow.co.uk>
2014-11-18 01:44:30 +0000

    Add the UsersManager and UsersClient classes.

---
+++ src/EventStore.ClientAPI/UsersClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using EventStore.ClientAPI.Exceptions;
+using EventStore.ClientAPI.SystemData;
+using EventStore.ClientAPI.Transport.Http;
+using HttpStatusCode = EventStore.ClientAPI.Transport.Http.HttpStatusCode;
+
+namespace EventStore.ClientAPI
+{
+    internal class UsersClient
+    {
+        private readonly HttpAsyncClient _client;
+
+        private readonly TimeSpan _operationTimeout;
+
+        public UsersClient(ILogger log, TimeSpan operationTimeout)
+        {
+            _operationTimeout = operationTimeout;
+            _client = new HttpAsyncClient(log);
+        }
+    }
+}
