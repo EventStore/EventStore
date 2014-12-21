b5d005d86538fe0cf01da859b2b871427844785e
Greg Young <gregoryyoung1@gmail.com>
2014-12-20 16:45:13 -0800

    fix bug in change password. add tests

---
+++ src/EventStore.Core.Tests/ClientAPI/UserManagement/reset_change_password.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EventStore.ClientAPI.SystemData;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.ClientAPI.UserManagement
+{
+    public class reset_password : TestWithUser
+    {
+        [Test]
+        public void can_reset_password()
+        {
+            _manager.ResetPasswordAsync(_username, "foo", new UserCredentials("admin", "changeit")).Wait();
+            Assert.Throws<AggregateException>(
+                () => _manager.ChangePasswordAsync(_username, "password", "foobar", new UserCredentials(_username, "password")).Wait()
+            );
+        }
+    }
+
+    public class change_password : TestWithUser
+    {
+        [Test]
+        public void can_change_password()
+        {
+            _manager.ChangePasswordAsync(_username, "password", "fubar", new UserCredentials(_username, "password")).Wait();
+            Assert.Throws<AggregateException>(
+                () => _manager.ChangePasswordAsync(_username, "password", "foobar", new UserCredentials(_username, "password")).Wait()
+            );
+        }
+    }
+
+}
