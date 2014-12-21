a8cd3f98bfe22a3bc4e18cc0213b0b514b4a941f
Greg Young <gregoryyoung1@gmail.com>
2014-12-20 16:31:19 -0800

    add test for enable/disable

--- src/EventStore.Core.Tests/ClientAPI/UserManagement/enable_disable_user.cs
+++ src/EventStore.Core.Tests/ClientAPI/UserManagement/enable_disable_user.cs
@@ -1,11 +1,19 @@
-﻿namespace EventStore.Core.Tests.ClientAPI.UserManagement
+﻿using System;
+using EventStore.ClientAPI.SystemData;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.ClientAPI.UserManagement
 {
     public class enable_disable_user : TestWithUser
     {
-         
-    }
-
-    public class TestWithUser
-    {
+        [Test]
+        public void can_enable_disable_user()
+        {
+            _manager.DisableAsync(_username, new UserCredentials("admin", "changeit")).Wait();
+            Assert.Throws<AggregateException>(
+                () => _manager.DisableAsync("foo", new UserCredentials(_username, "password")).Wait());
+            _manager.EnableAsync(_username, new UserCredentials("admin", "changeit")).Wait();
+            var c = _manager.GetCurrentUserAsync(new UserCredentials(_username, "password")).Result;
+        }
     }
 }
