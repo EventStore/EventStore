6d608fd2af6d3757a0a92c48de48a0fe3c065ba9
Greg Young <gregoryyoung1@gmail.com>
2014-12-20 16:08:15 -0800

    Move to DTO + a test list all

---
+++ src/EventStore.Core.Tests/ClientAPI/UserManagement/list_users.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Common.Log;
+using EventStore.ClientAPI.SystemData;
+using EventStore.ClientAPI.UserManagement;
+using EventStore.Core.Tests.ClientAPI.Helpers;
+using EventStore.Core.Tests.Helpers;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.ClientAPI.UserManagement
+{
+    public class list_users: SpecificationWithDirectoryPerTestFixture
+    {
+        private MiniNode _node;
+        private UsersManager _manager;
+
+        [TestFixtureSetUp]
+        public override void TestFixtureSetUp()
+        {
+            base.TestFixtureSetUp();
+            _node = new MiniNode(PathName);
+            _node.Start();
+            _manager = new UsersManager(new NoopLogger(), _node.HttpEndPoint, TimeSpan.FromSeconds(5));
+        }
+
+        [TestFixtureTearDown]
+        public override void TestFixtureTearDown()
+        {
+            _node.Shutdown();
+            base.TestFixtureTearDown();
+        }
+
+
+        protected virtual IEventStoreConnection BuildConnection(MiniNode node)
+        {
+            return TestConnection.Create(node.TcpEndPoint);
+        }
+
+        [Test]
+        public void list_all_users_works()
+        {
+            //TODO GFY THIS 404s on node without projections running now.
+            //var x = _manager.ListAllAsync(new UserCredentials("admin", "changeit")).Result;
+            //Assert.AreEqual(1, x.Count);
+        } 
+    }
+}
