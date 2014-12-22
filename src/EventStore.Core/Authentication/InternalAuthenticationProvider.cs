6e91be7ec53dce058c6ad8b4f7779d5ce837fba1
Greg Young <gregoryyoung1@gmail.com>
2014-12-22 14:26:35 -0800

    redo lost commit in reverted PR

--- src/EventStore.Core/Authentication/InternalAuthenticationProvider.cs
+++ src/EventStore.Core/Authentication/InternalAuthenticationProvider.cs
@@ -43,12 +43,14 @@ namespace EventStore.Core.Authentication
         {
             try
             {
-                if (completed.Result == ReadStreamResult.StreamDeleted)
+                if (completed.Result == ReadStreamResult.StreamDeleted || 
+                    completed.Result == ReadStreamResult.NoStream ||
+                    completed.Result == ReadStreamResult.AccessDenied)
                 {
                     authenticationRequest.Unauthorized();
                     return;
                 }
-                if (completed.Result != ReadStreamResult.Success)
+                if (completed.Result == ReadStreamResult.Error)
                 {
                     authenticationRequest.NotReady();
                     return;
