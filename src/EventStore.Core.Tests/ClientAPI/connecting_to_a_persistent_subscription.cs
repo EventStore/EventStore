1ead0e884dc740600420d866440856c5541076e5
Greg Young <gregoryyoung1@gmail.com>
2014-12-25 06:53:46 -0800

    add test for connecting to a persistent subscription on non-existing stream

--- src/EventStore.Core.Tests/ClientAPI/connecting_to_a_persistent_subscription.cs
+++ src/EventStore.Core.Tests/ClientAPI/connecting_to_a_persistent_subscription.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.Threading;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.Exceptions;
-using EventStore.ClientAPI.SystemData;
 using NUnit.Framework;
 
 namespace EventStore.Core.Tests.ClientAPI
@@ -99,6 +98,56 @@ namespace EventStore.Core.Tests.ClientAPI
                 Assert.IsInstanceOf<AggregateException>(ex);
                 Assert.IsInstanceOf<AccessDeniedException>(ex.InnerException);
             }
+        }
+    }
+
+    [TestFixture, Category("LongRunning")]
+    public class connect_to_existing_persistent_subscription_with_start_from_beginning_and_no_stream : SpecificationWithMiniNode
+    {
+        private readonly string _stream = "$" + Guid.NewGuid();
+        private readonly PersistentSubscriptionSettings _settings = PersistentSubscriptionSettings.Create()
+                                                                .DoNotResolveLinkTos()
+                                                                .StartFromBeginning();
+
+        private readonly AutoResetEvent _resetEvent = new AutoResetEvent(false);
+        private ResolvedEvent _firstEvent;
+        private readonly Guid _id = Guid.NewGuid();
+        private bool _set = false;
+
+        private const string _group = "startinbeginning1";
+
+        protected override void Given()
+        {
+            _conn.CreatePersistentSubscriptionAsync(_stream, _group, _settings,
+                DefaultData.AdminCredentials).Wait();
+            _conn.ConnectToPersistentSubscription(_group,
+             _stream,
+             HandleEvent,
+             (sub, reason, ex) => { },
+             DefaultData.AdminCredentials);
+
+        }
+
+        protected override void When()
+        {
+            _conn.AppendToStreamAsync(_stream, ExpectedVersion.Any, DefaultData.AdminCredentials,
+                new EventData(_id, "test", true, Encoding.UTF8.GetBytes("{'foo' : 'bar'}"), new byte[0])).Wait();            
+        }
+
+        private void HandleEvent(EventStorePersistentSubscription sub, ResolvedEvent resolvedEvent)
+        {
+            if (_set) return;
+            _set = true;
+            _firstEvent = resolvedEvent;
+            _resetEvent.Set();
+        }
+
+        [Test]
+        public void the_subscription_gets_event_zero_as_its_first_event()
+        {
+            Assert.IsTrue(_resetEvent.WaitOne(TimeSpan.FromSeconds(10)));
+            Assert.AreEqual(0, _firstEvent.Event.EventNumber);
+            Assert.AreEqual(_id, _firstEvent.Event.EventId);
         }
     }
 
