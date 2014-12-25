ac8ca083f26cbf002a7fe2c7835b74a7f6822a3b
Greg Young <gregoryyoung1@gmail.com>
2014-12-25 03:43:41 -0800

    add test for exception in autoack mode

--- src/EventStore.Core.Tests/ClientAPI/connecting_to_a_persistent_subscription.cs
+++ src/EventStore.Core.Tests/ClientAPI/connecting_to_a_persistent_subscription.cs
@@ -333,6 +333,62 @@ namespace EventStore.Core.Tests.ClientAPI
         }
     }
 
+    [TestFixture, Category("LongRunning")]
+    public class a_nak_in_subscription_handler_in_autoack_mode_drops_the_subscription : SpecificationWithMiniNode
+    {
+        private readonly string _stream = "$" + Guid.NewGuid();
+
+        private readonly PersistentSubscriptionSettings _settings = PersistentSubscriptionSettings.Create()
+            .DoNotResolveLinkTos()
+            .StartFromBeginning();
+
+        private readonly ManualResetEvent _resetEvent = new ManualResetEvent(false);
+        private Exception _exception;
+        private SubscriptionDropReason _reason;
+
+        private const string _group = "naktest";
+
+        protected override void Given()
+        {
+            _conn.CreatePersistentSubscriptionAsync(_stream, _group, _settings,
+                DefaultData.AdminCredentials).Wait();
+            _conn.ConnectToPersistentSubscription(_group,
+                _stream,
+                HandleEvent,
+                Dropped,
+                DefaultData.AdminCredentials);
+
+        }
+
+        private void Dropped(EventStorePersistentSubscription sub, SubscriptionDropReason reason, Exception exception)
+        {
+            _exception = exception;
+            _reason = reason;
+            _resetEvent.Set();
+        }
+
+        protected override void When()
+        {
+            _conn.AppendToStreamAsync(_stream, ExpectedVersion.Any, DefaultData.AdminCredentials,
+                new EventData(Guid.NewGuid(), "test", true, Encoding.UTF8.GetBytes("{'foo' : 'bar'}"), new byte[0])).Wait();
+
+        }
+
+        private static void HandleEvent(EventStorePersistentSubscription sub, ResolvedEvent resolvedEvent)
+        {
+            throw new Exception("test");
+        }
+
+        [Test]
+        public void the_subscription_gets_dropped()
+        {
+            Assert.IsTrue(_resetEvent.WaitOne(TimeSpan.FromSeconds(5)));
+            Assert.AreEqual(SubscriptionDropReason.EventHandlerException, _reason);
+            Assert.AreEqual(typeof(Exception), _exception.GetType());
+            Assert.AreEqual("test", _exception.Message);
+        }
+
+    }
 
 
     [TestFixture, Category("LongRunning")]
