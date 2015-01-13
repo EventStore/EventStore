fb5fa2d167ca560b55db0063a74b43d6ae6857f6
Greg Young <gregoryyoung1@gmail.com>
2015-01-12 21:09:19 -0800

    add test for $TB

--- src/EventStore.Core.Tests/TransactionLog/Scavenging/when_having_stream_with_max_age_specified.cs
+++ src/EventStore.Core.Tests/TransactionLog/Scavenging/when_having_stream_with_max_age_specified.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using EventStore.Core.Data;
-using EventStore.Core.Services.Storage.ReaderIndex;
 using EventStore.Core.Tests.TransactionLog.Scavenging.Helpers;
 using EventStore.Core.TransactionLog.LogRecords;
 using NUnit.Framework;
@@ -38,6 +37,48 @@ namespace EventStore.Core.Tests.TransactionLog.Scavenging
             return new[]
             {
                 dbResult.Recs[0].Where((x, i) => new[] {0, 1, 11, 12, 13, 14}.Contains(i)).ToArray()
+            };
+        }
+
+        [Test]
+        public void expired_prepares_are_scavenged()
+        {
+            CheckRecords();
+        }
+    }
+
+    [TestFixture]
+    public class when_having_stream_with_truncate_before_specified : ScavengeTestScenario
+    {
+        protected override DbResult CreateDb(TFChunkDbCreationHelper dbCreator)
+        {
+            return dbCreator
+                    .Chunk(Rec.Prepare(0, "$$bla", metadata: new StreamMetadata(null, null, 0, null, null)),
+                           Rec.Commit(0, "$$bla"),
+                           Rec.Prepare(3, "bla"),
+                           Rec.Commit(3, "bla"),
+                           Rec.Prepare(1, "bla"),
+                           Rec.Prepare(1, "bla"),
+                           Rec.Prepare(1, "bla"),
+                           Rec.Prepare(1, "bla"),
+                           Rec.Prepare(1, "bla"),
+                           Rec.Commit(1, "bla"),
+                           Rec.Prepare(2, "bla"),
+                           Rec.Prepare(2, "bla"),
+                           Rec.Prepare(2, "bla"),
+                           Rec.Prepare(2, "bla"),
+                           Rec.Commit(2, "bla"),
+                           Rec.Prepare(4, "$$bla", metadata: new StreamMetadata(null, null, 10, null, null)),
+                           Rec.Commit(4, "$$bla")                           )
+                    .CompleteLastChunk()
+                    .CreateDb();
+        }
+
+        protected override LogRecord[][] KeptRecords(DbResult dbResult)
+        {
+            return new[]
+            {
+                dbResult.Recs[0].Where((x, i) => new[] {0, 1, 15, 16}.Contains(i)).ToArray()
             };
         }
 
