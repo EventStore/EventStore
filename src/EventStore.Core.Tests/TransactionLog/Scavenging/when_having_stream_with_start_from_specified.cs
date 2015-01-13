ebe81b327fcfbe84493724db31bd1cb429ea646c
Greg Young <gregoryyoung1@gmail.com>
2015-01-12 21:11:58 -0800

    forgot to save all

--- src/EventStore.Core.Tests/TransactionLog/Scavenging/when_having_stream_with_start_from_specified.cs
+++ src/EventStore.Core.Tests/TransactionLog/Scavenging/when_having_stream_with_start_from_specified.cs
@@ -45,46 +45,4 @@ namespace EventStore.Core.Tests.TransactionLog.Scavenging
             CheckRecords();
         }
     }
-
-    [TestFixture]
-    public class when_having_stream_with_truncatebefore_specified_after : ScavengeTestScenario
-    {
-        protected override DbResult CreateDb(TFChunkDbCreationHelper dbCreator)
-        {
-            return dbCreator
-                    .Chunk(Rec.Prepare(1, "bla"), // event 0
-                           Rec.Commit(1, "bla"),
-                           Rec.Prepare(2, "bla"), // event 1
-                           Rec.Prepare(2, "bla"), // event 2
-                           Rec.Prepare(2, "bla"), // event 3
-                           Rec.Prepare(2, "bla"), // event 4
-                           Rec.Prepare(2, "bla"), // event 5
-                           Rec.Commit(2, "bla"),
-                           Rec.Prepare(3, "bla"), // event 6
-                           Rec.Prepare(3, "bla"), // event 7
-                           Rec.Prepare(3, "bla"), // event 8
-                           Rec.Prepare(3, "bla"), // event 9
-                           Rec.Commit(3, "bla"),
-                           Rec.Prepare(0, "$$bla", metadata: new StreamMetadata(truncateBefore: 7)),
-                           Rec.Commit(0, "$$bla")
-                           )
-                    .CompleteLastChunk()
-                    .CreateDb();
-        }
-
-        protected override LogRecord[][] KeptRecords(DbResult dbResult)
-        {
-            return new[]
-            {
-                dbResult.Recs[0].Where((x, i) => new [] {9, 10, 11, 12, 13, 14}.Contains(i)).ToArray()
-            };
-        }
-
-        [Test]
-        public void expired_prepares_are_scavenged()
-        {
-            CheckRecords();
-        }
-    }
-
 }
