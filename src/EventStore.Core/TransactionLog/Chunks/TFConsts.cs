3bd92f46476ae69b9cbc6641c24b5e45e4828a19
Greg Young <gregoryyoung1@gmail.com>
2015-01-15 07:43:03 -0800

    move back to 256

--- src/EventStore.Core/TransactionLog/Chunks/TFConsts.cs
+++ src/EventStore.Core/TransactionLog/Chunks/TFConsts.cs
@@ -8,7 +8,7 @@ namespace EventStore.Core.TransactionLog.Chunks
 
         public const int MidpointsDepth = 10;
 
-        public const int ChunkSize = 8 * 1024 * 1024;
+        public const int ChunkSize = 256 * 1024 * 1024;
         public const int ChunksCacheSize = 2 * (ChunkSize + ChunkHeader.Size + ChunkFooter.Size);
 
 	    public static TimeSpan MinFlushDelayMs = TimeSpan.FromMilliseconds(2);
