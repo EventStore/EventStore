9eea5bdf987dafb5aed05c17d10aca9535a23a1a
Greg Young <gregoryyoung1@gmail.com>
2015-01-18 19:14:08 -0800

    fixes test generation for single/multi

--- src/EventStore.Core.Tests/TransactionLog/Validation/TFChunkDbCreationTestBase.cs
+++ src/EventStore.Core.Tests/TransactionLog/Validation/TFChunkDbCreationTestBase.cs
@@ -12,9 +12,9 @@ namespace EventStore.Core.Tests.TransactionLog.Validation
         {
             var chunkHeader = new ChunkHeader(TFChunk.CurrentChunkVersion, config.ChunkSize, chunkNum, chunkNum, isScavenged, Guid.NewGuid());
             var chunkBytes = chunkHeader.AsByteArray();
-            var dataSize = actualDataSize ?? config.ChunkSize;
+            var foundDataSize = actualDataSize ?? config.ChunkSize;
             var extra = ChunkFooter.Size + ChunkHeader.Size;
-            dataSize = (dataSize+extra)%4096 == 0 ? dataSize : ((dataSize+extra)/4096 + 1)*4096 - extra;
+            var dataSize = (foundDataSize+extra)%4096 == 0 ? foundDataSize : ((foundDataSize+extra)/4096 + 1)*4096 - extra;
             var buf = new byte[ChunkHeader.Size + dataSize + ChunkFooter.Size];
             Buffer.BlockCopy(chunkBytes, 0, buf, 0, chunkBytes.Length);
             var chunkFooter = new ChunkFooter(true, true, dataSize, dataSize, 0, new byte[ChunkFooter.ChecksumSize]);
@@ -23,7 +23,7 @@ namespace EventStore.Core.Tests.TransactionLog.Validation
 
             if (contents != null)
             {
-                if (contents.Length != dataSize)
+                if (contents.Length != foundDataSize)
                     throw new Exception("Wrong contents size.");
                 Buffer.BlockCopy(contents, 0, buf, ChunkHeader.Size, contents.Length);
             }
@@ -38,11 +38,14 @@ namespace EventStore.Core.Tests.TransactionLog.Validation
 
             var chunkHeader = new ChunkHeader(TFChunk.CurrentChunkVersion, config.ChunkSize, chunkStartNum, chunkEndNum, true, Guid.NewGuid());
             var chunkBytes = chunkHeader.AsByteArray();
-            var physicalDataSize = physicalSize ?? config.ChunkSize;
+            var foundDataSize = physicalSize ?? config.ChunkSize;
+            var extra = ChunkFooter.Size + ChunkHeader.Size;
+            var dataSize = (foundDataSize + extra) % 4096 == 0 ? foundDataSize : ((foundDataSize + extra) / 4096 + 1) * 4096 - extra;
+            
             var logicalDataSize = logicalSize ?? (chunkEndNum - chunkStartNum + 1) * config.ChunkSize;
-            var buf = new byte[ChunkHeader.Size + physicalDataSize + ChunkFooter.Size];
+            var buf = new byte[ChunkHeader.Size + dataSize + ChunkFooter.Size];
             Buffer.BlockCopy(chunkBytes, 0, buf, 0, chunkBytes.Length);
-            var chunkFooter = new ChunkFooter(true, true, physicalDataSize, logicalDataSize, 0, new byte[ChunkFooter.ChecksumSize]);
+            var chunkFooter = new ChunkFooter(true, true, foundDataSize, logicalDataSize, 0, new byte[ChunkFooter.ChecksumSize]);
             chunkBytes = chunkFooter.AsByteArray();
             Buffer.BlockCopy(chunkBytes, 0, buf, buf.Length - ChunkFooter.Size, chunkBytes.Length);
             File.WriteAllBytes(filename, buf);
