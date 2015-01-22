f7939d8614bec60c7499aeb3eed45ef30b9ac638
Rich Knight <rich@rich-knight.com>
2015-01-22 16:10:08 +0000

    Changes to output format of DocumentGenerator

--- etc/EventStore.Documentation/DocumentGenerator.cs
+++ etc/EventStore.Documentation/DocumentGenerator.cs
@@ -29,21 +29,19 @@ namespace EventStore.Documentation
                         var optionConstructor = optionType.GetConstructor(new Type[] { });
                         var options = optionConstructor.Invoke(null);
                         var optionDocumentation = String.Format("###{0}{1}", options.GetType().Name, Environment.NewLine);
-                        optionDocumentation += String.Format("| Group  | Parameter | Environment *(all prefixed with EVENTSTORE_)* | Yaml | Description | Default |{0}", Environment.NewLine);
-                        optionDocumentation += String.Format("| ------ | --------- | --------------------------------------------- | ---- | ----------- | ------- |{0}", Environment.NewLine);
+                        
                         var properties = options.GetType().GetProperties();
                         var currentGroup = String.Empty;
                         foreach (var property in properties.OrderBy(x => x.Attr<ArgDescriptionAttribute>().Group))
                         {
                             var parameterRow = String.Empty;
-                            var groupColumn = "|";
                             if (currentGroup != property.Attr<ArgDescriptionAttribute>().Group)
                             {
                                 currentGroup = property.Attr<ArgDescriptionAttribute>().Group;
-                                groupColumn += "**" + currentGroup + "**";
+                                optionDocumentation += String.Format("{0}###{1}{0}", Environment.NewLine, currentGroup);
+                                optionDocumentation += String.Format("| Parameter | Environment *(all prefixed with EVENTSTORE_)* | Yaml | Description |{0}", Environment.NewLine);
+                                optionDocumentation += String.Format("| --------- | --------------------------------------------- | ---- | ----------- |{0}", Environment.NewLine);
                             }
-
-                            parameterRow += groupColumn;
 
                             var parameterDefinition = new ArgumentUsageInfo(property);
                             var parameterUsageFormat = "-{0}=VALUE<br/>";
@@ -56,10 +54,12 @@ namespace EventStore.Documentation
                             parameterUsage += String.Format(parameterUsageFormat, parameterDefinition.Name);
 
                             parameterRow += String.Format("|{0}", parameterUsage);
-                            parameterRow += String.Format("|{0}", NameTranslators.PrefixEnvironmentVariable(property.Name, "EVENTSTORE_").ToUpper());
+                            parameterRow += String.Format("|{0}", NameTranslators.PrefixEnvironmentVariable(property.Name, "").ToUpper());
                             parameterRow += String.Format("|{0}", property.Name);
-                            parameterRow += String.Format("|{0}", property.Attr<ArgDescriptionAttribute>().Description);
-                            parameterRow += String.Format("|{0}|{1}", GetValues(property.GetValue(options, null)), Environment.NewLine);
+                            
+                            var defaultValue = GetValues(property.GetValue(options, null));
+                            var defaultString = defaultValue == "" ? "" : String.Format(" (Default: {0})", defaultValue);
+                            parameterRow += String.Format("|{0}{1}|{2}", property.Attr<ArgDescriptionAttribute>().Description, defaultString, Environment.NewLine);
 
                             optionDocumentation += parameterRow;
                         }
