c94822aaf844c1e625813b13b912a9b467d708b1
pgermishuys <pieter@pieterg.com>
2015-03-01 18:47:15 +0200

    - For the mininode, acceptable to check if the histogram has been added to the dictionary. - The Accepts header is invalid and the status code is not acceptable.

--- src/EventStore.Core.Tests/Http/Streams/feed.cs
+++ src/EventStore.Core.Tests/Http/Streams/feed.cs
@@ -128,9 +128,9 @@ namespace EventStore.Core.Tests.Http.Streams
             }
 
             [Test]
-            public void returns_ok_status_code()
+            public void returns_not_acceptable_status_code()
             {
-                Assert.AreEqual(HttpStatusCode.OK, _lastResponse.StatusCode);
+                Assert.AreEqual(HttpStatusCode.NotAcceptable, _lastResponse.StatusCode);
             }
         }
 
