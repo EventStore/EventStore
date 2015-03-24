54673b700c76c2c51550a7cb195e31ec37e00365
Greg Young <gregoryyoung1@gmail.com>
2015-03-24 15:45:34 -0700

    get rid of spinlocks

--- src/EventStore.ClientAPI/Transport.Tcp/TcpConnectionSsl.cs
+++ src/EventStore.ClientAPI/Transport.Tcp/TcpConnectionSsl.cs
@@ -54,7 +54,7 @@ namespace EventStore.ClientAPI.Transport.Tcp
         private readonly Common.Concurrent.ConcurrentQueue<ArraySegment<byte>> _receiveQueue = new Common.Concurrent.ConcurrentQueue<ArraySegment<byte>>();
         private readonly MemoryStream _memoryStream = new MemoryStream();
 
-        private readonly SpinLock2 _streamLock = new SpinLock2();
+        private readonly object _streamLock = new object();
         private bool _isSending;
         private int _receiveHandling;
         private int _isClosed;
@@ -88,7 +88,7 @@ namespace EventStore.ClientAPI.Transport.Tcp
 
             _validateServer = validateServer;
 
-            using (_streamLock.Acquire())
+            lock(_streamLock)
             {
                 try
                 {
@@ -126,7 +126,7 @@ namespace EventStore.ClientAPI.Transport.Tcp
         {
             try
             {
-                using (_streamLock.Acquire())
+                lock(_streamLock)
                 {
                     var sslStream = (SslStream) ar.AsyncState;
                     sslStream.EndAuthenticateAsClient(ar);
@@ -200,7 +200,7 @@ namespace EventStore.ClientAPI.Transport.Tcp
 
         public void EnqueueSend(IEnumerable<ArraySegment<byte>> data)
         {
-            using (_streamLock.Acquire())
+            lock(_streamLock)
             {
                 int bytes = 0;
                 foreach (var segment in data)
@@ -215,7 +215,7 @@ namespace EventStore.ClientAPI.Transport.Tcp
 
         private void TrySend()
         {
-            using (_streamLock.Acquire())
+            lock(_streamLock)
             {
                 if (_isSending || _sendQueue.Count == 0 || _sslStream == null || !_isAuthenticated) return;
                 if (TcpConnectionMonitor.Default.IsSendBlocked()) return;
@@ -261,7 +261,7 @@ namespace EventStore.ClientAPI.Transport.Tcp
                 _sslStream.EndWrite(ar);
                 NotifySendCompleted(_sendingBytes);
 
-                using (_streamLock.Acquire())
+                lock(_streamLock)
                 {
                     _isSending = false;
                 }
