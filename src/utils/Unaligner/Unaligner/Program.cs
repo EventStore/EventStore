5cd570112b1d9c37e41cc01198aa8a586789465a
Greg Young <gregoryyoung1@gmail.com>
2015-06-01 08:15:25 -0700

    read header and footer

--- src/utils/Unaligner/Unaligner/Program.cs
+++ src/utils/Unaligner/Unaligner/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using EventStore.Core.TransactionLog.Chunks;
 
 namespace Unaligner
@@ -12,29 +13,52 @@ namespace Unaligner
 
         private void Unalignv2FileToOld(string filename)
         {
-            //takes a v2 file and aligns it so it can be used with unbuffered
+            //takes a v2 file and unaligns it so it can be used with previous versions
             try
             {
                 SetAttributes(filename, false);
                 using (var stream = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
                 {
-                    if (stream.Length % 4096 == 0) return;
-                    var footerStart = stream.Length - ChunkFooter.Size;
-                    var alignedSize = (stream.Length / 4096 + 1) * 4096;
-                    var footer = new byte[ChunkFooter.Size];
-                    stream.SetLength(alignedSize);
-                    stream.Seek(footerStart, SeekOrigin.Begin);
-                    stream.Read(footer, 0, ChunkFooter.Size);
-                    stream.Seek(footerStart, SeekOrigin.Begin);
-                    var bytes = new byte[alignedSize - footerStart - ChunkFooter.Size];
-                    stream.Write(bytes, 0, bytes.Length);
-                    stream.Write(footer, 0, footer.Length);
+                    Console.WriteLine("File size is {0}", stream.Length);
+                    var header = ChunkHeader.FromStream(new MemoryStream(ReadHeader(stream)));
+                    Console.WriteLine("Read header chunk {0} ({1}-{2} start: {3} end: {4}", 
+                        header.ChunkId, 
+                        header.ChunkStartNumber, 
+                        header.ChunkEndNumber,
+                        header.ChunkStartPosition,
+                        header.ChunkEndPosition);
+                    var footer = ChunkHeader.FromStream(new MemoryStream(ReadFooter(stream)));
+                    Console.WriteLine("Read footer chunk {0} ({1}-{2} start: {3} end: {4} size: {5}",
+                        footer.ChunkId,
+                        footer.ChunkStartNumber,
+                        footer.ChunkEndNumber,
+                        footer.ChunkStartPosition,
+                        footer.ChunkEndPosition,
+                        footer.ChunkSize);
                 }
             }
             finally
             {
                 SetAttributes(filename, true);
             }
+        }
+
+        private byte[] ReadHeader(FileStream stream)
+        {
+            var header = new byte[ChunkHeader.Size];
+            var footerStart = stream.Length - ChunkHeader.Size;
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.Read(header, 0, ChunkHeader.Size);
+            return header;            
+        }
+
+        private byte[] ReadFooter(FileStream stream)
+        {
+            var footer = new byte[ChunkFooter.Size];
+            var footerStart = stream.Length - ChunkFooter.Size;
+            stream.Seek(footerStart, SeekOrigin.Begin);
+            stream.Read(footer, 0, ChunkFooter.Size);
+            return footer;
         }
 
         private void SetAttributes(string filename, bool isReadOnly)
