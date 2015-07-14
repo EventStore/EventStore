d599866828a5799116c14fd35c13203e7a460af4
James Nugent <james@jen20.com>
2015-07-14 17:16:08 -0500

    Add script for packaging from Xamarin Mono package
    This commit adds a new script, package-mono-xamarin.sh, which allows
    creation of a statically linked Event Store package with Mono installed
    from the Xamarin Debian repository.
    
    Unfortunately there are some assembly config files which are present in
    the packages which are not present if you build from source, which then
    cause what appears to be a bug in mkbundle to be hit whereby it
    generates C code which declares arrays of unsigned char which are then
    passed into a function expecting const char *. This produces warnings
    under gcc 4.8.
    
    Rather than fix mkbundle (which would take time to be reflected in the
    package repositories, if it were even accepted at all), we instead pass
    the generated source through sed, removing the unsigned modifier for the
    path to the configuration files. The source then compiles without
    warnings.

---
+++ scripts/package-mono/package-mono-xamarin.sh
@@ -0,0 +1,169 @@
+#!/usr/bin/env bash
+
+set -e
+
+version=$1
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function usage {
+	echo "Usage:"
+	echo "  $0 version"
+	echo ""
+	echo "Note: This script assumes Mono is installed at the default locations from"
+    echo "      the Xamarin package files."
+	exit
+}
+
+function writeLog {
+	message=$1
+	echo "[package-mono.sh] - $message"
+}
+
+if [[ "$version" == "" ]] ; then
+	VERSIONSTRING="0.0.0.0"
+	writeLog "Version defaulted to: 0.0.0.0"
+else
+	VERSIONSTRING=$version
+	writeLog "Version set to: $VERSIONSTRING"
+fi
+
+MONOPREFIX=/usr
+CONFIGPREFIX=/etc
+
+MKBUNDLEPATH=$MONOPREFIX/bin/mkbundle
+if [[ -f $MKBUNDLEPATH ]] ; then
+	writeLog "Using mkbundle: $MKBUNDLEPATH"
+else
+	writeLog "Cannot find mkbundle"
+	exit 1
+fi
+
+MACHINECONFIG=$CONFIGPREFIX/mono/4.0/machine.config
+if [[ -f $MACHINECONFIG ]] ; then
+    writeLog "Using --machine-config: $MACHINECONFIG"
+else
+    writeLog "Cannot find machine config at $MACHINECONFIG"
+    exit 1
+fi
+
+MONOCONFIG=$CONFIGPREFIX/mono/config
+if [[ -f $MONOCONFIG ]] ; then
+    writeLog "Using --config: $MONOCONFIG"
+else
+    writeLog "Cannot find mono config at $MONOCONFIG"
+    exit 1
+fi
+
+GCCPATH=`which gcc`
+if [[ $? != 0 ]] ; then
+	writeLog "Cannot find gcc"
+	exit 1
+else
+	writeLog "Using gcc: $GCCPATH"
+fi
+
+OUTPUTDIR="$SCRIPTDIR/../../bin/packaged"
+[[ -d $OUTPUTDIR ]] || mkdir -p $OUTPUTDIR
+
+
+if [[ $OS == "Darwin" ]] ; then
+    soext="dylib"
+    PACKAGENAME="EventStore-OSS-Mac-v$VERSIONSTRING"
+else
+    soext="so"
+    PACKAGENAME="EventStore-OSS-Linux-v$VERSIONSTRING"
+fi
+
+PACKAGEDIRECTORY="$OUTPUTDIR/$PACKAGENAME"
+
+if [[ -d $PACKAGEDIRECTORY ]] ; then
+    rm -rf $PACKAGEDIRECTORY
+fi
+mkdir $PACKAGEDIRECTORY
+
+
+pushd $SCRIPTDIR/../../bin/clusternode/
+
+mkbundle -c -o clusternode.c -oo clusternode.a \
+	EventStore.ClusterNode.exe \
+	EventStore.Rags.dll \
+	EventStore.Core.dll \
+	EventStore.BufferManagement.dll \
+	EventStore.Common.dll \
+	EventStore.Projections.Core.dll \
+	EventStore.ClusterNode.Web.dll \
+	EventStore.Transport.Http.dll \
+	EventStore.Transport.Tcp.dll \
+	Newtonsoft.Json.dll \
+	NLog.dll protobuf-net.dll \
+	Mono.Security.dll \
+	--static --deps --config $MONOCONFIG --machine-config $MACHINECONFIG
+
+#mkbundle appears to be doing it wrong, though maybe there's something I'm not seeing.
+sed -e '/_config_/ s/unsigned //' -i"" clusternode.c
+
+cc -o eventstored \
+	-Wall `pkg-config --cflags monosgen-2` \
+	clusternode.c \
+	`pkg-config --libs-only-L monosgen-2` \
+	-Wl,-Bstatic -lmonosgen-2.0 \
+	-Wl,-Bdynamic `pkg-config --libs-only-l monosgen-2 | sed -e "s/\-lmono-2.0 //"` \
+	clusternode.a
+
+cp -r clusternode-web $PACKAGEDIRECTORY/
+cp -r Prelude $PACKAGEDIRECTORY/
+cp -r projections $PACKAGEDIRECTORY/
+cp libjs1.$soext $PACKAGEDIRECTORY/
+cp eventstored $PACKAGEDIRECTORY/
+cp log.config $PACKAGEDIRECTORY/
+cp $SCRIPTDIR/run-node.sh $PACKAGEDIRECTORY/run-node.sh
+
+popd
+
+pushd $SCRIPTDIR/../../bin/testclient
+
+mkbundle -c \
+	-o testclient.c \
+	-oo testclient.a \
+	EventStore.TestClient.exe \
+	EventStore.Core.dll \
+	EventStore.Rags.dll \
+	EventStore.ClientAPI.dll \
+	EventStore.BufferManagement.dll \
+	EventStore.Common.dll \
+	EventStore.Transport.Http.dll \
+	EventStore.Transport.Tcp.dll \
+	Newtonsoft.Json.dll \
+	NLog.dll \
+	protobuf-net.dll \
+	--static --deps --config $MONOCONFIG --machine-config $MACHINECONFIG
+
+#mkbundle appears to be doing it wrong, though maybe there's something I'm not seeing.
+sed -e '/_config_/ s/unsigned //' -i"" testclient.c
+
+cc -o testclient \
+	-Wall `pkg-config --cflags monosgen-2` \
+	testclient.c \
+	`pkg-config --libs-only-L monosgen-2` \
+	-Wl,-Bstatic -lmonosgen-2.0 \
+	-Wl,-Bdynamic `pkg-config --libs-only-l monosgen-2 | sed -e "s/\-lmono-2.0 //"` \
+	testclient.a
+
+cp testclient $PACKAGEDIRECTORY/
+
+popd
+
+
+pushd $OUTPUTDIR
+
+tar -zcvf $PACKAGENAME.tar.gz $PACKAGENAME
+rm -r $PACKAGEDIRECTORY
+
+[[ -d ../../packages ]] || mkdir -p ../../packages
+mv $PACKAGENAME.tar.gz ../../packages/
+
+popd
+
+rm -r $OUTPUTDIR
+
