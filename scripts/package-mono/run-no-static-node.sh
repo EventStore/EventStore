5eaa492934d3b4bdb8615b7bb56104572fe2bbe1
Daniel Nauck <dna@mono-project.de>
2015-07-20 19:16:24 +0200

    Use correct path on executing eventstore

--- scripts/package-mono/run-no-static-node.sh
+++ scripts/package-mono/run-no-static-node.sh
@@ -1,4 +1,4 @@
 ##!/usr/bin/env bash
 EVENTSTORE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 MONO_DIR=`which mono`
-exec $MONO_DIR EventStore.ClusterNode.exe $@
+exec $MONO_DIR ${EVENTSTORE_DIR}/EventStore.ClusterNode.exe $@
