06197988189c5f1d4e8b642be82fcccefbc57ae2
pgermishuys <pieter@pieterg.com>
2015-07-24 11:44:34 +0200

    Added TeamCity Test Runner. Bug in TC 9.1 doesnt allow us to use the TC Nunit Runner

---
+++ run_tc_tests.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+while getopts "m:x:p" option
+do
+    case $option in
+        m)
+            MONOPATH=$OPTARG 
+            ;;   
+        x)
+            EXCLUDE="-exclude $OPTARG"
+            ;;
+        p)
+            RUNPROJECTIONS="TRUE"
+            ;;
+        ?)
+            echo "Usage: run_tests.sh [-x ExcludeCategories] [-m /path/to/mono] [-p]"
+            echo "Defaults:"
+            echo "   Mono Path: /opt/mono"
+            echo "   Exclude: None"
+            echo "   projections: false"
+            exit
+            ;;
+    esac
+done
+
+if [[ $MONOPATH == "" ]]; then
+    MONOPATH="/opt/mono"
+fi
+
+LD_LIBRARY_PATH=bin/tests:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/tests/EventStore.Core.Tests.dll
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+
+if [[ $RUNPROJECTIONS == "TRUE" ]]; then
+    LD_LIBRARY_PATH=bin/tests/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/tests/EventStore.Projections.Core.Tests.dll $EXCLUDE
+fi
+
+echo ##teamcity[importData type='nunit' Path='tools/nunit-2.6.3/results.xml']" 
