22cc1353efe00e828bdd1c2400416b4164c08551
pgermishuys <pieter@pieterg.com>
2015-07-29 19:12:59 +0200

    Add path prefix, it's necessary to add a ./run_tc_tests . for oss

--- run_tc_tests.sh
+++ run_tc_tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+pathPrefix=$1
 while getopts "m:x:p" option
 do
     case $option in
@@ -26,10 +27,10 @@ if [[ $MONOPATH == "" ]]; then
     MONOPATH="/opt/mono"
 fi
 
-LD_LIBRARY_PATH=bin/tests:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/tests/EventStore.Core.Tests.dll $EXCLUDE
+LD_LIBRARY_PATH=$pathPrefix/bin/tests:$MONOPATH/lib/:$LD_LIBRARY_PATH mono $pathPrefix/tools/nunit-2.6.3/bin/nunit-console.exe $pathPrefix/bin/tests/EventStore.Core.Tests.dll $EXCLUDE
 
 if [[ $RUNPROJECTIONS == "TRUE" ]]; then
-    LD_LIBRARY_PATH=bin/tests/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono tools/nunit-2.6.3/bin/nunit-console.exe bin/tests/EventStore.Projections.Core.Tests.dll $EXCLUDE
+    LD_LIBRARY_PATH=$pathPrefix/bin/tests/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono $pathPrefix/tools/nunit-2.6.3/bin/nunit-console.exe $pathPrefix/bin/tests/EventStore.Projections.Core.Tests.dll $EXCLUDE
 fi
 
-echo "##teamcity[importData type='nunit' path='TestResult.xml']"
+echo "##teamcity[importData type='nunit' path='$pathPrefix/TestResult.xml']"
