1df3bdc5741703e9f88d3767d7e3ddc312b18e87
pgermishuys <pieter@pieterg.com>
2015-09-06 14:44:10 +0200

    move etc over to EventStore.Rags

--- etc/EventStore.Documentation/DocumentGenerator.cs
+++ etc/EventStore.Documentation/DocumentGenerator.cs
@@ -1,9 +1,9 @@
 ﻿using EventStore.Common.Options;
-using PowerArgs;
 using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using EventStore.Rags;
 
 namespace EventStore.Documentation
 {
@@ -29,38 +29,45 @@ namespace EventStore.Documentation
                         var optionConstructor = optionType.GetConstructor(new Type[] { });
                         var options = optionConstructor.Invoke(null);
                         var optionDocumentation = String.Format("###{0}{1}", options.GetType().Name, Environment.NewLine);
-                        optionDocumentation += String.Format("| Group  | Parameter | Environment *(all prefixed with EVENTSTORE_)* | Yaml | Description | Default |{0}", Environment.NewLine);
-                        optionDocumentation += String.Format("| ------ | --------- | --------------------------------------------- | ---- | ----------- | ------- |{0}", Environment.NewLine);
+
                         var properties = options.GetType().GetProperties();
-                        var argumentsDefinition = new CommandLineArgumentsDefinition(optionType);
                         var currentGroup = String.Empty;
-                        foreach (var property in properties.OrderBy(x => x.Attr<ArgDescription>().Group))
+                        foreach (var property in properties.OrderBy(x => x.Attr<ArgDescriptionAttribute>().Group))
                         {
                             var parameterRow = String.Empty;
-                            var groupColumn = "|";
-                            if (currentGroup != property.Attr<ArgDescription>().Group)
+                            if (currentGroup != property.Attr<ArgDescriptionAttribute>().Group)
                             {
-                                currentGroup = property.Attr<ArgDescription>().Group;
-                                groupColumn += "**" + currentGroup + "**";
+                                currentGroup = property.Attr<ArgDescriptionAttribute>().Group;
+                                optionDocumentation += String.Format("{0}###{1}{0}", Environment.NewLine, currentGroup);
+                                optionDocumentation += String.Format("| Parameter | Environment *(all prefixed with EVENTSTORE_)* | Yaml | Description |{0}", Environment.NewLine);
+                                optionDocumentation += String.Format("| --------- | --------------------------------------------- | ---- | ----------- |{0}", Environment.NewLine);
                             }
 
-                            parameterRow += groupColumn;
-
-                            var parameterDefinition = argumentsDefinition.Arguments.First(x => ((PropertyInfo)x.Source).Name == property.Name);
-                            var parameterUsageFormat = "-{0} <br/>";
+                            var parameterDefinition = new ArgumentUsageInfo(property);
+                            var parameterUsageFormat = "-{0}=VALUE<br/>";
                             var parameterUsage = String.Empty;
 
-                            foreach (var alias in parameterDefinition.Aliases.Reverse())
+                            foreach (var alias in parameterDefinition.Aliases)
                             {
-                                parameterUsage += String.Format(parameterUsageFormat, alias);
-                                parameterUsageFormat = "--{0}=VALUE";
+                                parameterUsage += alias + "<br/>";
                             }
+                            parameterUsage += String.Format(parameterUsageFormat, parameterDefinition.Name);
 
                             parameterRow += String.Format("|{0}", parameterUsage);
-                            parameterRow += String.Format("|{0}", EnvironmentVariableNameProvider.GetName("EVENTSTORE_", property.Name.ToUpper()));
+                            parameterRow += String.Format("|{0}", NameTranslators.PrefixEnvironmentVariable(property.Name, "").ToUpper());
                             parameterRow += String.Format("|{0}", property.Name);
-                            parameterRow += String.Format("|{0}", property.Attr<ArgDescription>().Description);
-                            parameterRow += String.Format("|{0}|{1}", GetValues(property.GetValue(options, null)), Environment.NewLine);
+
+                            var defaultValue = GetValues(property.GetValue(options, null));
+                            var defaultString = defaultValue == "" ? "" : String.Format(" (Default: {0})", defaultValue);
+                            string[] possibleValues = null;
+                            if (property.PropertyType.IsEnum)
+                            {
+                                possibleValues = property.PropertyType.GetEnumNames();
+                            }
+                            parameterRow += String.Format("|{0}{1} {2}|{3}",
+                                    property.Attr<ArgDescriptionAttribute>().Description,
+                                    defaultString, possibleValues != null ? "Possible Values:" + String.Join(",", possibleValues) : String.Empty,
+                                    Environment.NewLine);
 
                             optionDocumentation += parameterRow;
                         }
