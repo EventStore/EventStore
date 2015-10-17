b97b75e599e974100078074a38055851be6397e9
Pål Ruud <ruudud@gmail.com>
2015-10-17 21:08:02 +0200

    Use single hash in shebang
    When running an executable, the kernel (and other executors) looks at the first
    few bytes to determine the type of the file.
    Some get confused when met by double hash.

--- scripts/package-mono/run-mono-node.sh
+++ scripts/package-mono/run-mono-node.sh
@@ -1,4 +1,4 @@
-##!/usr/bin/env bash
+#!/usr/bin/env bash
 EVENTSTORE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 MONO_PATH=`which mono`
 LD_LIBRARY_PATH=${EVENTSTORE_DIR}:$LD_LIBRARY_PATH MONO_GC_DEBUG=clear-at-gc $MONO_PATH ${EVENTSTORE_DIR}/EventStore.ClusterNode.exe $@
