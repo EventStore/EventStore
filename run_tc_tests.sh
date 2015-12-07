564acab39120e252b8ca90309470c09b4633eb0c
pgermishuys <pieter@pieterg.com>
2015-12-07 14:04:03 +0200

    Add Buffer Management and Projection tests

--- run_tc_tests.sh
+++ run_tc_tests.sh
@@ -1,36 +1,3 @@
 #!/usr/bin/env bash
 pathPrefix=$1
-while getopts "m:x:p" option
-do
-    case $option in
-        m)
-            MONOPATH=$OPTARG 
-            ;;   
-        x)
-            EXCLUDE="-exclude $OPTARG"
-            ;;
-        p)
-            RUNPROJECTIONS="TRUE"
-            ;;
-        ?)
-            echo "Usage: run_tests.sh [-x ExcludeCategories] [-m /path/to/mono] [-p]"
-            echo "Defaults:"
-            echo "   Mono Path: /opt/mono"
-            echo "   Exclude: None"
-            echo "   projections: false"
-            exit
-            ;;
-    esac
-done
-
-if [[ $MONOPATH == "" ]]; then
-    MONOPATH="/opt/mono"
-fi
-
-LD_LIBRARY_PATH=$pathPrefix/bin/tests:$MONOPATH/lib/:$LD_LIBRARY_PATH mono $pathPrefix/tools/nunit-2.6.3/bin/nunit-console.exe $pathPrefix/bin/tests/EventStore.Core.Tests.dll $EXCLUDE
-
-if [[ $RUNPROJECTIONS == "TRUE" ]]; then
-    LD_LIBRARY_PATH=$pathPrefix/bin/tests/:$MONOPATH/lib/:$LD_LIBRARY_PATH mono $pathPrefix/tools/nunit-2.6.3/bin/nunit-console.exe $pathPrefix/bin/tests/EventStore.Projections.Core.Tests.dll $EXCLUDE
-fi
-
-echo "##teamcity[importData type='nunit' path='TestResult.xml']"
+LD_LIBRARY_PATH=$pathPrefix/bin/tests:$MONOPATH/lib/:$LD_LIBRARY_PATH mono $pathPrefix/tools/nunit-2.6.3/bin/nunit-console.exe $pathPrefix/bin/tests/EventStore.BufferManagement.Tests.dll $pathPrefix/bin/tests/EventStore.Core.Tests.dll $pathPrefix/bin/tests/EventStore.Projections.Core.Tests.dll
