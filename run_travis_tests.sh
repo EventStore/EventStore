56c0378bdba113d35f09f3e3225acc74ef8a3ef6
James Nugent <james@jen20.com>
2015-12-08 13:52:29 -0500

    We aren't using Maven...

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -25,9 +25,6 @@ trap 'error_handler' ERR
 bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
-# My build is using maven, but you could build anything with this, E.g.
-# your_build_command_1 >> $BUILD_OUTPUT 2>&1
-# your_build_command_2 >> $BUILD_OUTPUT 2>&1
 ./run_tc_tests.sh . >> $BUILD_OUTPUT 2>&1
 
 # The build finished without returning an error so dump a tail of the output
