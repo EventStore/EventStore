d69b5a2f3d50b6c2a4708aed3eaf8e3018dd2e9b
Pieter Germishuys <pieter@pieterg.com>
2016-02-26 11:29:20 +0200

    Filter out unimportant messages for logging

---
+++ src/EventStore.Projections.Core/Utils/Logging.cs
@@ -0,0 +1,7 @@
+﻿namespace EventStore.Projections.Core.Utils
+{
+    public static class Logging
+    {
+        public static readonly string[] FilteredMessages = { "$get-state", "$state", "$get-result", "$result", "$statistics-report" };
+    }
+}
