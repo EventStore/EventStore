7d0fc2a350816252aa476a16de6b1705ed565988
Greg Young <gregoryyoung1@gmail.com>
2017-01-04 16:11:29 +0000

    add forwarded prefix

--- src/EventStore.Transport.Http/ProxyHeaders.cs
+++ src/EventStore.Transport.Http/ProxyHeaders.cs
@@ -5,6 +5,7 @@
         public const string XForwardedPort = "X-Forwarded-Port";
         public const string XForwardedProto = "X-Forwarded-Proto";
         public const string XForwardedHost = "X-Forwarded-Host";
+        public const string XForwardedPrefix = "X-Forwarded-Prefix";
     }
 
     public static class ProxyHeaderValues
