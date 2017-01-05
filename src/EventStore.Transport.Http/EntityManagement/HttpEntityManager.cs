5245da045b609f0c51a491a2131813881da4672f
Greg Young <gregoryyoung1@gmail.com>
2017-01-05 08:41:51 +0000

    allow header should be linktos not linkto

--- src/EventStore.Transport.Http/EntityManagement/HttpEntityManager.cs
+++ src/EventStore.Transport.Http/EntityManagement/HttpEntityManager.cs
@@ -142,7 +142,7 @@ namespace EventStore.Transport.Http.EntityManagement
             try
             {
                 HttpEntity.Response.AddHeader("Access-Control-Allow-Methods", string.Join(", ", _allowedMethods));
-                HttpEntity.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, X-Forwarded-Host, X-Forwarded-Prefix, X-PINGOTHER, Authorization, ES-LongPoll, ES-ExpectedVersion, ES-EventId, ES-EventType, ES-RequiresMaster, ES-HardDelete, ES-ResolveLinkTo");
+                HttpEntity.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, X-Forwarded-Host, X-Forwarded-Prefix, X-PINGOTHER, Authorization, ES-LongPoll, ES-ExpectedVersion, ES-EventId, ES-EventType, ES-RequiresMaster, ES-HardDelete, ES-ResolveLinkTos");
                 HttpEntity.Response.AddHeader("Access-Control-Allow-Origin", "*");
                 HttpEntity.Response.AddHeader("Access-Control-Expose-Headers", "Location, ES-Position, ES-CurrentVersion");
                 if (HttpEntity.Response.StatusCode == HttpStatusCode.Unauthorized)
