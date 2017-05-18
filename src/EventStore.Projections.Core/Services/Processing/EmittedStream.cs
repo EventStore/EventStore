c023c98a8fd53f2b836ae5bda95ed18627073c6c
Pieter Germishuys <pieter@pieterg.com>
2017-05-18 10:56:54 +0100

    Add additional logging for failure conditions

--- src/EventStore.Projections.Core/Services/Processing/EmittedStream.cs
+++ src/EventStore.Projections.Core/Services/Processing/EmittedStream.cs
@@ -236,19 +236,19 @@ namespace EventStore.Projections.Core.Services.Processing
             switch (message.Result)
             {
                 case OperationResult.WrongExpectedVersion:
-                    RequestRestart(string.Format("The '{0}' stream has been written to from the outside", _streamId));
+                    RequestRestart(string.Format("The '{0}' stream has been written to from the outside. Expected Version: {1}, Current Version: {2}. Checkpoint: {3}.", _streamId, _lastKnownEventNumber, message.CurrentVersion, _fromCheckpointPosition));
                     break;
                 case OperationResult.PrepareTimeout:
                 case OperationResult.ForwardTimeout:
                 case OperationResult.CommitTimeout:
                     if(retryCount > 0)
                     {
-                        if (_logger != null) _logger.Info("Retrying write to {0} (Retry {1} of {2})", _streamId, (MaxRetryCount - retryCount) + 1, MaxRetryCount);
+                        if (_logger != null) _logger.Info("Retrying write to {0} (Retry {1} of {2}). Checkpoint: {3}.", _streamId, (MaxRetryCount - retryCount) + 1, MaxRetryCount, _fromCheckpointPosition);
                         PublishWriteEvents(--retryCount);
                     }
                     else
                     {
-                        Failed(string.Format("Failed to write an events to {0}. Retry limit of {1} reached. Reason: {2}", _streamId, MaxRetryCount, message.Result));
+                        Failed(string.Format("Failed to write events to {0}. Retry limit of {1} reached. Reason: {2}. Checkpoint: {3}.", _streamId, MaxRetryCount, message.Result, _fromCheckpointPosition));
                     }
                     break;
                 default:
