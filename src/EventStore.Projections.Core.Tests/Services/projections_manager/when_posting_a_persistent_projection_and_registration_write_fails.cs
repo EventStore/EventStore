1bcb66c0562a35b736a5d9af79e384bd212300ad
Pieter Germishuys <pieter@pieterg.com>
2017-07-12 14:50:38 +0100

    Make use of the idempotency checks when attempting rewrites

--- src/EventStore.Projections.Core.Tests/Services/projections_manager/when_posting_a_persistent_projection_and_registration_write_fails.cs
+++ src/EventStore.Projections.Core.Tests/Services/projections_manager/when_posting_a_persistent_projection_and_registration_write_fails.cs
@@ -8,12 +8,29 @@ using EventStore.Projections.Core.Services;
 using EventStore.Projections.Core.Services.Management;
 using NUnit.Framework;
 using EventStore.Projections.Core.Services.Processing;
+using System.Collections;
 
 namespace EventStore.Projections.Core.Tests.Services.projections_manager
 {
-    [TestFixture]
+    public class FailureConditions : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return OperationResult.CommitTimeout;
+            yield return OperationResult.ForwardTimeout;
+            yield return OperationResult.PrepareTimeout;
+        }
+    }
+
+    [TestFixture, TestFixtureSource(typeof(FailureConditions))]
     public class when_posting_a_persistent_projection_and_registration_write_fails : TestFixtureWithProjectionCoreAndManagementServices
     {
+        private OperationResult _failureCondition;
+        public when_posting_a_persistent_projection_and_registration_write_fails(OperationResult failureCondition)
+        {
+            _failureCondition = failureCondition;
+        }
+
         protected override void Given()
         {
             NoStream("$projections-test-projection-order");
@@ -38,13 +55,15 @@ namespace EventStore.Projections.Core.Tests.Services.projections_manager
         }
 
         [Test, Category("v8")]
-        public void retries_creating_the_projection_only_the_specified_number_of_times()
+        public void retries_creating_the_projection_only_the_specified_number_of_times_and_the_same_event_id()
         {
             int retryCount = 0;
             var projectionRegistrationWrite = _consumer.HandledMessages.OfType<ClientMessage.WriteEvents>().Where(x => x.EventStreamId == ProjectionNamesBuilder.ProjectionsRegistrationStream).Last();
+            var eventId = projectionRegistrationWrite.Events[0].EventId;
             while (projectionRegistrationWrite != null)
             {
-                projectionRegistrationWrite.Envelope.ReplyWith(new ClientMessage.WriteEventsCompleted(projectionRegistrationWrite.CorrelationId, OperationResult.CommitTimeout, "Commit Timeout"));
+                Assert.AreEqual(eventId, projectionRegistrationWrite.Events[0].EventId);
+                projectionRegistrationWrite.Envelope.ReplyWith(new ClientMessage.WriteEventsCompleted(projectionRegistrationWrite.CorrelationId, _failureCondition, Enum.GetName(typeof(OperationResult), _failureCondition)));
                 _queue.Process();
                 projectionRegistrationWrite = _consumer.HandledMessages.OfType<ClientMessage.WriteEvents>().Where(x => x.EventStreamId == ProjectionNamesBuilder.ProjectionsRegistrationStream).LastOrDefault();
                 if(projectionRegistrationWrite != null)
