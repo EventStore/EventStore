88665d28a96f42f19d9364badab224702193b417
Laurence Pike <lscpike@gmail.com>
2017-11-30 09:48:43 +0000

    Attempt 2 at writing failing test.

--- src/EventStore.Core.Tests/Index/IndexV2/table_index_when_merging_upgrading_to_64bit_if_entry_doesnt_exist_drops_entry_and_carries_on.cs
+++ src/EventStore.Core.Tests/Index/IndexV2/table_index_when_merging_upgrading_to_64bit_if_entry_doesnt_exist_drops_entry_and_carries_on.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading;
 using EventStore.Core.Index;
 using EventStore.Core.Index.Hashes;
+using EventStore.Core.Tests.Services.Storage;
 using EventStore.Core.TransactionLog;
 using EventStore.Core.TransactionLog.LogRecords;
 using NUnit.Framework;
@@ -18,6 +19,11 @@ namespace EventStore.Core.Tests.Index.IndexV2
         private string _indexDir;
         protected byte _ptableVersion;
 
+        // Note hash is by length so stream ids are set to order them specifically in the index.
+        private const string Stream1 = "hhh";
+        private const string Stream2 = "hh";
+        private const string Stream3 = "h";
+
         public table_index_when_merging_upgrading_to_64bit_if_single_stream_entry_doesnt_exist_drops_entry_and_carries_on()
         {
             _ptableVersion = PTableVersions.IndexV2;
@@ -30,31 +36,33 @@ namespace EventStore.Core.Tests.Index.IndexV2
 
             _indexDir = PathName;
             var fakeReader = new TFReaderLease(new FakeIndexReader2());
-            _lowHasher = new XXHashUnsafe();
-            _highHasher = new Murmur3AUnsafe();
+            _lowHasher = new ByLengthHasher();
+            _highHasher = new ByLengthHasher();
             _tableIndex = new TableIndex(_indexDir, _lowHasher, _highHasher,
-                () => new HashListMemTable(PTableVersions.IndexV1, maxSize: 2),
+                () => new HashListMemTable(PTableVersions.IndexV1, maxSize: 3),
                 () => fakeReader,
                 PTableVersions.IndexV1,
-                maxSizeForMemory: 5,
+                maxSizeForMemory: 3,
                 maxTablesPerLevel: 2);
             _tableIndex.Initialize(long.MaxValue);
 
-            _tableIndex.Add(1, "LPN-FC002_LPK51001", 0, 1);
-            _tableIndex.Add(1, "account--696193173", 0, 2);
+            _tableIndex.Add(1, Stream1, 0, 1);
+            _tableIndex.Add(1, Stream2, 0, 2);
+            _tableIndex.Add(1, Stream3, 0, 3);
 
             _tableIndex.Close(false);
 
             _tableIndex = new TableIndex(_indexDir, _lowHasher, _highHasher,
-                () => new HashListMemTable(_ptableVersion, maxSize: 2),
+                () => new HashListMemTable(_ptableVersion, maxSize: 3),
                 () => fakeReader,
                 _ptableVersion,
-                maxSizeForMemory: 5,
+                maxSizeForMemory: 3,
                 maxTablesPerLevel: 2);
             _tableIndex.Initialize(long.MaxValue);
 
-            _tableIndex.Add(1, "account--696193173", 1, 3);
-            _tableIndex.Add(1, "LPN-FC002_LPK51001", 1, 4);
+            _tableIndex.Add(1, Stream3, 1, 4);
+            _tableIndex.Add(1, Stream2, 1, 5);
+            _tableIndex.Add(1, Stream1, 1, 6);
 
             Thread.Sleep(500);
         }
@@ -70,19 +78,27 @@ namespace EventStore.Core.Tests.Index.IndexV2
         [Test]
         public void should_have_all_entries_except_scavenged()
         {
-            var streamId = "LPN-FC002_LPK51001";
+            var streamId = Stream1;
             var result = _tableIndex.GetRange(streamId, 0, 1).ToArray();
             var hash = (ulong)_lowHasher.Hash(streamId) << 32 | _highHasher.Hash(streamId);
 
             Assert.That(result.Count(), Is.EqualTo(1));
 
             Assert.That(result[0].Stream, Is.EqualTo(hash));
+            Assert.That(result[0].Version, Is.EqualTo(1));
+            Assert.That(result[0].Position, Is.EqualTo(6));
+
+            streamId = Stream2;
+            result = _tableIndex.GetRange(streamId, 0, 1).ToArray();
+            hash = (ulong)_lowHasher.Hash(streamId) << 32 | _highHasher.Hash(streamId);
+
+            Assert.That(result.Count(), Is.EqualTo(1));
 
             Assert.That(result[0].Stream, Is.EqualTo(hash));
             Assert.That(result[0].Version, Is.EqualTo(1));
-            Assert.That(result[0].Position, Is.EqualTo(4));
+            Assert.That(result[0].Position, Is.EqualTo(5));
 
-            streamId = "account--696193173";
+            streamId = Stream3;
             result = _tableIndex.GetRange(streamId, 0, 1).ToArray();
             hash = (ulong)_lowHasher.Hash(streamId) << 32 | _highHasher.Hash(streamId);
 
@@ -90,11 +106,11 @@ namespace EventStore.Core.Tests.Index.IndexV2
 
             Assert.That(result[0].Stream, Is.EqualTo(hash));
             Assert.That(result[0].Version, Is.EqualTo(1));
-            Assert.That(result[0].Position, Is.EqualTo(3));
+            Assert.That(result[0].Position, Is.EqualTo(4));
 
             Assert.That(result[1].Stream, Is.EqualTo(hash));
             Assert.That(result[1].Version, Is.EqualTo(0));
-            Assert.That(result[1].Position, Is.EqualTo(2));
+            Assert.That(result[1].Position, Is.EqualTo(3));
         }
 
         private class FakeIndexReader2 : ITransactionFileReader
@@ -116,14 +132,28 @@ namespace EventStore.Core.Tests.Index.IndexV2
 
             public RecordReadResult TryReadAt(long position)
             {
-                var eventStreamId = position % 2 == 0 ? "account--696193173" : "LPN-FC002_LPK51001";
+                string eventStreamId;
+                switch (position)
+                {
+                    case 1:
+                        eventStreamId = Stream1;
+                        break;
+                    case 2:
+                        eventStreamId = Stream2;
+                        break;
+                    case 3:
+                        eventStreamId = Stream3;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Unexpected position look up.");
+                }
                 var record = (LogRecord)new PrepareLogRecord(position, Guid.NewGuid(), Guid.NewGuid(), 0, 0, eventStreamId, -1, DateTime.UtcNow, PrepareFlags.None, "type", new byte[0], null);
                 return new RecordReadResult(true, position + 1, record, 1);
             }
 
             public bool ExistsAt(long position)
             {
-                return position != 1;
+                return position != 2 && position != 1;
             }
         }
     }
