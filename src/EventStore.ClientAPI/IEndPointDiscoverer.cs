fca3b41c80fc7c870b3b1f1ad118bc2a7fcd20a4
Greg Young <gregoryyoung1@gmail.com>
2018-04-02 14:56:05 +0700

    add ability to add custom node discovery

---
+++ src/EventStore.ClientAPI/IEndPointDiscoverer.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace EventStore.ClientAPI
+{
+    /// <summary>
+    /// Represents a way of discovering cluster endpoints this could be through gossip, consul, text files, etc
+    /// </summary>
+    public interface IEndPointDiscoverer
+    {
+        /// <summary>
+        /// Called to discover a new <see cref="IPEndPoint"/>
+        /// </summary>
+        /// <param name="failedTcpEndPoint">The <see cref="IPEndPoint"/> The recently failed endpoint</param>
+        Task<NodeEndPoints> DiscoverAsync(IPEndPoint failedTcpEndPoint);
+    }
+}
