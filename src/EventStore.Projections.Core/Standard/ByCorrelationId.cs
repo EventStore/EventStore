7a536f04e8213798ce251f04ffaea1653897c94c
Greg Young <gregoryyoung1@gmail.com>
2018-05-05 10:25:06 +0700

    iadd debug messages

--- src/EventStore.Projections.Core/Standard/ByCorrelationId.cs
+++ src/EventStore.Projections.Core/Standard/ByCorrelationId.cs
@@ -68,19 +68,21 @@ namespace EventStore.Projections.Core.Standard
             string partition, CheckpointTag eventPosition, string category1, ResolvedEvent data,
             out string newState, out string newSharedState, out EmittedEventEnvelope[] emittedEvents)
         {
+            Console.WriteLine("in ProcessEvent");
             newSharedState = null;
             emittedEvents = null;
             newState = null;
             if (data.EventStreamId != data.PositionStreamId)
                 return false;
+            Console.WriteLine("testing is json");
             if (!data.IsJson) 
                 return false;
+            Console.WriteLine("parsing json");
             var metadata = JObject.Parse(data.Metadata);
             var indexedEventType = data.EventType;
             
             string correlationId = metadata["$correlationId"].Value<string>();
-            Console.WriteLine("correlationId is " + metadata);
-            
+
             if (correlationId == null) 
                 return false;
             Console.WriteLine("correlationId is " + metadata);
