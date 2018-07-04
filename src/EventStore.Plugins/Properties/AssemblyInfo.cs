28873fa13cff64e96279f72b416f94b71ffe67ca
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2018-07-04 18:13:20 +0100

    fix AssemblyInfo

--- src/EventStore.Plugins/Properties/AssemblyInfo.cs
+++ src/EventStore.Plugins/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EventStore.Plugins")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Event Store LLP")]
 [assembly: AssemblyProduct("EventStore.Plugins")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
