d7dd56f285dfe5d36a76e5b5f66e177a56ecba97
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2018-07-16 10:44:04 +0100

    Fixed AssemblyInfo for the new EventStore.Plugins project

--- src/EventStore.Plugins/Properties/AssemblyInfo.cs
+++ src/EventStore.Plugins/Properties/AssemblyInfo.cs
@@ -1,35 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("EventStore.Plugins")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Event Store LLP")]
 [assembly: AssemblyProduct("EventStore.Plugins")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Event Store LLP. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+//NOTE: Do not change these, they will be modified during the build by TeamCity.
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f853ad1d-d328-4169-9a85-94d500844aff")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("ccc0be24-ec2c-4a7e-98ae-892648fb1e4c")]
