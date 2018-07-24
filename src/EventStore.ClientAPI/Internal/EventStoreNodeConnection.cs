62b8b1f1264d1524231888acc950620222b00aa7
Enrico Sada <enrico.sada@jet.com>
2018-07-24 16:06:20 +0200

    fix build - remove cref to SqlConnection (not reference in netstandard)

--- src/EventStore.ClientAPI/Internal/EventStoreNodeConnection.cs
+++ src/EventStore.ClientAPI/Internal/EventStoreNodeConnection.cs
@@ -12,7 +12,7 @@ namespace EventStore.ClientAPI.Internal
     /// Maintains a full duplex connection to the EventStore
     /// </summary>
     /// <remarks>
-    /// An <see cref="EventStoreConnection"/> operates quite differently than say a <see cref="SqlConnection"/>. Normally
+    /// An <see cref="EventStoreConnection"/> operates quite differently than say a SqlConnection. Normally
     /// when using an <see cref="EventStoreConnection"/> you want to keep the connection open for a much longer of time than
     /// when you use a SqlConnection. If you prefer the usage pattern of using(new Connection()) .. then you would likely
     /// want to create a FlyWeight on top of the <see cref="EventStoreConnection"/>.
