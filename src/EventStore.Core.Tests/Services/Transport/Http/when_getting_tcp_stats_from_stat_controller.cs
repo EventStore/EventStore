39c813f445f1be32fc7ec0bcaa3d0fb4812ca9ab
shaan1337 <sniper111@gmail.com>
2018-08-27 14:05:53 +0400

    Return port after use in several tests

--- src/EventStore.Core.Tests/Services/Transport/Http/when_getting_tcp_stats_from_stat_controller.cs
+++ src/EventStore.Core.Tests/Services/Transport/Http/when_getting_tcp_stats_from_stat_controller.cs
@@ -20,29 +20,31 @@ namespace EventStore.Core.Tests.Services.Transport.Http
     {
         private PortableServer _portableServer;
         private IPEndPoint _serverEndPoint;
+        private int _serverPort;
         private IEventStoreConnection _connection;
         private string _url;
         private string _clientConnectionName = "test-connection";
-        
+
         private List<MonitoringMessage.TcpConnectionStats> _results = new List<MonitoringMessage.TcpConnectionStats>();
         private HttpResponse _response;
-        
+
         protected override void Given()
         {
-            _serverEndPoint = new IPEndPoint(IPAddress.Loopback, PortsHelper.GetAvailablePort(IPAddress.Loopback));
+            _serverPort = PortsHelper.GetAvailablePort(IPAddress.Loopback);
+            _serverEndPoint = new IPEndPoint(IPAddress.Loopback, _serverPort);
             _url = _HttpEndPoint.ToHttpUrl(EndpointExtensions.HTTP_SCHEMA, "/stats/tcp");
-            
+
             var settings = ConnectionSettings.Create();
             _connection = EventStoreConnection.Create(settings, _node.TcpEndPoint, _clientConnectionName);
             _connection.ConnectAsync().Wait();
-            
+
             var testEvent = new EventData(Guid.NewGuid(),"TestEvent",true,Encoding.ASCII.GetBytes("{'Test' : 'OneTwoThree'}"),null);
             _connection.AppendToStreamAsync("tests",ExpectedVersion.Any,testEvent).Wait();
-            
+
             _portableServer = new PortableServer(_serverEndPoint);
             _portableServer.SetUp();
         }
-        
+
         protected override void When()
         {
             Func<HttpResponse, bool> verifier = response => {
@@ -50,7 +52,7 @@ namespace EventStore.Core.Tests.Services.Transport.Http
                 _response = response;
                 return true;
             };
-            
+
             var res = _portableServer.StartServiceAndSendRequest(y => {}, _url, verifier);
             Assert.IsEmpty(res.Item2, "Http call failed");
         }
@@ -66,13 +68,13 @@ namespace EventStore.Core.Tests.Services.Transport.Http
         {
             Assert.AreEqual(2, _results.Count(r => r.IsExternalConnection));
         }
-        
+
         [Test]
         public void should_return_the_total_number_of_bytes_sent_for_external_connections()
         {
             Assert.Greater(_results.Sum(r=> r.IsExternalConnection ? r.TotalBytesSent : 0), 0);
         }
-        
+
         [Test]
         public void should_return_the_total_number_of_bytes_received_from_external_connections()
         {
@@ -84,10 +86,11 @@ namespace EventStore.Core.Tests.Services.Transport.Http
         {
             Assert.IsTrue(_results.Any(x=>x.ClientConnectionName == _clientConnectionName));
         }
-        
+
         [OneTimeTearDown]
         public override void TestFixtureTearDown()
         {
+            PortsHelper.ReturnPort(_serverPort);
             _portableServer.TearDown();
             _connection.Dispose();
             base.TestFixtureTearDown();
