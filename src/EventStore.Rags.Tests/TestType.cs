f57698417798638f7f38373ae248e7e151870e9b
James Nugent <james@jen20.com>
2018-09-01 16:51:51 -0400

    rags: Use NuGet package for YAML.NET

--- src/EventStore.Rags.Tests/TestType.cs
+++ src/EventStore.Rags.Tests/TestType.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventStore.Rags.Tests
 {
