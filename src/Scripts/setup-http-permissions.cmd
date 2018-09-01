ec08161c126401811ee4cbe3c5bdd423b7c6eba0
James Nugent <james@jen20.com>
2018-09-01 15:29:56 -0500

    cleanup: Remove a bunch of old unused scripts

--- src/Scripts/setup-http-permissions.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-netsh http add urlacl url=http://127.0.0.1:2111/ user=%USERDOMAIN%\%USERNAME%
-netsh http add urlacl url=http://127.0.0.1:2112/ user=%USERDOMAIN%\%USERNAME%
-netsh http add urlacl url=http://127.0.0.1:2113/ user=%USERDOMAIN%\%USERNAME%
-netsh http add urlacl url=http://127.0.0.1:30777/ user=%USERDOMAIN%\%USERNAME%
