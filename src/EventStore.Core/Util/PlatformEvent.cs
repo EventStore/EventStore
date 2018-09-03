fa1a6d9000f345b015470d009ea2bb608db6caaa
James Nugent <james@jen20.com>
2018-09-03 19:44:40 -0400

    cleanup: Add PlatformEvent class

---
+++ src/EventStore.Core/Util/PlatformEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using EventStore.Common.Utils;
+
+namespace EventStore.Core.Util
+{
+    public class PlatformEvent
+    {
+        private readonly AutoResetEvent _monoEvent;
+        private readonly ManualResetEventSlim _windowsEvent;
+
+        public PlatformEvent()
+        {
+            if (Runtime.IsWindows)
+            {
+                _windowsEvent = new ManualResetEventSlim();
+            }
+            else
+            {
+                _monoEvent = new AutoResetEvent(false);
+            }
+        }
+
+        public bool Wait(TimeSpan timeout)
+        {
+            return _windowsEvent?.Wait(timeout) ?? _monoEvent.WaitOne(timeout);
+        }
+
+        public void Set()
+        {
+            if (_windowsEvent != null)
+            {
+                _windowsEvent.Set();
+                return;
+            }
+
+            _monoEvent.Set();
+        }
+
+        public void Reset()
+        {
+            if (_windowsEvent != null)
+            {
+                _windowsEvent.Reset();
+                return;
+            }
+
+            _monoEvent.Reset();
+        }
+    }
+}
