428746aac5133ab9a4e90f4184d4cebc498122c1
shaan1337 <sniper111@gmail.com>
2018-09-17 13:18:10 +0400

    Fix emitted_streams_tracker tests that were not waiting for events to be written before continuing

--- src/EventStore.Projections.Core.Tests/Services/emitted_streams_tracker/when_tracking/with_tracking_disabled.cs
+++ src/EventStore.Projections.Core.Tests/Services/emitted_streams_tracker/when_tracking/with_tracking_disabled.cs
@@ -1,11 +1,17 @@
-﻿using EventStore.Projections.Core.Services.Processing;
+﻿using EventStore.ClientAPI.SystemData;
+using EventStore.Projections.Core.Services.Processing;
 using NUnit.Framework;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EventStore.Projections.Core.Tests.Services.emitted_streams_tracker.when_tracking
 {
     public class with_tracking_disabled : SpecificationWithEmittedStreamsTrackerAndDeleter
     {
+        private CountdownEvent _eventAppeared = new CountdownEvent(1);
+        private UserCredentials _credentials = new UserCredentials("admin","changeit");
+
         protected override void Given()
         {
             _trackEmittedStreams = false;
@@ -14,19 +20,28 @@ namespace EventStore.Projections.Core.Tests.Services.emitted_streams_tracker.whe
 
         protected override void When()
         {
+            var sub = _conn.SubscribeToStreamAsync(_projectionNamesBuilder.GetEmittedStreamsName(), true, (s, evnt) => {
+                _eventAppeared.Signal();
+                return Task.CompletedTask;
+            }, userCredentials: _credentials).Result;
+
             _emittedStreamsTracker.TrackEmittedStream(new EmittedEvent[]
             {
                 new EmittedDataEvent(
                      "test_stream", Guid.NewGuid(),  "type1",  true,
                      "data",  null, CheckpointTag.FromPosition(0, 100, 50),  null, null)
             });
+
+            _eventAppeared.Wait(TimeSpan.FromSeconds(5));
+            sub.Unsubscribe();
         }
 
         [Test]
         public void should_write_a_stream_tracked_event()
         {
-            var result = _conn.ReadStreamEventsForwardAsync(_projectionNamesBuilder.GetEmittedStreamsName(), 0, 200, false, new EventStore.ClientAPI.SystemData.UserCredentials("admin", "changeit")).Result;
+            var result = _conn.ReadStreamEventsForwardAsync(_projectionNamesBuilder.GetEmittedStreamsName(), 0, 200, false, _credentials).Result;
             Assert.AreEqual(0, result.Events.Length);
+            Assert.AreEqual(1, _eventAppeared.CurrentCount); //no event appeared should get through
         }
     }
 }
--- src/EventStore.Projections.Core.Tests/Services/emitted_streams_tracker/when_tracking/with_tracking_enabled.cs
+++ src/EventStore.Projections.Core.Tests/Services/emitted_streams_tracker/when_tracking/with_tracking_enabled.cs
@@ -1,13 +1,18 @@
 ﻿using EventStore.ClientAPI.Common.Utils;
+using EventStore.ClientAPI.SystemData;
 using EventStore.Projections.Core.Services.Processing;
 using NUnit.Framework;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EventStore.Projections.Core.Tests.Services.emitted_streams_tracker.when_tracking
 {
     [TestFixture]
     public class with_tracking_enabled : SpecificationWithEmittedStreamsTrackerAndDeleter
     {
+        private CountdownEvent _eventAppeared = new CountdownEvent(1);
+        private UserCredentials _credentials = new UserCredentials("admin","changeit");
         protected override void Given()
         {
             base.Given();
@@ -15,20 +20,29 @@ namespace EventStore.Projections.Core.Tests.Services.emitted_streams_tracker.whe
 
         protected override void When()
         {
+            var sub = _conn.SubscribeToStreamAsync(_projectionNamesBuilder.GetEmittedStreamsName(), true, (s, evnt) => {
+                _eventAppeared.Signal();
+                return Task.CompletedTask;
+            }, userCredentials: _credentials).Result;
+
             _emittedStreamsTracker.TrackEmittedStream(new EmittedEvent[]
             {
                 new EmittedDataEvent(
                      "test_stream", Guid.NewGuid(),  "type1",  true,
                      "data",  null, CheckpointTag.FromPosition(0, 100, 50),  null, null)
             });
+
+            _eventAppeared.Wait(TimeSpan.FromSeconds(5));
+            sub.Unsubscribe();
         }
 
         [Test]
         public void should_write_a_stream_tracked_event()
         {
-            var result = _conn.ReadStreamEventsForwardAsync(_projectionNamesBuilder.GetEmittedStreamsName(), 0, 200, false, new EventStore.ClientAPI.SystemData.UserCredentials("admin", "changeit")).Result;
+            var result = _conn.ReadStreamEventsForwardAsync(_projectionNamesBuilder.GetEmittedStreamsName(), 0, 200, false, _credentials).Result;
             Assert.AreEqual(1, result.Events.Length);
             Assert.AreEqual("test_stream", Helper.UTF8NoBom.GetString(result.Events[0].Event.Data));
+            Assert.AreEqual(0, _eventAppeared.CurrentCount);
         }
     }
 }
