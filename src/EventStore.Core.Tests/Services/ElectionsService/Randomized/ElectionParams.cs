c97612739d58d3a19d170f622b0cebb09969fd5f
shaan1337 <sniper111@gmail.com>
2018-10-22 14:12:53 +0400

    Always run Election tests 50 times

--- src/EventStore.Core.Tests/Services/ElectionsService/Randomized/ElectionParams.cs
+++ src/EventStore.Core.Tests/Services/ElectionsService/Randomized/ElectionParams.cs
@@ -2,12 +2,7 @@ namespace EventStore.Core.Tests.Services.ElectionsService.Randomized
 {
     public static class ElectionParams
     {
-		#if CI_TESTS 
-			public const int TestRunCount = 10;
-		#else
-			public const int TestRunCount = 1;
-		#endif
-        
+		public const int TestRunCount = 50;
         public const int MaxIterationCount = 25000;
     }
 }
