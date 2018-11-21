cadfe50e7a2e3c2bf568ac0abd882abde23dbef3
shaan1337 <sniper111@gmail.com>
2018-11-21 15:52:35 +0400

    Remove unused scripts

--- check-test-results.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-test_result=$(grep result=\"Failed\" TestResult.xml)
-if [[ "$test_result" == "" ]] ; then
-    echo "All tests passed"
-    exit 0
-else
-    echo "There are test failures"
-    exit 1
-fi
--- run_tests.cmd
+++
@@ -1,1 +0,0 @@
-powershell -NoProfile -ExecutionPolicy Bypass -Command  "& '%~dp0tools\nunit-3.4.1\bin\nunit3-console.exe' '%~dp0bin\tests\EventStore.Core.Tests.dll';"
