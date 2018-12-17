d49496fe32941fefbe3cde47b39adbfd23c2bc29
shaan1337 <sniper111@gmail.com>
2018-12-17 08:38:40 +0400

    Add AssemblyInfo.cs file for EventStore.Rags

---
+++ src/EventStore.Rags/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EventStore.Rags")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Event Store LLP")]
+[assembly: AssemblyProduct("EventStore.Rags")]
+[assembly: AssemblyCopyright("Copyright © Event Store LLP. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//NOTE: Do not change these, they will be modified during the build
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("523e7fe2-f46e-4346-8a96-f8f775fd78db")]
