7856d0f327c58b4e1a04884e5d7eae0cd75b9480
shaan1337 <sniper111@gmail.com>
2018-12-17 08:50:42 +0400

    Add AssemblyInfo.cs for EventStore.Native

---
+++ src/EventStore.Native/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventStore.Native")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Event Store LLP")]
+[assembly: AssemblyProduct("EventStore.Native")]
+[assembly: AssemblyCopyright("Copyright © Event Store LLP. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//NOTE: Do not change these, they will be modified during the build
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8c747fbf-987b-42b2-8284-0fa44a0131be")]
