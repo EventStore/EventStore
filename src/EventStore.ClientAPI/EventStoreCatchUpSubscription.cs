278061f1eafca391908190ed1030eea43057960c
Chris Condron <chris@condron.net>
2019-04-16 19:26:27 -0400

    clear drop data and flag on reconnection

--- src/EventStore.ClientAPI/EventStoreCatchUpSubscription.cs
+++ src/EventStore.ClientAPI/EventStoreCatchUpSubscription.cs
@@ -180,6 +180,9 @@ namespace EventStore.ClientAPI {
 			if (Verbose)
 				Log.Debug("Catch-up Subscription {0} to {1}: unhooking from connection.Connected.", SubscriptionName,
 					IsSubscribedToAll ? "<all>" : StreamId);
+			Interlocked.Exchange(ref _dropData, null); //clear drop 
+			Interlocked.Exchange(ref _isDropped, 0);
+			
 			_connection.Connected -= OnReconnect;
 			RunSubscriptionAsync();
 		}
