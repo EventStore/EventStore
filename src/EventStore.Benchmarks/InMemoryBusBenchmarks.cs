45dacb99a4a0de6e4b891d95067b7ec9b519c1d3
James Geall <jageall@gmail.com>
2019-04-30 10:17:08 +0300

    Adds benchmark project for microbenchmarking  - Adds benchmark for message dispatching

---
+++ src/EventStore.Benchmarks/InMemoryBusBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using BenchmarkDotNet.Attributes;
+using EventStore.Core.Bus;
+using EventStore.Core.Messaging;
+
+namespace EventStore.Benchmarks
+{
+	public class InMemoryBusBenchmarks {
+		private readonly InMemoryBus _bus;
+		private readonly SingleHierarchy _singleHierarchy;
+
+		public InMemoryBusBenchmarks() {
+			_bus = new InMemoryBus("test");
+			_bus.Subscribe(new NoopHandler<Message>());
+			_singleHierarchy = new SingleHierarchy();
+		}
+
+		[Benchmark(OperationsPerInvoke = 10000)]
+		public void DispatchSingleHierarchy() {
+			for (var i = 0; i < 10000; i++) _bus.Handle(_singleHierarchy);
+		}
+
+		private class SingleHierarchy : Message {
+			private static readonly int TypeId = Interlocked.Increment(ref NextMsgId);
+			public override int MsgTypeId => TypeId;
+		}
+
+		private class NoopHandler<T> : IHandle<T> where T : Message {
+			public void Handle(T msg) {
+			}
+		}
+	}
+}
---
+++ src/EventStore.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace EventStore.Benchmarks {
+	internal class Program {
+		private static void Main(string[] args) {
+			BenchmarkRunner.Run(typeof(Program).Assembly);
+		}
+	}
+}
