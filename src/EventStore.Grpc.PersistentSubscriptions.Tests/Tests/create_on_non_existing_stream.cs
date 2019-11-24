18eb35431671ae245a2b91402a19fd297c73cb18
chris.condron <chris@condron.net>
2019-11-24 15:56:45 -0500

    Remove redundant file naming in Persisent Subscription tests to avoid windows 260 char path limits

---
+++ src/EventStore.Grpc.PersistentSubscriptions.Tests/Tests/create_on_non_existing_stream.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EventStore.Grpc.PersistentSubscriptions {
+	public class create_on_non_existing_stream
+		: IClassFixture<create_on_non_existing_stream.Fixture> {
+		public create_on_non_existing_stream(Fixture fixture) {
+			_fixture = fixture;
+		}
+
+		private const string Stream = nameof(create_on_non_existing_stream);
+		private readonly Fixture _fixture;
+
+		public class Fixture : EventStoreGrpcFixture {
+			protected override Task Given() => Task.CompletedTask;
+			protected override Task When() => Task.CompletedTask;
+		}
+
+		[Fact]
+		public async Task the_completion_succeeds() {
+			await _fixture.Client.PersistentSubscriptions.CreateAsync(Stream, "nonexistinggroup",
+				new PersistentSubscriptionSettings(),
+				TestCredentials.Root);
+		}
+	}
+}
