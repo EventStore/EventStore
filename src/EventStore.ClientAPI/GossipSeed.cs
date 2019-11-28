171c72c8fce824b9f71cf5ab515f4a89c0710726
Pieter Germishuys <pieter@pieterg.com>
2019-11-28 13:56:12 +0100

    Provide the ability for DnsEndpoint to be used
    Given a valid certificate for `localhost` the least amount of change would be to provide the ability to use DnsEndpoint.

--- src/EventStore.ClientAPI/GossipSeed.cs
+++ src/EventStore.ClientAPI/GossipSeed.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace EventStore.ClientAPI {
 	/// <summary>
@@ -6,16 +7,16 @@ namespace EventStore.ClientAPI {
 	/// </summary>
 	public class GossipSeed {
 		/// <summary>
-		/// The <see cref="IPEndPoint"/> for the External HTTP endpoint of the gossip seed.
+		/// The <see cref="EndPoint"/> for the External HTTP endpoint of the gossip seed.
 		///
 		/// The HTTP endpoint is used rather than the TCP endpoint because it is required
 		/// for the client to exchange gossip with the server. The standard port which should be
 		/// used here is 2113.
 		/// </summary>
-		public readonly IPEndPoint EndPoint;
+		public readonly EndPoint EndPoint;
 
 		/// <summary>
-		/// If Gossip should be requested
+		/// If Gossip should be requested over Tls
 		/// </summary>
 		public readonly bool SeedOverTls;
 
@@ -27,10 +28,10 @@ namespace EventStore.ClientAPI {
 		/// <summary>
 		/// Creates a new <see cref="GossipSeed" />.
 		/// </summary>
-		/// <param name="endPoint">The <see cref="IPEndPoint"/> for the External HTTP endpoint of the gossip seed. The standard port is 2113.</param>
+		/// <param name="endPoint">The <see cref="EndPoint"/> for the External HTTP endpoint of the gossip seed. The standard port is 2113.</param>
 		/// <param name="hostHeader">The host header to be sent when requesting gossip. Defaults to String.Empty</param>
 		/// <param name="seedOverTls">Specifies that eventstore should use https when connecting to gossip</param>
-		public GossipSeed(IPEndPoint endPoint, string hostHeader = "", bool seedOverTls = true) {
+		public GossipSeed(EndPoint endPoint, string hostHeader = "", bool seedOverTls = true) {
 			EndPoint = endPoint;
 			HostHeader = hostHeader;
 			SeedOverTls = seedOverTls;
