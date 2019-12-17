772e2fd703d617d59b321333fccc4c697aacf3dc
danielmarbach <daniel.marbach@openplace.net>
2019-12-17 11:01:00 +0100

    Dispose TokenRegistrations

--- src/EventStore.Core/Services/Transport/Grpc/Enumerators.AllSubscriptionFiltered.cs
+++ src/EventStore.Core/Services/Transport/Grpc/Enumerators.AllSubscriptionFiltered.cs
@@ -22,7 +22,7 @@ namespace EventStore.Core.Services.Transport.Grpc {
 			private readonly IReadIndex _readIndex;
 			private readonly CancellationTokenSource _disposedTokenSource;
 			private readonly ConcurrentQueue<ResolvedEvent> _buffer;
-
+			private readonly CancellationTokenRegistration _tokenRegistration;
 			private Position _nextPosition;
 			private ResolvedEvent _current;
 
@@ -55,11 +55,12 @@ namespace EventStore.Core.Services.Transport.Grpc {
 				_readIndex = readIndex;
 				_disposedTokenSource = new CancellationTokenSource();
 				_buffer = new ConcurrentQueue<ResolvedEvent>();
-				cancellationToken.Register(_disposedTokenSource.Dispose);
+				_tokenRegistration = cancellationToken.Register(_disposedTokenSource.Dispose);
 			}
 
 			public ValueTask DisposeAsync() {
 				_disposedTokenSource.Dispose();
+				_tokenRegistration.Dispose();
 				return default;
 			}
 
