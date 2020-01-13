77abd7f5f2dd04d79c9510d8041d0fe1ce8557fe
chris.condron <chris@condron.net>
2020-01-13 14:39:23 -0500

    fix chaser tests

--- src/EventStore.Core.Tests/Services/Storage/Chaser/when_chaser_reads_system_event.cs
+++ src/EventStore.Core.Tests/Services/Storage/Chaser/when_chaser_reads_system_event.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Linq;
-using EventStore.Core.Messages;
 using EventStore.Core.TransactionLog.LogRecords;
 using NUnit.Framework;
 
 namespace EventStore.Core.Tests.Services.Storage.Chaser {
 	[TestFixture]
 	public class when_chaser_reads_system_event : with_storage_chaser_service {
-		private long _logPosition;
 		private Guid _epochId;
 		private int _epochNumber;
 
@@ -18,15 +15,8 @@ namespace EventStore.Core.Tests.Services.Storage.Chaser {
 			var rec = new SystemLogRecord(epoch.EpochPosition, epoch.TimeStamp, SystemRecordType.Epoch,
 				SystemRecordSerialization.Json, epoch.AsSerialized());
 
-			Assert.True(Writer.Write(rec, out _logPosition));
+			Assert.True(Writer.Write(rec, out _));
 			Writer.Flush();
-		}
-		[Test]
-		public void log_written_should_be_published() {
-			Assert.Fail("Fix Test");
-			//AssertEx.IsOrBecomesTrue(() => LogWrittenTos.Count == 1, msg: "LogWrittenTo msg not recieved");
-			//var writtenTo = LogWrittenTos[0];
-			//Assert.AreEqual(_logPosition, writtenTo.LogPosition);
 		}
 		[Test]
 		public void epoch_should_be_updated() {
