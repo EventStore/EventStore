9aac762c0ea642349896fa1e13ac800e79c6f8c0
shaan1337 <sniper111@gmail.com>
2020-01-29 09:39:56 +0400

    Add regression test to TcpConnectionSsl (server)

--- src/EventStore.Core.Tests/Services/Transport/Tcp/TcpConnectionSslTests.cs
+++ src/EventStore.Core.Tests/Services/Transport/Tcp/TcpConnectionSslTests.cs
@@ -86,6 +86,56 @@ namespace EventStore.Core.Tests.Services.Transport.Tcp {
 			}
 		}
 
+		[Test, Timeout(120000)]
+		public void when_connection_closed_quickly_socket_should_be_properly_disposed() {
+			for (int i = 0; i < 1000; i++) {
+				var listeningSocket = CreateListeningSocket();
+				ITcpConnection clientTcpConnection = null;
+				ITcpConnection serverTcpConnection = null;
+				Socket serverSocket = null;
+				try {
+					ManualResetEventSlim mre = new ManualResetEventSlim(false);
+
+					clientTcpConnection = TcpConnectionSsl.CreateConnectingConnection(
+						Guid.NewGuid(),
+						(IPEndPoint)listeningSocket.LocalEndPoint,
+						"localhost",
+						false,
+						new TcpClientConnector(),
+						TimeSpan.FromSeconds(5),
+						(conn) => {},
+						(conn, error) => {},
+						false);
+
+					clientTcpConnection.ConnectionClosed += (conn, error) => {
+						mre.Set();
+					};
+
+					serverSocket = listeningSocket.Accept();
+					clientTcpConnection.Close("Intentional close");
+					serverTcpConnection = TcpConnectionSsl.CreateServerFromSocket(Guid.NewGuid(),
+						(IPEndPoint)serverSocket.RemoteEndPoint, serverSocket, GetCertificate(), false);
+
+					mre.Wait(TimeSpan.FromSeconds(10));
+					SpinWait.SpinUntil(() => serverTcpConnection.IsClosed, TimeSpan.FromSeconds(10));
+
+					var disposed = false;
+					try {
+						int x = serverSocket.Available;
+					} catch (ObjectDisposedException) {
+						disposed = true;
+					}
+
+					Assert.AreEqual(true, disposed);
+				} finally {
+					clientTcpConnection?.Close("Shut down");
+					serverTcpConnection?.Close("Shut down");
+					listeningSocket.Dispose();
+					serverSocket?.Dispose();
+				}
+			}
+		}
+
 		private X509Certificate GetCertificate() {
 			using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("EventStore.Core.Tests.server.p12");
 			using var mem = new MemoryStream();
