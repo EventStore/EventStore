2c89b5c3ef330544bb36fc3745764faf7eb7e6ff
shaan1337 <sniper111@gmail.com>
2020-01-31 10:09:38 +0400

    Prevent client operations except writes on the internal TCP channel. Allowing writes is necessary to allow forwarding of write messages from slave to master over the internal TCP channel.

--- src/EventStore.Core/Services/Transport/Tcp/InternalTcpDispatcher.cs
+++ src/EventStore.Core/Services/Transport/Tcp/InternalTcpDispatcher.cs
@@ -10,7 +10,7 @@ using EventStore.Core.TransactionLog.Chunks;
 using EventStore.Core.TransactionLog.LogRecords;
 
 namespace EventStore.Core.Services.Transport.Tcp {
-	public class InternalTcpDispatcher : ClientTcpDispatcher {
+	public class InternalTcpDispatcher : ClientWriteTcpDispatcher {
 		public InternalTcpDispatcher() {		
 			AddUnwrapper(TcpCommand.LeaderReplicatedTo, UnwrapReplicatedTo, ClientVersion.V2);
 			AddWrapper<ReplicationTrackingMessage.ReplicatedTo>(WrapReplicatedTo, ClientVersion.V2);
