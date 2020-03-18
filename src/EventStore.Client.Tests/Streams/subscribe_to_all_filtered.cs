a2cee3f60e47be5750aec5fae36b194f9e7fbb19
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2020-03-18 11:15:44 +0100

    fix bug where checkpoint reached not invoked during catchup

--- src/EventStore.Client.Tests/Streams/subscribe_to_all_filtered.cs
+++ src/EventStore.Client.Tests/Streams/subscribe_to_all_filtered.cs
@@ -19,6 +19,66 @@ namespace EventStore.Client.Streams {
 		public static IEnumerable<object[]> FilterCases() => Filters.All.Select(filter => new object[] {filter});
 
 		[Theory, MemberData(nameof(FilterCases))]
+		public async Task reads_all_existing_events(string filterName) {
+			var streamPrefix = _fixture.GetStreamName();
+			var (getFilter, prepareEvent) = Filters.GetFilter(filterName);
+
+			var appeared = new TaskCompletionSource<bool>();
+			var dropped = new TaskCompletionSource<(SubscriptionDroppedReason, Exception)>();
+			var checkpointSeen = new TaskCompletionSource<bool>();
+			var filter = getFilter(streamPrefix);
+			var events = _fixture.CreateTestEvents(20).Select(e => prepareEvent(streamPrefix, e))
+				.ToArray();
+
+			using var enumerator = events.OfType<EventData>().GetEnumerator();
+			enumerator.MoveNext();
+
+			foreach (var e in events) {
+				await _fixture.Client.AppendToStreamAsync($"{streamPrefix}_{Guid.NewGuid():n}",
+					AnyStreamRevision.NoStream, new[] {e});
+			}
+
+			using var subscription = await _fixture.Client.SubscribeToAllAsync(EventAppeared, false,
+				filterOptions: new SubscriptionFilterOptions(filter, 5, CheckpointReached),
+				subscriptionDropped: SubscriptionDropped);
+
+			await Task.WhenAll(appeared.Task, checkpointSeen.Task).WithTimeout();
+
+			Assert.False(dropped.Task.IsCompleted);
+
+			subscription.Dispose();
+
+			var (reason, ex) = await dropped.Task.WithTimeout();
+
+			Assert.Equal(SubscriptionDroppedReason.Disposed, reason);
+			Assert.Null(ex);
+
+			Task EventAppeared(StreamSubscription _, ResolvedEvent e, CancellationToken ct) {
+				try {
+					Assert.Equal(enumerator.Current.EventId, e.OriginalEvent.EventId);
+					if (!enumerator.MoveNext()) {
+						appeared.TrySetResult(true);
+					}
+				} catch (Exception ex) {
+					appeared.TrySetException(ex);
+					throw;
+				}
+
+				return Task.CompletedTask;
+			}
+
+			void SubscriptionDropped(StreamSubscription s, SubscriptionDroppedReason reason, Exception ex) =>
+				dropped.SetResult((reason, ex));
+
+			Task CheckpointReached(StreamSubscription _, Position position, CancellationToken ct) {
+				checkpointSeen.TrySetResult(true);
+
+				return Task.CompletedTask;
+			}
+		}
+
+
+		[Theory, MemberData(nameof(FilterCases))]
 		public async Task reads_all_existing_events_and_keep_listening_to_new_ones(string filterName) {
 			var streamPrefix = _fixture.GetStreamName();
 			var (getFilter, prepareEvent) = Filters.GetFilter(filterName);
