7d237882840676f9106bb61d69770c4b2f3c004e
Chris Ward <chris.ward@eventstore.com>
2020-03-25 15:56:52 +0100

    Add docs for connectivity settings

--- src/EventStore.Client/EventStoreClientConnectivitySettings.cs
+++ src/EventStore.Client/EventStoreClientConnectivitySettings.cs
@@ -3,16 +3,27 @@ using System.Net;
 
 namespace EventStore.Client {
 	public class EventStoreClientConnectivitySettings {
+		/// <summary>
+		/// URI of a cluster
+		/// </summary>
 		public Uri Address { get; set; } = new UriBuilder {
 			Scheme = Uri.UriSchemeHttps,
 			Port = 2113
 		}.Uri;
+
+		/// <summary>
+		/// The maximum number of attempts for discovering endpoints.
+		/// </summary>
 		public int MaxDiscoverAttempts { get; set; }
 
+		/// <summary>
+		/// Endpoints for seeding gossip if not using DNS.
+		/// </summary>
 		public EndPoint[] GossipSeeds {
 			get {
 				object endpoints = (object)DnsGossipSeeds ?? IpGossipSeeds;
-				return endpoints switch {
+				return endpoints switch
+				{
 					DnsEndPoint[] dns => Array.ConvertAll<DnsEndPoint, EndPoint>(dns, x => x),
 					IPEndPoint[] ip => Array.ConvertAll<IPEndPoint, EndPoint>(ip, x => x),
 					_ => Array.Empty<EndPoint>()
@@ -20,10 +31,29 @@ namespace EventStore.Client {
 			}
 		}
 
+		/// <summary>
+		/// Endpoints for seeding gossip when using DNS.
+		/// </summary>
 		public DnsEndPoint[] DnsGossipSeeds { get; set; }
+
+		/// <summary>
+		/// Endpoints for seeding gossip if using IP adresses.
+		/// </summary>
 		public IPEndPoint[] IpGossipSeeds { get; set; }
+
+		/// <summary>
+		/// Timeout for cluster gossip.
+		/// </summary>
 		public TimeSpan GossipTimeout { get; set; }
+
+		/// <summary>
+		/// Interval between discovering each node.
+		/// </summary>
 		public TimeSpan DiscoveryInterval { get; set; }
+
+		/// <summary>
+		/// Prefer a randomly selected node. 
+		/// </summary>
 		public NodePreference NodePreference { get; set; }
 
 		public static EventStoreClientConnectivitySettings Default => new EventStoreClientConnectivitySettings {
