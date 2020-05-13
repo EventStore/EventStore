b580ea906626b47079271074ae320f2e057eaa5c
Avish Cheetaram <avishcheetaram.ai@gmail.com>
2020-05-13 12:41:50 +0400

    forbid the same index and database path from being specified
    Update src/EventStore.ClusterNode/ClusterVNodeHostedService.cs
    
    Co-authored-by: Pieter Germishuys <pieter@pieterg.com>

--- src/EventStore.ClusterNode/ClusterVNodeHostedService.cs
+++ src/EventStore.ClusterNode/ClusterVNodeHostedService.cs
@@ -83,6 +83,13 @@ namespace EventStore.ClusterNode {
 					"The given database path starts with a '~'. Event Store does not expand '~'.");
 			}
 
+			string absolutePathIndex = Path.GetFullPath(options.Index);
+			string absolutePathDb = Path.GetFullPath(options.Db);
+			if(absolutePathDb.Equals(absolutePathIndex))
+			{
+				throw new ApplicationInitializationException($"The given database ({absolutePathDb}) and index ({absolutePathIndex}) paths cannot point to the same directory.");
+			}
+
 			if (options.Log.StartsWith("~")) {
 				throw new ApplicationInitializationException(
 					"The given log path starts with a '~'. Event Store does not expand '~'.");
