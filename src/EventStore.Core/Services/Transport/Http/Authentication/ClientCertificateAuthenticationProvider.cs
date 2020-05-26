70c6969dc438567bca8f8218f13379e943ee8c02
shaan1337 <sniper111@gmail.com>
2020-05-26 11:09:27 +0400

    Require an IP or DNS SAN to be present in a client certificate to be authenticated as system user

--- src/EventStore.Core/Services/Transport/Http/Authentication/ClientCertificateAuthenticationProvider.cs
+++ src/EventStore.Core/Services/Transport/Http/Authentication/ClientCertificateAuthenticationProvider.cs
@@ -1,14 +1,42 @@
-﻿using EventStore.Core.Services.UserManagement;
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using EventStore.Core.Services.UserManagement;
 using Microsoft.AspNetCore.Http;
 
 namespace EventStore.Core.Services.Transport.Http.Authentication {
 	public class ClientCertificateAuthenticationProvider : IHttpAuthenticationProvider {
 		public bool Authenticate(HttpContext context, out HttpAuthenticationRequest request) {
 			request = null;
-			if (context.Connection.ClientCertificate is null) return false;
-			request = new HttpAuthenticationRequest(context, "system", "");
-			request.Authenticated(SystemAccounts.System);
-			return true;
+			var clientCertificate = context.Connection.ClientCertificate;
+			if (clientCertificate is null) return false;
+
+			X509ExtensionCollection extensions;
+			try {
+				extensions = clientCertificate.Extensions;
+			} catch (CryptographicException) {
+				return false;
+			}
+
+			foreach (var extension in extensions) {
+				AsnEncodedData asnData = new AsnEncodedData(extension.Oid, extension.RawData);
+				if (extension.Oid.Value == "2.5.29.17") { //Oid for Subject Alternative Names extension
+					var data = asnData.Format(false);
+					string[] parts = data.Split(new[] {':', '=', ','}, StringSplitOptions.RemoveEmptyEntries);
+					var acceptedHeaders = new[] {"DNS", "DNS Name", "IP", "IP Address"};
+					for (int i = 0; i < parts.Length; i += 2) {
+						var header = parts[i].Trim();
+						if (acceptedHeaders.Any(x => x == header)) {
+							request = new HttpAuthenticationRequest(context, "system", "");
+							request.Authenticated(SystemAccounts.System);
+							return true;
+						}
+					}
+				}
+			}
+
+			return false;
 		}
 	}
 }
