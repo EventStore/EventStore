ffd98c560cd3088246dbea4ee7b5c598474960be
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2020-05-28 07:52:10 +0200

    add failing test for ExclusiveDbLock

---
+++ src/EventStore.Core.Tests/ExclusiveDbLockTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests {
+	[TestFixture]
+	public class ExclusiveDbLockTests {
+		[Test]
+		public async Task can_release_when_running_in_task_pool() {
+			var dbPath = $"/tmp/eventstore/{Guid.NewGuid()}";
+			var sut = new ExclusiveDbLock(dbPath);
+			Assert.True(sut.Acquire());
+			Assert.True(sut.IsAcquired);
+			await Task.Delay(1);
+			sut.Release();
+		}
+	}
+}
