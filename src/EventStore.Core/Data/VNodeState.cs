eadfd9465576f853926299a66749e77fedb34a40
shaan1337 <sniper111@gmail.com>
2020-05-29 18:34:05 +0400

    Add warning about modifying VNodeState enum and assign explicit values Add DiscoverLeader state to cluster.proto

--- src/EventStore.Core/Data/VNodeState.cs
+++ src/EventStore.Core/Data/VNodeState.cs
@@ -1,21 +1,22 @@
 namespace EventStore.Core.Data {
+	//WARNING: new states must be added at the bottom of the enum otherwise it may break cluster and client compatibility
 	public enum VNodeState {
-		Initializing,
-		DiscoverLeader,
-		Unknown,
-		PreReplica,
-		CatchingUp,
-		Clone,
-		Follower,
-		PreLeader,
-		Leader,
-		Manager,
-		ShuttingDown,
-		Shutdown,
-		ReadOnlyLeaderless,
-		PreReadOnlyReplica,
-		ReadOnlyReplica,
-		ResigningLeader
+		Initializing = 0,
+		DiscoverLeader = 1,
+		Unknown = 2,
+		PreReplica = 3,
+		CatchingUp = 4,
+		Clone = 5,
+		Follower = 6,
+		PreLeader = 7,
+		Leader = 8,
+		Manager = 9,
+		ShuttingDown = 10,
+		Shutdown = 11,
+		ReadOnlyLeaderless = 12,
+		PreReadOnlyReplica = 13,
+		ReadOnlyReplica = 14,
+		ResigningLeader = 15
 	}
 
 	public static class VNodeStateExtensions {
