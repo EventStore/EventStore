326c1f4233aa8d09435502b1f6e33deaa2559f1c
Pieter Germishuys <pieter@pieterg.com>
2020-06-30 19:50:11 +0200

    Do not print stack trace when invalid configuration is encountered
    - This improves invalid configuration output and makes it easier to spot
    what the actual issue is.
    - This also ensures that an invalid configuration is seen as a graceful
    shutdown rather than the server terminating unexpectedly.

---
+++ src/EventStore.Common/Exceptions/InvalidConfigurationException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace EventStore.Common.Exceptions {
+	public class InvalidConfigurationException : Exception {
+		public InvalidConfigurationException(string message) : base(message) { }
+	}
+}
