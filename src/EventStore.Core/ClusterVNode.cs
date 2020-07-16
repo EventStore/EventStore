4c8d7c7053d4fd5d808b7243cd5208ace598ba9c
shaan1337 <sniper111@gmail.com>
2020-07-16 15:53:04 +0400

    Add ability to reload the default log level by triggering /admin/reloadconfig endpoint or with a SIGHUP signal

--- src/EventStore.Core/ClusterVNode.cs
+++ src/EventStore.Core/ClusterVNode.cs
@@ -40,6 +40,7 @@ using EventStore.Core.Services.PersistentSubscription.ConsumerStrategy;
 using System.Threading.Tasks;
 using EventStore.Common.Exceptions;
 using EventStore.Common.Log;
+using EventStore.Common.Options;
 using EventStore.Core.Authorization;
 using EventStore.Core.Cluster;
 using EventStore.Native.UnixSignalManager;
@@ -943,6 +944,7 @@ namespace EventStore.Core {
 			Task.Run(() => {
 				try {
 					var options = _vNodeSettings.LoadConfigFunc();
+					ReloadLogOptions(options);
 					ReloadCertificates(options);
 					Log.Information("The node's configuration was successfully reloaded");
 				} catch (Exception exc) {
@@ -951,6 +953,19 @@ namespace EventStore.Core {
 					Interlocked.Exchange(ref _reloadingConfig, 0);
 				}
 			});
+		}
+
+		private void ReloadLogOptions(ClusterNodeOptions options) {
+			if (options.LogLevel != LogLevel.Default) {
+				var changed = EventStoreLoggerConfiguration.AdjustMinimumLogLevel(options.LogLevel);
+				if (changed) {
+					Log.Information($"The log level was adjusted to: {options.LogLevel}");
+
+					if (options.LogLevel > LogLevel.Information) {
+						Console.WriteLine($"The log level was adjusted to: {options.LogLevel}");
+					}
+				}
+			}
 		}
 
 		private void ReloadCertificates(ClusterNodeOptions options) {
