c482db272a9796c95c6de364a383ffa4a4d27f1d
shaan1337 <sniper111@gmail.com>
2020-09-17 14:47:50 +0400

    Add HasMergedAny and CanMergeAny properties to MergeResult

--- src/EventStore.Core/Index/MergeResult.cs
+++ src/EventStore.Core/Index/MergeResult.cs
@@ -4,10 +4,14 @@ namespace EventStore.Core.Index {
 	public class MergeResult {
 		public readonly IndexMap MergedMap;
 		public readonly List<PTable> ToDelete;
+		public readonly bool HasMergedAny;
+		public readonly bool CanMergeAny;
 
-		public MergeResult(IndexMap mergedMap, List<PTable> toDelete) {
+		public MergeResult(IndexMap mergedMap, List<PTable> toDelete, bool hasMergedAny, bool canMergeAny) {
 			MergedMap = mergedMap;
 			ToDelete = toDelete;
+			HasMergedAny = hasMergedAny;
+			CanMergeAny = canMergeAny;
 		}
 	}
 }
