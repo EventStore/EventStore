df7cf6ffe1a3ed890b7315fca88524a547a3ce92
shaan1337 <sniper111@gmail.com>
2021-01-05 10:23:15 +0400

    Proactively send heartbeat requests to the remote party to generate some data on the connection if we have not sent any data within a heartbeat interval This is particularly useful if the remote party's heartbeat request is stuck behind a large, multi-megabyte TCP message.
    Note: The heartbeat interval logic had to be changed to always trigger a heartbeat check on each heartbeat interval.
    Previously, if a heartbeat request was in progress, we would schedule another heartbeat check only after the heartbeat timeout expires.

--- src/EventStore.Core.Tests/Services/Transport/Tcp/TcpConnectionManagerTests.cs
+++ src/EventStore.Core.Tests/Services/Transport/Tcp/TcpConnectionManagerTests.cs
@@ -265,6 +265,8 @@ namespace EventStore.Core.Tests.Services.Transport.Tcp {
 			get { return _clientConnectionName; }
 		}
 
+		public long TotalBytesSent { get; }
+
 		public bool IsClosed {
 			get { return false; }
 		}
