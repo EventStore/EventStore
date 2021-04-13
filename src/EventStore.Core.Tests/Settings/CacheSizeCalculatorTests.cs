77794287a9e5acb1da53352a9b01a94c717b33ec
Timothy Coleman <timothy.coleman@gmail.com>
2021-04-13 11:44:48 +0100

    improve dynamic cache calculation
    - increase linearly after 2gb mem
    - tests
    - code clarity

---
+++ src/EventStore.Core.Tests/Settings/CacheSizeCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using EventStore.Core.Settings;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.Settings {
+	[TestFixture]
+	public class CacheSizeCalculatorTests {
+		const ulong Gibibyte = CacheSizeCalculator.Gibibyte;
+
+		[TestCase]
+		public void configured_takes_precedence() => Test(configuredCapacity: 1, mem: Gibibyte, expected: 1);
+
+		[TestCase]
+		public void enforces_minimum() => Test(configuredCapacity: 0, mem: Gibibyte, expected: 100_000);
+
+		[TestCase]
+		public void just_before_threshold() => Test(configuredCapacity: 0, mem: 2 * Gibibyte, expected: 100_000);
+
+		[TestCase]
+		public void onek_above_threshold() => Test(configuredCapacity: 0, mem: 2 * Gibibyte + 1024, expected: 100_001);
+
+		[TestCase]
+		public void at_01gib() => Test(configuredCapacity: 0, mem: 1 * Gibibyte, expected: 100_000);
+
+		[TestCase]
+		public void at_02gib() => Test(configuredCapacity: 0, mem: 2 * Gibibyte, expected: 100_000);
+
+		[TestCase]
+		public void at_04gib() => Test(configuredCapacity: 0, mem: 4 * Gibibyte, expected: 2_197_152);
+
+		[TestCase]
+		public void at_08gib() => Test(configuredCapacity: 0, mem: 8 * Gibibyte, expected: 6_391_456);
+
+		[TestCase]
+		public void at_16gib() => Test(configuredCapacity: 0, mem: 16 * Gibibyte, expected: 14_780_064);
+
+		[TestCase]
+		public void at_32gib() => Test(configuredCapacity: 0, mem: 32 * Gibibyte, expected: 31_557_280);
+
+		public static void Test(int configuredCapacity, ulong mem, int expected) =>
+			Assert.AreEqual(expected, CacheSizeCalculator.CalculateStreamInfoCacheCapacity(configuredCapacity, mem));
+	}
+}
---
+++ src/EventStore.Core/Settings/CacheSizeCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace EventStore.Core.Settings {
+	public static class CacheSizeCalculator {
+		// each time we increase the capacity by 1 we need about this much more memory
+		const ulong BytesPerUnitCapacity = 1024;
+		public const long Gibibyte = 1L * 1024 * 1024 * 1024;
+
+		public static int CalculateStreamInfoCacheCapacity(int configuredCapacity, ulong availableMemoryBytes) {
+			if (configuredCapacity > 0)
+				return configuredCapacity;
+
+			// determine cache sizes based on available memory
+			// always have at least 100k
+			const int baseCapacity = 100_000;
+
+			// grow the capacity linearly with memory above 2gib
+			if (availableMemoryBytes <= 2 * Gibibyte)
+				return baseCapacity;
+
+			availableMemoryBytes -= 2 * Gibibyte;
+
+			// allow 1kib for each stream. nb the memory will only be used on demand
+			var extraCapacity = (int)(availableMemoryBytes / BytesPerUnitCapacity);
+
+			return baseCapacity + extraCapacity;
+		}
+	}
+}
