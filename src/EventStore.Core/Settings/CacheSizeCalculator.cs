379e353e4c645b84422193698ad9bbea90306e51
Timothy Coleman <timothy.coleman@gmail.com>
2021-04-13 15:53:14 +0100

    rename gibi -> giga since i changed the constant

--- src/EventStore.Core/Settings/CacheSizeCalculator.cs
+++ src/EventStore.Core/Settings/CacheSizeCalculator.cs
@@ -2,7 +2,7 @@
 	public static class CacheSizeCalculator {
 		// each time we increase the capacity by 1 we need about this much more memory
 		const ulong BytesPerUnitCapacity = 1000;
-		public const long Gibibyte = 1L * 1000 * 1000 * 1000;
+		public const long Gigabyte = 1L * 1000 * 1000 * 1000;
 
 		public static int CalculateStreamInfoCacheCapacity(int configuredCapacity, ulong availableMemoryBytes) {
 			if (configuredCapacity > 0)
@@ -10,11 +10,11 @@
 
 			var estimatedSystemMem = availableMemoryBytes + 800 * 1000 * 1000;
 
-			if (estimatedSystemMem <= 1 * Gibibyte)
+			if (estimatedSystemMem <= 1 * Gigabyte)
 				return 100_000;
 
 			double proportionOfMem;
-			if (estimatedSystemMem <= 4 * Gibibyte)
+			if (estimatedSystemMem <= 4 * Gigabyte)
 				proportionOfMem = 0.5;
 			else
 				proportionOfMem = 0.75;
