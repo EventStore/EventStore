043a543e2e116caf74dde4a4895faff540ea132e
Timothy Coleman <timothy.coleman@gmail.com>
2021-06-08 14:47:11 +0100

    adjust checkpoint name

--- src/EventStore.Core/TransactionLog/Checkpoint/Checkpoint.cs
+++ src/EventStore.Core/TransactionLog/Checkpoint/Checkpoint.cs
@@ -7,6 +7,6 @@ namespace EventStore.Core.TransactionLog.Checkpoint {
 		public const string Truncate = "truncate";
 		public const string Replication = "replication";
 		public const string Index = "index";
-		public const string StreamExistenceFilter = "streamexistence";
+		public const string StreamExistenceFilter = "streamExistenceFilter";
 	}
 }
