668a98b43c5b3d0d045c3ee4019c156ec4cae829
StevenBlair123 <steven.blair@vmeretail.com>
2021-07-02 10:22:45 +0100

    Fixed bug where projection could be null when deleting (#3014)

---
+++ src/EventStore.Projections.Core.Tests/Services/projections_manager/when_deleting_a_persistent_projection_with_default_options.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventStore.Common.Utils;
+using EventStore.Core.Messages;
+using EventStore.Core.Messaging;
+using EventStore.Core.Tests;
+using EventStore.Projections.Core.Messages;
+using EventStore.Projections.Core.Services;
+using NUnit.Framework;
+
+namespace EventStore.Projections.Core.Tests.Services.projections_manager {
+	[TestFixture(typeof(LogFormat.V2), typeof(string))]
+	[TestFixture(typeof(LogFormat.V3), typeof(uint))]
+	public class when_deleting_a_persistent_projection_with_default_options<TLogFormat, TStreamId> : TestFixtureWithProjectionCoreAndManagementServices<TLogFormat, TStreamId> {
+		private string _projectionName;
+
+		protected override void Given() {
+			_projectionName = "test-projection";
+			AllWritesSucceed();
+			NoOtherStreams();
+		}
+
+		protected override IEnumerable<WhenStep> When() {
+			yield return new ProjectionSubsystemMessage.StartComponents(Guid.NewGuid());
+			yield return
+				new ProjectionManagementMessage.Command.Post(
+					new PublishEnvelope(_bus), ProjectionMode.Continuous, _projectionName,
+					ProjectionManagementMessage.RunAs.System, "JS", @"fromAll().when({$any:function(s,e){return s;}});",
+					enabled: true, checkpointsEnabled: true, emitEnabled: false, trackEmittedStreams: false);
+			yield return
+				new ProjectionManagementMessage.Command.Disable(
+					new PublishEnvelope(_bus), _projectionName, ProjectionManagementMessage.RunAs.System);
+			yield return
+				new ProjectionManagementMessage.Command.Delete(
+					new PublishEnvelope(_bus), _projectionName,
+					ProjectionManagementMessage.RunAs.System, true, true, true);
+		}
+
+		[Test, Category("v8")]
+		public void a_projection_deleted_event_is_written() {
+			Assert.AreEqual(true, _consumer.HandledMessages.OfType<ClientMessage.WriteEvents>().Any(x => x.Events[0].EventType == ProjectionEventTypes.ProjectionDeleted && Helper.UTF8NoBom.GetString(x.Events[0].Data) == _projectionName));
+		}
+	}
+}
