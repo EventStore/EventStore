cdd6b9ddc64b0db420c4b4594d792f5247bac4bf
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2021-07-08 23:50:31 +0000

    Removing redundant json files to simplify samples

--- samples/http-api/projections/create-projection.sh
+++ samples/http-api/projections/create-projection.sh
@@ -1,3 +1,3 @@
-curl -i --data-binary "@xbox-one-s-counter.json" \
+curl -i --data-binary "@xbox-one-s-counter.js" \
     http://localhost:2113/projections/continuous?name=xbox-one-s-counter%26type=js%26enabled=true%26emit=true%26trackemittedstreams=true \
     -u admin:changeit
