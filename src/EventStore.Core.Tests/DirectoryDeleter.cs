5c2ae83411b9c8dda7dee4d78d6fbcf323cefb15
Timothy Coleman <timothy.coleman@gmail.com>
2021-07-13 13:08:17 +0100

    try harder to delete directory after tests

---
+++ src/EventStore.Core.Tests/DirectoryDeleter.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace EventStore.Core.Tests {
+	public static class DirectoryDeleter {
+		public static async Task TryForceDeleteDirectory(string path, int retries) {
+			// retry because ClusterNode.StopAsync completes immediately on receiving BecomeShutdown
+			// but BecomeShutdown also causes the StorageReaderService to close the index.
+			for (var i = 0; i < retries; i++) {
+				if (TryForceDeleteDirectory(path))
+					return;
+				await Task.Delay(1000);
+			}
+		}
+		private static bool TryForceDeleteDirectory(string path) {
+			try {
+				ForceDeleteDirectory(path);
+				return true;
+			} catch {
+				return false;
+			}
+		}
+
+		private static void ForceDeleteDirectory(string path) {
+			var directory = new DirectoryInfo(path) { Attributes = FileAttributes.Normal };
+			foreach (var info in directory.GetFileSystemInfos("*", SearchOption.AllDirectories)) {
+				info.Attributes = FileAttributes.Normal;
+			}
+
+			directory.Delete(true);
+		}
+	}
+}
