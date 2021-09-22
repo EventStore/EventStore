39fbf9fced85edc36b8d516a65371d8916905a5a
Hayley Campbell <hayleyjeancampbell@gmail.com>
2021-09-22 10:08:49 +0200

    Don't read epochs if they should be cached
    - Prevent the EpochManager from attempting to read epochs that
    should have been cached.
    - Handle read errors in EpochManager.IsCorrectEpochAt
    - Add tests for EpochManager and LeaderReplicationService
    
    Since epochs are numbered according to the election proposal that
    created them, it is possible for there to be gaps in epoch numbers.
    
    The EpochManager would treat these gaps as cache misses and attempt to
    read the epoch. This resulted in an invalid read which would prevent a
    replica from subscribing.

---
+++ src/EventStore.Core.Tests/Services/Replication/LeaderReplication/when_replica_subscribes_with_epochs.cs
@@ -0,0 +1,346 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventStore.Core.Data;
+using EventStore.Core.Messages;
+using EventStore.Core.Services.Transport.Tcp;
+using EventStore.Core.TransactionLog.LogRecords;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.Services.Replication.LeaderReplication {
+	[TestFixture]
+	public class when_replica_subscribes_with_no_common_epochs : with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(1);
+
+			var epochs = new [] {
+				new Epoch(1010, 1, Guid.NewGuid()),
+				new Epoch(999, 2, Guid.NewGuid())
+			};
+
+			AddSubscription(_replicaId, true, epochs, 1010, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_a_replica_subscribed_message_from_start() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.Zero(subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	public class when_replica_with_same_epochs_subscribes_from_last_epoch_position : with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private EpochRecord _lastEpoch;
+
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(1);
+
+			_lastEpoch = EpochManager.GetLastEpoch();
+			var epochs = EpochManager.GetLastEpochs(10)
+				.Select(e => new Epoch(e.EpochPosition, e.EpochNumber, e.EpochId)).ToArray();
+
+			AddSubscription(_replicaId, true, epochs, _lastEpoch.EpochPosition, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_a_replica_subscribed_message_from_last_epoch_position() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(_lastEpoch.EpochPosition, subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	public class when_replica_with_same_epochs_subscribes_from_position_less_than_last_epoch_position
+		: with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private EpochRecord _lastEpoch;
+		private long _subscribedPosition;
+
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _subscribedPosition);
+			EpochManager.WriteNewEpoch(1);
+
+			_lastEpoch = EpochManager.GetLastEpoch();
+			var epochs = EpochManager.GetLastEpochs(10)
+				.Select(e => new Epoch(e.EpochPosition, e.EpochNumber, e.EpochId)).ToArray();
+
+			AddSubscription(_replicaId, true, epochs, _subscribedPosition, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_a_replica_subscribed_message_from_requested_position() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(_subscribedPosition, subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	public class when_replica_with_additional_epochs_subscribes_to_position_past_leaders_last_epoch
+		: with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private List<Epoch> _replicaEpochs;
+
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(1);
+			Writer.Write(CreateLogRecord(5, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(6, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(7, "test-stream", "test"), out var lastWritePosition);
+			Writer.Flush();
+
+			_replicaEpochs = new List<Epoch> {
+				new Epoch(lastWritePosition + 2000, 4, Guid.NewGuid()),
+				new Epoch(lastWritePosition + 1000, 3, Guid.NewGuid()),
+				new Epoch(lastWritePosition, 2, Guid.NewGuid()),
+			};
+			_replicaEpochs.AddRange(EpochManager.GetLastEpochs(10)
+				.Select(e => new Epoch(e.EpochPosition, e.EpochNumber, e.EpochId)).ToList());
+
+			AddSubscription(_replicaId, true, _replicaEpochs.ToArray(), lastWritePosition + 2000, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_replica_subscribed_message_for_epoch_after_common_epoch() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(_replicaEpochs[2].EpochPosition, subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	public class when_replica_subscribes_with_epoch_that_doesnt_exist_on_leader_but_is_before_leaders_last_epoch
+		: with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private List<Epoch> _replicaEpochs;
+
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out var otherEpochLogPosition);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(2);
+
+			var firstEpoch = EpochManager.GetLastEpochs(10).First(e => e.EpochNumber == 0);
+			_replicaEpochs = new List<Epoch> {
+				new Epoch(otherEpochLogPosition, 1, Guid.NewGuid()),
+				new Epoch(firstEpoch.EpochPosition, firstEpoch.EpochNumber, firstEpoch.EpochId)
+			};
+
+			AddSubscription(_replicaId, true, _replicaEpochs.ToArray(), _replicaEpochs[0].EpochPosition, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_replica_subscribed_message_for_epoch_after_common_epoch() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(_replicaEpochs[0].EpochPosition, subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	public class when_replica_subscribes_with_additional_epoch_past_leaders_writer_checkpoint
+		: with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private List<Epoch> _replicaEpochs;
+
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(1);
+
+			var subscribePosition = Db.Config.WriterCheckpoint.ReadNonFlushed() + 1000;
+			_replicaEpochs = new List<Epoch> {
+				new Epoch(subscribePosition, 2, Guid.NewGuid()),
+			};
+			_replicaEpochs.AddRange(EpochManager.GetLastEpochs(10)
+				.Select(e => new Epoch(e.EpochPosition, e.EpochNumber, e.EpochId)).ToList());
+
+			AddSubscription(_replicaId, true, _replicaEpochs.ToArray(), subscribePosition, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_replica_subscribed_message_for_leaders_writer_checkpoint() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(Db.Config.WriterCheckpoint.ReadNonFlushed(), subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	public class when_replica_subscribes_with_additional_epoch_and_leader_has_epoch_after_common_epoch
+		: with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private List<Epoch> _replicaEpochs;
+
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(1);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(4);
+
+			var subscribePosition = Db.Config.WriterCheckpoint.ReadNonFlushed() + 1000;
+			_replicaEpochs = new List<Epoch> {
+				new Epoch(subscribePosition, 2, Guid.NewGuid()),
+			};
+			_replicaEpochs.AddRange(EpochManager.GetLastEpochs(10)
+				.Where(e => e.EpochNumber < 4)
+				.Select(e => new Epoch(e.EpochPosition, e.EpochNumber, e.EpochId)).ToList());
+
+			AddSubscription(_replicaId, true, _replicaEpochs.ToArray(), subscribePosition, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_replica_subscribed_message_for_leaders_epoch_after_common_epoch() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(EpochManager.GetLastEpoch().EpochPosition, subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	[TestFixture]
+	public class when_replica_subscribes_with_uncached_epoch : with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private List<Epoch> _replicaEpochs;
+		public override void When() {
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(1);
+
+			// The EpochManager for these tests only caches 5 epochs
+			_replicaEpochs = EpochManager.GetLastEpochs(2)
+				.Select(e => new Epoch(e.EpochPosition, e.EpochNumber, e.EpochId)).ToList();
+
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(2);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(3);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(4);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(5);
+			Writer.Write(CreateLogRecord(5, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(6);
+
+			AddSubscription(_replicaId, true, _replicaEpochs.ToArray(), _replicaEpochs[0].EpochPosition, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_a_replica_subscribed_message_common_epoch() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(_replicaEpochs[0].EpochPosition, subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+
+	[TestFixture]
+	public class when_replica_subscribes_with_uncached_epoch_that_does_not_exist_on_leader
+		: with_replication_service_and_epoch_manager {
+		private readonly Guid _replicaId = Guid.NewGuid();
+		private TcpConnectionManager _replicaManager;
+		private List<Epoch> _replicaEpochs;
+		private EpochRecord[] _uncachedLeaderEpochs;
+		public override void When() {
+			// The EpochManager for these tests only caches 5 epochs
+			// Epochs 2 and 3 don't exist
+			EpochManager.WriteNewEpoch(0);
+			Writer.Write(CreateLogRecord(0, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(1);
+			Writer.Write(CreateLogRecord(1, "test-stream", "test"), out _);
+
+			_uncachedLeaderEpochs = EpochManager.GetLastEpochs(2);
+
+			EpochManager.WriteNewEpoch(4);
+			Writer.Write(CreateLogRecord(2, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(5);
+			Writer.Write(CreateLogRecord(3, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(6);
+			Writer.Write(CreateLogRecord(4, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(7);
+			Writer.Write(CreateLogRecord(5, "test-stream", "test"), out _);
+			EpochManager.WriteNewEpoch(8);
+
+			_replicaEpochs = new List<Epoch> {
+				new Epoch(_uncachedLeaderEpochs[0].EpochPosition + 800, 3, Guid.NewGuid()),
+				new Epoch(_uncachedLeaderEpochs[0].EpochPosition + 400, 2, Guid.NewGuid()),
+				new Epoch(_uncachedLeaderEpochs[0].EpochPosition, _uncachedLeaderEpochs[0].EpochNumber, _uncachedLeaderEpochs[0].EpochId),
+				new Epoch(_uncachedLeaderEpochs[1].EpochPosition, _uncachedLeaderEpochs[1].EpochNumber, _uncachedLeaderEpochs[1].EpochId)
+			};
+
+			AddSubscription(_replicaId, true, _replicaEpochs.ToArray(), _replicaEpochs[0].EpochPosition, out _replicaManager);
+		}
+
+		[Test]
+		public void subscription_is_sent_a_replica_subscribed_message_to_epoch_position_after_common_epoch() {
+			var subscribed = GetTcpSendsFor(_replicaManager).Select(x => x.Message)
+				.OfType<ReplicationMessage.ReplicaSubscribed>().ToArray();
+			Assert.AreEqual(1, subscribed.Length);
+			Assert.AreEqual(EpochManager.GetLastEpochs(5).First(x => x.EpochNumber == 4).EpochPosition, subscribed[0].SubscriptionPosition);
+			Assert.AreEqual(_replicaId, subscribed[0].SubscriptionId);
+			Assert.AreEqual(LeaderId, subscribed[0].LeaderId);
+		}
+	}
+}
