31454a061fbfb27a790aa7c2e3f698bf02a79fde
Hayley Campbell <hayleyjeancampbell@gmail.com>
2021-11-18 11:57:26 +0100

    Update server protos for persistent subscriptions
    Add the following new operations
    - Replay parked messages
    - Restart Subsystem
    - GetInfo
    - List
    - Add ConsumerStrategy string property to CreateReq
    - Deprecate NamedConsumerStrategy

---
+++ src/EventStore.Core.Tests/Services/Transport/Grpc/PersistentSubscriptionTests/RestartSubsystemTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using EventStore.Client;
+using EventStore.Client.PersistentSubscriptions;
+using EventStore.Core.Tests.Services.Transport.Grpc.StreamsTests;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.Services.Transport.Grpc.PersistentSubscriptionTests {
+	internal class RestartSubsystemTests {
+
+		[TestFixture(typeof(LogFormat.V2), typeof(string))]
+		[TestFixture(typeof(LogFormat.V3), typeof(uint))]
+		public class
+			when_restarting_the_persistent_subscription_subsystem<TLogFormat, TStreamId>
+				: GrpcSpecification<TLogFormat, TStreamId> {
+			private PersistentSubscriptions.PersistentSubscriptionsClient _persistentSubscriptionsClient;
+			private Exception _exception;
+
+			protected override Task Given() {
+				_persistentSubscriptionsClient = new PersistentSubscriptions.PersistentSubscriptionsClient(Channel);
+				return Task.CompletedTask;
+			}
+
+			protected override async Task When() {
+				try {
+					await _persistentSubscriptionsClient.RestartSubsystemAsync(new Empty(), GetCallOptions(AdminCredentials));
+				} catch (Exception e) {
+					_exception = e;
+				}
+			}
+
+			[Test]
+			public void should_not_throw_an_exception() {
+				Assert.IsNull(_exception);
+			}
+		}
+
+	}
+}
