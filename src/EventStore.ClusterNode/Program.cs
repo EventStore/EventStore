de9e5cacea27b6c599567511e7b05f94985dc02a
Timothy Coleman <timothy.coleman@gmail.com>
2021-12-02 15:10:01 +0000

    Add Simple benchmark to compare Regular and MemoryMapped checkpoints

--- src/EventStore.ClusterNode/Program.cs
+++ src/EventStore.ClusterNode/Program.cs
@@ -1,98 +1,42 @@
 using System;
-using System.Threading;
-using System.Threading.Tasks;
-using EventStore.Common.Configuration;
-using EventStore.Common.Log;
-using EventStore.Common.Utils;
-using EventStore.Core.Services.Transport.Http;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Https;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Serilog;
+using System.Diagnostics;
+using EventStore.Core.TransactionLog.Checkpoint;
 
-namespace EventStore.ClusterNode {
+namespace EventStore.CheckpointBenchmark {
 	internal static class Program {
-		public static async Task<int> Main(string[] args) {
-			ThreadPool.SetMaxThreads(1000, 1000);
-			Log.Logger = EventStoreLoggerConfiguration.ConsoleLog;
-			try {
-				Serilog.Log.Logger = EventStoreLoggerConfiguration.ConsoleLog;
-				var cts = new CancellationTokenSource();
-				var hostedService = new ClusterVNodeHostedService(args);
-				if (hostedService.SkipRun)
-					return 0;
+		public static void Main(string[] args) {
+			string path;
+			int flushes;
 
-				var exitCodeSource = new TaskCompletionSource<int>();
-				Application.RegisterExitAction(code => {
-					cts.Cancel();
-					exitCodeSource.SetResult(code);
-				});
-				Console.CancelKeyPress += delegate {
-					Application.Exit(0, "Cancelled.");
-				};
-
-				if (hostedService.Options.WhatIf) {
-					Log.Information("Exiting with exit code: 0.\nExit reason: WhatIf option specified");
-					return (int)ExitCode.Success;
-				}
-
-				await CreateHostBuilder(hostedService, args)
-					.RunConsoleAsync(options => options.SuppressStatusMessages = true, cts.Token);
-				return await exitCodeSource.Task;
-			} catch (Exception ex) {
-				Log.Fatal(ex, "Host terminated unexpectedly.");
-				return 1;
-			} finally {
-				Log.CloseAndFlush();
+			// CONFIGURATION
+			if (args?.Length < 2) {
+				path = @"C:\checkpoints\test.chk";
+				flushes = 2000;
+			} else {
+				path = args[0];
+				flushes = int.Parse(args[1]);
 			}
+
+			Console.WriteLine("Path: {0}", path);
+			Console.WriteLine("Flushes: {0}", flushes);
+
+			Measure(flushes, new MemoryMappedFileCheckpoint(path, "MemoryMappedFileCheckpoint", true));
+			Measure(flushes, new FileCheckpoint(path,             "RegularFileCheckpoint     ", true));
 		}
 
-		private static IHostBuilder CreateHostBuilder(ClusterVNodeHostedService hostedService, string[] args) =>
-			new HostBuilder()
-				.ConfigureHostConfiguration(builder =>
-					builder.AddEnvironmentVariables("DOTNET_").AddCommandLine(args ?? Array.Empty<string>()))
-				.ConfigureAppConfiguration(builder =>
-					builder.AddEnvironmentVariables().AddCommandLine(args ?? Array.Empty<string>()))
-				.ConfigureServices(services => services.AddSingleton<IHostedService>(hostedService))
-				.ConfigureServices(services => services.Configure<KestrelServerOptions>(
-					EventStoreKestrelConfiguration.GetConfiguration()))
-				.ConfigureLogging(logging => logging.AddSerilog())
-				.ConfigureWebHostDefaults(builder => builder
-					.UseKestrel(server => {
-						server.Limits.Http2.KeepAlivePingDelay =
-							TimeSpan.FromMilliseconds(hostedService.Options.KeepAliveInterval);
-						server.Limits.Http2.KeepAlivePingTimeout =
-							TimeSpan.FromMilliseconds(hostedService.Options.KeepAliveTimeout);
-						server.Listen(hostedService.Options.ExtIp, hostedService.Options.HttpPort,
-							listenOptions => {
-								if (hostedService.Node.DisableHttps) {
-									listenOptions.Use(next =>
-										new ClearTextHttpMultiplexingMiddleware(next).OnConnectAsync);
-								} else {
-									listenOptions.UseHttps(new HttpsConnectionAdapterOptions {
-										ServerCertificateSelector = delegate {
-											return hostedService.Node.CertificateSelector();
-										},
-										ClientCertificateMode = ClientCertificateMode.AllowCertificate,
-										ClientCertificateValidation = (certificate, chain, sslPolicyErrors) => {
-											var (isValid, error) =
-												hostedService.Node.InternalClientCertificateValidator(certificate,
-													chain,
-													sslPolicyErrors);
-											if (!isValid && error != null) {
-												Log.Error("Client certificate validation error: {e}", error);
-											}
+		public static void Measure(int numFlushes, ICheckpoint cp) {
+			var sw = Stopwatch.StartNew();
+			for (int i = 0; i < numFlushes; i++) {
+				cp.Write(i);
+				cp.Flush();
+				cp.Read();
+			}
+			cp.Dispose();
 
-											return isValid;
-										}
-									});
-								}
-							});
-					})
-					.ConfigureServices(services => hostedService.Node.Startup.ConfigureServices(services))
-					.Configure(hostedService.Node.Startup.Configure));
+			var elapsed = sw.Elapsed;
+			var flushesPerSecond = numFlushes / elapsed.TotalSeconds;
+			Console.WriteLine("{0} did {1} flushes in {2}. {3:N0} flushes/second",
+				cp.Name, numFlushes, elapsed, flushesPerSecond);
+		}
 	}
 }
