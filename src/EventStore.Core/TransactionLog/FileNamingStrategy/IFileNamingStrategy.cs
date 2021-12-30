5bad62770a8319da129be767338e107ef14478d9
shaan1337 <sniper111@gmail.com>
2021-12-30 08:43:09 +0400

    Add EnumerateAllFiles() method to IFileNamingStrategy interface

--- src/EventStore.Core/TransactionLog/FileNamingStrategy/IFileNamingStrategy.cs
+++ src/EventStore.Core/TransactionLog/FileNamingStrategy/IFileNamingStrategy.cs
@@ -1,9 +1,16 @@
+using System;
+
 namespace EventStore.Core.TransactionLog.FileNamingStrategy {
 	public interface IFileNamingStrategy {
 		string GetFilenameFor(int index, int version);
 		string DetermineBestVersionFilenameFor(int index);
 		string[] GetAllVersionsFor(int index);
 		string[] GetAllPresentFiles();
+		void EnumerateAllFiles(
+			Func<string, int> getNextFileNumber,
+			Action<string, int, int> onLatestVersionFound = null,
+			Action<string, int> onOldVersionFound = null,
+			Action<string, int> onFileMissing = null);
 
 		string GetTempFilename();
 		string[] GetAllTempFiles();
--- src/EventStore.Core/TransactionLog/FileNamingStrategy/VersionedPatternFileNamingStrategy.cs
+++ src/EventStore.Core/TransactionLog/FileNamingStrategy/VersionedPatternFileNamingStrategy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
 using EventStore.Common.Utils;
@@ -42,6 +43,59 @@ namespace EventStore.Core.TransactionLog.FileNamingStrategy {
 				.OrderByDescending(x => x, StringComparer.CurrentCultureIgnoreCase)
 				.ToArray();
 			return versions;
+		}
+
+		private int GetFileNumberFor(string path) {
+			var fileName = Path.GetFileName(path);
+			if (!_chunkNamePattern.IsMatch(fileName))
+				throw new ArgumentException($"Invalid file name: {fileName}");
+
+			var start = _prefix.Length;
+			var end = fileName.IndexOf('.', _prefix.Length);
+			Debug.Assert(end != -1);
+
+			if (!int.TryParse(fileName[start..end], out var chunkNumber))
+				throw new ArgumentException($"Invalid file name: {fileName}");
+
+			return chunkNumber;
+		}
+
+		public void EnumerateAllFiles(
+			Func<string, int> getNextFileNumber,
+			Action<string, int, int> onLatestVersionFound = null,
+			Action<string, int> onOldVersionFound = null,
+			Action<string, int> onFileMissing = null) {
+			var allFiles = GetAllPresentFiles();
+			Array.Sort(allFiles, StringComparer.CurrentCultureIgnoreCase);
+
+			int expectedChunkNumber = 0;
+			for (int i = 0; i < allFiles.Length; i++) {
+				var chunkFileName = allFiles[i];
+				var chunkNumber = GetFileNumberFor(allFiles[i]);
+				var nextChunkNumber = -1;
+				if (i + 1 < allFiles.Length)
+					nextChunkNumber = GetFileNumberFor(allFiles[i+1]);
+
+				if (chunkNumber < expectedChunkNumber) { // present in an earlier, merged, chunk
+					onOldVersionFound?.Invoke(chunkFileName, chunkNumber);
+					continue;
+				}
+
+				if (chunkNumber > expectedChunkNumber) { // one or more chunks are missing
+					for (int j = expectedChunkNumber; j < chunkNumber; j++) {
+						onFileMissing?.Invoke(GetFilenameFor(j, 0), j);
+					}
+					// set the expected chunk number to prevent calling onFileMissing() again for the same chunk numbers
+					expectedChunkNumber = chunkNumber;
+				}
+
+				if (chunkNumber == nextChunkNumber) { // there is a newer version of this chunk
+					onOldVersionFound?.Invoke(chunkFileName, chunkNumber);
+				} else { // latest version of chunk with the expected chunk number
+					expectedChunkNumber = getNextFileNumber(chunkFileName);
+					onLatestVersionFound?.Invoke(chunkFileName, chunkNumber, expectedChunkNumber - 1);
+				}
+			}
 		}
 
 		public string[] GetAllPresentFiles() {
