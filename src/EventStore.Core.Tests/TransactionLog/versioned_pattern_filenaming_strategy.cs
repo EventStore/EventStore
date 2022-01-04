b4d72f4c974ef43e6cceaac310d891ead1b82dcc
shaan1337 <sniper111@gmail.com>
2022-01-04 16:30:14 +0400

    Add test

--- src/EventStore.Core.Tests/TransactionLog/versioned_pattern_filenaming_strategy.cs
+++ src/EventStore.Core.Tests/TransactionLog/versioned_pattern_filenaming_strategy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using EventStore.Core.TransactionLog.FileNamingStrategy;
 using NUnit.Framework;
@@ -140,6 +141,65 @@ namespace EventStore.Core.Tests.TransactionLog {
 			Assert.AreEqual(2, tempFiles.Length);
 			Assert.AreEqual(tmp[0], tempFiles[0]);
 			Assert.AreEqual(tmp[1], tempFiles[1]);
+		}
+
+		[Test]
+		public void enumerates_files_with_correct_callback_order() {
+			File.Create(GetFilePathFor("foo")).Close(); // should be ignored
+			File.Create(GetFilePathFor("bla")).Close(); // should be ignored
+			File.Create(GetFilePathFor("chunk-000001.000000.tmp")).Close(); // should be ignored
+			File.Create(GetFilePathFor("chunk-001.000")).Close(); // should be ignored
+
+			// chunk 0 is missing
+			File.Create(GetFilePathFor("chunk-000001.000000")).Close(); // chunks 1 - 1 (latest)
+			File.Create(GetFilePathFor("chunk-000002.000001")).Close(); // chunks 2 - 2 (latest)
+			// chunks 3 & 4 are missing
+			File.Create(GetFilePathFor("chunk-000005.000000")).Close(); // chunks 5 - 5 (old)
+			File.Create(GetFilePathFor("chunk-000005.000001")).Close(); // chunks 5 - 6 (old)
+			File.Create(GetFilePathFor("chunk-000005.000002")).Close(); // chunks 5 - 7 (latest)
+			File.Create(GetFilePathFor("chunk-000006.000000")).Close(); // chunks 6 - 6 (old)
+			// chunk 7 is not missing - it's merged with chunk 5
+			File.Create(GetFilePathFor("chunk-000008.000007")).Close(); // chunks 8 - 8 (latest)
+			// chunk 9 is missing
+			File.Create(GetFilePathFor("chunk-000010.000005")).Close(); // chunks 10 - 10 (latest)
+
+			var strategy = new VersionedPatternFileNamingStrategy(PathName, "chunk-");
+			var result = new List<string>();
+			int GetNextFileNumber(string chunk) {
+				return Path.GetFileName(chunk) switch {
+					"chunk-000001.000000" => 2,
+					"chunk-000002.000001" => 3,
+					"chunk-000005.000000" => 6,
+					"chunk-000005.000001" => 7,
+					"chunk-000005.000002" => 8,
+					"chunk-000006.000000" => 7,
+					"chunk-000008.000007" => 9,
+					"chunk-000010.000005" => 11,
+					_ => throw new Exception($"Unexpected file: {chunk}")
+				};
+			}
+
+			strategy.EnumerateAllFiles(GetNextFileNumber,
+				onLatestVersionFound: (chunk, start, end) => result.Add($"latest {Path.GetFileName(chunk)} {start}-{end}"),
+				onOldVersionFound: (chunk, start) => result.Add($"old {Path.GetFileName(chunk)} {start}"),
+				onFileMissing: (chunk, start) => result.Add($"missing {Path.GetFileName(chunk)} {start}")
+			);
+
+			var expectedResult = new List<string> {
+				"missing chunk-000000.000000 0",
+				"latest chunk-000001.000000 1-1",
+				"latest chunk-000002.000001 2-2",
+				"missing chunk-000003.000000 3",
+				"missing chunk-000004.000000 4",
+				"old chunk-000005.000000 5",
+				"old chunk-000005.000001 5",
+				"latest chunk-000005.000002 5-7",
+				"old chunk-000006.000000 6",
+				"latest chunk-000008.000007 8-8",
+				"missing chunk-000009.000000 9",
+				"latest chunk-000010.000005 10-10"
+			};
+			Assert.AreEqual(expectedResult, result);
 		}
 	}
 }
