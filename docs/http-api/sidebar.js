7f390d3c41a7219ed3d714b16f00c7b4c2996574
Alexey Zimarev <alex@zimarev.com>
2022-01-18 17:13:09 +0100

    Added HTTP API

---
+++ docs/http-api/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        text: "",
+        header: "HTTP",
+        children: [
+            "README.md",
+            "security.md",
+            "persistent.md",
+            "projections.md",
+            "optional-http-headers.md",
+            "api.md",
+        ]
+    }
+]
