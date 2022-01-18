93c63401e5dda535406e3d588c603ac5374c6162
Alexey Zimarev <alex@zimarev.com>
2022-01-18 17:07:31 +0100

    Updated alias config and sidebar

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -1,36 +1,34 @@
-module.exports = {
-    "/": [
-        {
-            text:     "Getting started",
-            children: [
-                "/README.md",
-                "/installation.md",
-            ]
-        },
-        {
-            text:     "Configuration",
-            children: [
-                "/configuration.md",
-                "/security.md",
-                "/networking.md",
-                "/cluster.md",
-            ]
-        },
-        {
-            text:     "Features",
-            children: [
-                "/streams.md",
-                "/indexes.md",
-                "/projections.md",
-                "/persistent-subscriptions.md",
-            ]
-        },
-        {
-            text:     "Operations",
-            children: [
-                "/operations.md",
-                "/diagnostics.md",
-            ]
-        }
-    ]
-}
+module.exports = [
+    {
+        text:     "Getting started",
+        children: [
+            "/README.md",
+            "/installation.md",
+        ]
+    },
+    {
+        text:     "Configuration",
+        children: [
+            "/configuration.md",
+            "/security.md",
+            "/networking.md",
+            "/cluster.md",
+        ]
+    },
+    {
+        text:     "Features",
+        children: [
+            "/streams.md",
+            "/indexes.md",
+            "/projections.md",
+            "/persistent-subscriptions.md",
+        ]
+    },
+    {
+        text:     "Operations",
+        children: [
+            "/operations.md",
+            "/diagnostics.md",
+        ]
+    }
+];
