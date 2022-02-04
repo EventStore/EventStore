60e600dc3252090147d5e4dceb20aec498bfed00
Alexey Zimarev <alex@zimarev.com>
2022-02-04 15:41:18 +0100

    Removed the confused sample, fixed sidebars

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -2,35 +2,35 @@ module.exports = [
     {
         text:     "Getting started",
         children: [
-            "/README.md",
-            "/installation.md"
+            "README.md",
+            "installation.md"
         ]
     },
     {
         text:     "Configuration",
         children: [
-            "/configuration.md",
-            "/security.md",
-            "/networking.md",
-            "/cluster.md",
-            "/server-settings.md"
+            "configuration.md",
+            "security.md",
+            "networking.md",
+            "cluster.md",
+            "server-settings.md"
         ]
     },
     {
         text:     "Features",
         children: [
-            "/admin-ui.md",
-            "/streams.md",
-            "/indexes.md",
-            "/projections.md",
-            "/persistent-subscriptions.md",
+            "admin-ui.md",
+            "streams.md",
+            "indexes.md",
+            "projections.md",
+            "persistent-subscriptions.md",
         ]
     },
     {
         text:     "Operations",
         children: [
-            "/operations.md",
-            "/diagnostics.md",
+            "operations.md",
+            "diagnostics.md",
         ]
     }
 ];
