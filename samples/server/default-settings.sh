b6089cd3d64b59414f159e61e04470d20c7815dc
Alexey Zimarev <alex@zimarev.com>
2022-02-04 15:26:25 +0100

    Fixing remaining broken links

---
+++ samples/server/default-settings.sh
@@ -0,0 +1,23 @@
+curl -i "http://localhost:2113/streams/%24settings" \
+    --user admin:changeit \
+    -H "Content-Type: application/vnd.eventstore.events+json" \
+    -d $'[{
+        "eventId": "7c314750-05e1-439f-b2eb-f5b0e019be72",
+        "eventType": "update-default-acl",
+        "data": {
+            "$userStreamAcl" : {
+                "$r"  : ["$admin", "$ops", "service-a", "service-b"],
+                "$w"  : ["$admin", "$ops", "service-a", "service-b"],
+                "$d"  : ["$admin", "$ops"],
+                "$mr" : ["$admin", "$ops"],
+                "$mw" : ["$admin", "$ops"]
+            },
+            "$systemStreamAcl" : {
+                "$r"  : "$admins",
+                "$w"  : "$admins",
+                "$d"  : "$admins",
+                "$mr" : "$admins",
+                "$mw" : "$admins"
+            }
+        }
+    }]'
