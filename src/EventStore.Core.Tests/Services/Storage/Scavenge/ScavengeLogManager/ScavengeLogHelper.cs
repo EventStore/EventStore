805879c865f21e68daab7fd9f34563b906e4ad39
Hayley Campbell <hayleyjeancampbell@gmail.com>
2022-04-21 14:39:59 +0000

    Fix overflow when scavenge has saved a lot of space
    - Fix int overflow when trying to complete a scavenge after the scavenge
      has saved a lot of space.
    - Add tests around TFChunkScavengerLogManager

---
+++ src/EventStore.Core.Tests/Services/Storage/Scavenge/ScavengeLogManager/ScavengeLogHelper.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using EventStore.Core.Data;
+using EventStore.Core.Services;
+using EventStore.Core.TransactionLog.Chunks;
+
+namespace EventStore.Core.Tests.Services.Storage.Scavenge.ScavengeLogManager {
+	public static class ScavengerLogHelper {
+		public static string ScavengeStreamId(Guid scavengeId) {
+			return $"{SystemStreams.ScavengesStream}-{scavengeId}";
+		}
+
+		public static StreamMetadata CreateScavengeMetadata(TimeSpan scavengeHistoryMaxAge) {
+			return new StreamMetadata(maxAge: scavengeHistoryMaxAge, acl: new(
+				new[] { "$ops" },
+				new string[] { },
+				new string[] { },
+				new string[] { },
+				new string[] { }
+			));
+		}
+
+		public static Dictionary<string, object> CreateScavengeStarted(Guid scavengeId, string nodeEndpoint,
+			int startFromChunk = 0) {
+			return new Dictionary<string, object> {
+				{ "scavengeId", scavengeId },
+				{ "nodeEndpoint", nodeEndpoint },
+				{ "alwaysKeepScavenged", false },
+				{ "mergeChunks", true },
+				{ "startFromChunk", startFromChunk },
+				{ "threads", 1 },
+			};
+		}
+
+		public static Dictionary<string, object> CreateScavengeInterruptedByRestart
+			(Guid scavengeId, string nodeEndpoint, TimeSpan timeTaken, long spaceSaved = 0, int maxChunkScavenged = 0) {
+			return new Dictionary<string, object> {
+				{ "scavengeId", scavengeId },
+				{ "nodeEndpoint", nodeEndpoint },
+				{ "result", ScavengeResult.Failed },
+				{ "error", "The node was restarted." },
+				{ "timeTaken", timeTaken },
+				{ "spaceSaved", spaceSaved },
+				{ "maxChunkScavenged", maxChunkScavenged }
+			};
+		}
+
+		public static Dictionary<string, object> CreateScavengeChunkCompleted
+			(Guid scavengeId, string nodeEndpoint, int chunkStart, int chunkEnd, TimeSpan elapsed, long spaceSaved) {
+			return new Dictionary<string, object> {
+				{ "scavengeId", scavengeId },
+				{ "chunkStartNumber", chunkStart },
+				{ "chunkEndNumber", chunkEnd },
+				{ "timeTaken", elapsed },
+				{ "wasScavenged", true },
+				{ "spaceSaved", spaceSaved },
+				{ "nodeEndpoint", nodeEndpoint },
+				{ "errorMessage", "" }
+			};
+		}
+
+		public static Dictionary<string, object> CreateScavengeCompletedSuccessfully(
+			Guid scavengeId, string nodeEndpoint, TimeSpan elapsed, long spaceSaved, int maxChunkScavenged) {
+			return new Dictionary<string, object> {
+				{ "scavengeId", scavengeId },
+				{ "nodeEndpoint", nodeEndpoint },
+				{ "result", ScavengeResult.Success },
+				{ "error", "" },
+				{ "timeTaken", elapsed },
+				{ "spaceSaved", spaceSaved },
+				{ "maxChunkScavenged", maxChunkScavenged }
+			};
+		}
+	}
+}
