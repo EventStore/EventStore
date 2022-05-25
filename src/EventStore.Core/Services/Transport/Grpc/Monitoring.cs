6e32b1b85e52a7d11ef06192ce6eff3c2ad03321
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2022-05-25 12:35:33 +0200

    use channels for stats monitoring

--- src/EventStore.Core/Services/Transport/Grpc/Monitoring.cs
+++ src/EventStore.Core/Services/Transport/Grpc/Monitoring.cs
@@ -1,26 +1,50 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using EventStore.Client.Monitoring;
 using EventStore.Core.Bus;
 using EventStore.Core.Messages;
 using EventStore.Core.Messaging;
-using Google.Protobuf.WellKnownTypes;
-using EventStore.Common.Utils;
-using Google.Protobuf.Collections;
 using Grpc.Core;
 
 namespace EventStore.Core.Services.Transport.Grpc {
 	internal partial class Monitoring : EventStore.Client.Monitoring.Monitoring.MonitoringBase {
 		private readonly IPublisher _publisher;
 		
-		public override async Task Stats(StatsReq request, IServerStreamWriter<StatsResp> responseStream, ServerCallContext context) {
-			await using var enumerator = CollectStats(request);
-			await using (context.CancellationToken.Register(() => enumerator.DisposeAsync())) {
-				while (await enumerator.MoveNextAsync().ConfigureAwait(false)) {
-					await responseStream.WriteAsync(enumerator.Current).ConfigureAwait(false);
+		public override Task Stats(StatsReq request, IServerStreamWriter<StatsResp> responseStream, ServerCallContext context) {
+			var channel = Channel.CreateBounded<StatsResp>(new BoundedChannelOptions(1) {
+				SingleReader = true,
+				SingleWriter = true
+			});
+
+			_ = Receive();
+
+			return channel.Reader.ReadAllAsync(context.CancellationToken)
+				.ForEachAwaitAsync(responseStream.WriteAsync, context.CancellationToken);
+			
+			async Task Receive() {
+				var delay = TimeSpan.FromMilliseconds(request.RefreshTimePeriodInMs);
+				var envelope = new CallbackEnvelope(message => {
+					if (message is not MonitoringMessage.GetFreshStatsCompleted completed) {
+						channel.Writer.TryComplete(UnknownMessage<MonitoringMessage.GetFreshStatsCompleted>(message));
+						return;
+					}
+
+					var response = new StatsResp();
+
+					foreach (var (key, value) in completed.Stats.Where(stat => stat.Value is not null)) {
+						response.Stats.Add(key, value.ToString());
+					}
+
+					channel.Writer.TryWrite(response);
+
+				});
+				while (!context.CancellationToken.IsCancellationRequested) {
+					_publisher.Publish(
+						new MonitoringMessage.GetFreshStats(envelope, x => x, request.UseMetadata, false));
+
+					await Task.Delay(delay, context.CancellationToken).ConfigureAwait(false);
 				}
 			}
 		}
@@ -29,34 +53,6 @@ namespace EventStore.Core.Services.Transport.Grpc {
 			_publisher = publisher;
 		}
 
-		private async IAsyncEnumerator<StatsResp> CollectStats(StatsReq request) {
-			for (;;) {
-				var source = new TaskCompletionSource<StatsResp>();
-				var envelope = new CallbackEnvelope(message => {
-					if (message is not MonitoringMessage.GetFreshStatsCompleted completed) {
-						source.TrySetException(UnknownMessage<MonitoringMessage.GetFreshStatsCompleted>(message));
-					} else {
-						var resp = new StatsResp();
-
-						foreach (var tuple in completed.Stats) {
-							if (tuple.Value == null)
-								continue;
-							
-							resp.Stats.Add(tuple.Key, tuple.Value.ToString());
-						}
-
-						source.TrySetResult(resp);
-					}
-				});
-				_publisher.Publish(new MonitoringMessage.GetFreshStats(envelope, x => x, request.UseMetadata, false));
-				var resp = await source.Task.ConfigureAwait(false);
-				
-				yield return resp;
-
-				await Task.Delay(TimeSpan.FromMilliseconds(request.RefreshTimePeriodInMs)).ConfigureAwait(false);
-			}
-		}
-		
 		private static Exception UnknownMessage<T>(Message message) where T : Message =>
 			new RpcException(
 				new Status(StatusCode.Unknown,
