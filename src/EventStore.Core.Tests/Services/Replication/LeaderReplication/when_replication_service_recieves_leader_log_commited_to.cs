d6961a59fdfbdd9a587f24fbd31479e1cdfe8844
Timothy Coleman <timothy.coleman@gmail.com>
2022-07-20 12:00:39 +0100

    Use follower writer checkpoint for replication tracking
    Previously the replication subscription position was used, but the subscription position can be ahead of the writer, resulting in the leader thinking something has been written on the follower when it has not been.

--- src/EventStore.Core.Tests/Services/Replication/LeaderReplication/when_replication_service_recieves_leader_log_commited_to.cs
+++ src/EventStore.Core.Tests/Services/Replication/LeaderReplication/when_replication_service_recieves_leader_log_commited_to.cs
@@ -16,11 +16,11 @@ namespace EventStore.Core.Tests.Services.Replication.LeaderReplication {
 		public void replicated_to_should_be_sent_to_subscriptions() {
 			AssertEx.IsOrBecomesTrue(() => TcpSends.Count > 4, msg:"TcpSend msg not recieved");
 			var sends = TcpSends.Where(tcpSend => tcpSend.Message is ReplicationTrackingMessage.ReplicatedTo).ToList();
-			Assert.AreEqual(3 * 2,sends.Count); //one ReplicatedTo message in sent when the subscription is added and the second one in When()
-			Assert.AreEqual(2, sends.Count( msg=> msg.ConnectionManager.ConnectionId == ReplicaSubscriptionId));
-			Assert.AreEqual(2, sends.Count( msg=> msg.ConnectionManager.ConnectionId == ReplicaSubscriptionId2));
-			Assert.AreEqual(2, sends.Count( msg=> msg.ConnectionManager.ConnectionId == ReadOnlyReplicaSubscriptionId));
-			
+			Assert.AreEqual(4 * 2, sends.Count); //one ReplicatedTo message in sent when the subscription is added and the second one in When()
+			Assert.AreEqual(2, sends.Count(msg => msg.ConnectionManager.ConnectionId == ReplicaSubscriptionId));
+			Assert.AreEqual(2, sends.Count(msg => msg.ConnectionManager.ConnectionId == ReplicaSubscriptionId2));
+			Assert.AreEqual(2, sends.Count(msg => msg.ConnectionManager.ConnectionId == ReadOnlyReplicaSubscriptionId));
+			Assert.AreEqual(2, sends.Count(msg => msg.ConnectionManager.ConnectionId == ReplicaSubscriptionIdV0));
 		}
 	}
 }
