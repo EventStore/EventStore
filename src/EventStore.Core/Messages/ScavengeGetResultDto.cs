f9dc0409efacee42714ed4c9b2805f25c7b65d98
Timothy Coleman <timothy.coleman@gmail.com>
2022-11-11 09:33:58 +0000

    Scavenge HTTP api quality of life improvements
    - GET the current scavenge (id and link for deleting) if any, from /admin/scavenge/current
    - DELETE /admin/scavenge/current to stop the current scavenge regardless of its id.
    - Stopping a scavenge now waits for the scavenge to stop before responding to the request

---
+++ src/EventStore.Core/Messages/ScavengeGetResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EventStore.Core.Messages {
+	public class ScavengeGetResultDto {
+		public string ScavengeId { get; set; } = "";
+		public string ScavengeLink { get; set; } = "";
+
+		public override string ToString() =>
+			$"ScavengeId: {ScavengeId}, " +
+			$"ScavengeLink: {ScavengeLink}";
+	}
+}
--- src/EventStore.Core/Services/Storage/StorageScavenger.cs
+++ src/EventStore.Core/Services/Storage/StorageScavenger.cs
@@ -16,6 +16,7 @@ namespace EventStore.Core.Services.Storage {
 	public class StorageScavenger :
 		IHandle<ClientMessage.ScavengeDatabase>,
 		IHandle<ClientMessage.StopDatabaseScavenge>,
+		IHandle<ClientMessage.GetDatabaseScavenge>,
 		IHandle<SystemMessage.StateChangeMessage> {
 
 		protected static ILogger Log { get; } = Serilog.Log.ForContext<StorageScavenger>();
@@ -24,6 +25,8 @@ namespace EventStore.Core.Services.Storage {
 		private readonly object _lock = new object();
 
 		private IScavenger _currentScavenge;
+		// invariant: _currentScavenge is not null => _currentScavengeTask is the task of the current scavenge
+		private Task _currentScavengeTask;
 		private CancellationTokenSource _cancellationTokenSource;
 
 		public StorageScavenger(
@@ -55,10 +58,10 @@ namespace EventStore.Core.Services.Storage {
 
 						_cancellationTokenSource = new CancellationTokenSource();
 
-						var newScavenge = _currentScavenge = _scavengerFactory.Create(message, tfChunkScavengerLog);
-						var newScavengeTask = _currentScavenge.ScavengeAsync(_cancellationTokenSource.Token);
+						_currentScavenge = _scavengerFactory.Create(message, tfChunkScavengerLog);
+						_currentScavengeTask = _currentScavenge.ScavengeAsync(_cancellationTokenSource.Token);
 
-						HandleCleanupWhenFinished(newScavengeTask, newScavenge);
+						HandleCleanupWhenFinished(_currentScavengeTask, _currentScavenge);
 
 						message.Envelope.ReplyWith(new ClientMessage.ScavengeDatabaseResponse(message.CorrelationId,
 							ClientMessage.ScavengeDatabaseResponse.ScavengeResult.Started,
@@ -71,16 +74,37 @@ namespace EventStore.Core.Services.Storage {
 		public void Handle(ClientMessage.StopDatabaseScavenge message) {
 			if (IsAllowed(message.User, message.CorrelationId, message.Envelope)) {
 				lock (_lock) {
-					if (_currentScavenge != null && _currentScavenge.ScavengeId == message.ScavengeId) {
+					if (_currentScavenge != null &&
+						(_currentScavenge.ScavengeId == message.ScavengeId || message.ScavengeId == "current")) {
 						_cancellationTokenSource.Cancel();
 
-						message.Envelope.ReplyWith(new ClientMessage.ScavengeDatabaseResponse(message.CorrelationId,
-							ClientMessage.ScavengeDatabaseResponse.ScavengeResult.Stopped,
-							message.ScavengeId));
+						_currentScavengeTask.ContinueWith(_ => {
+							message.Envelope.ReplyWith(new ClientMessage.ScavengeDatabaseResponse(message.CorrelationId,
+								ClientMessage.ScavengeDatabaseResponse.ScavengeResult.Stopped,
+								_currentScavenge.ScavengeId));
+						});
 					} else {
 						message.Envelope.ReplyWith(new ClientMessage.ScavengeDatabaseResponse(message.CorrelationId,
 							ClientMessage.ScavengeDatabaseResponse.ScavengeResult.InvalidScavengeId,
 							_currentScavenge?.ScavengeId));
+					}
+				}
+			}
+		}
+
+		public void Handle(ClientMessage.GetDatabaseScavenge message) {
+			if (IsAllowed(message.User, message.CorrelationId, message.Envelope)) {
+				lock (_lock) {
+					if (_currentScavenge != null) {
+						message.Envelope.ReplyWith(new ClientMessage.ScavengeDatabaseResponse(
+							message.CorrelationId,
+							ClientMessage.ScavengeDatabaseResponse.ScavengeResult.InProgress,
+							_currentScavenge.ScavengeId));
+					} else {
+						message.Envelope.ReplyWith(new ClientMessage.ScavengeDatabaseResponse(
+							message.CorrelationId,
+							ClientMessage.ScavengeDatabaseResponse.ScavengeResult.Stopped,
+							scavengeId: null));
 					}
 				}
 			}
