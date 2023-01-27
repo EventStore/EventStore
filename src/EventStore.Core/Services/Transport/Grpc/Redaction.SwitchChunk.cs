e08ab3a3f47446b10f6d5f480115dafa54b13356
shaan1337 <sniper111@gmail.com>
2023-01-27 16:35:24 +0400

    improve locking

--- src/EventStore.Core/Services/Transport/Grpc/Redaction.SwitchChunk.cs
+++ src/EventStore.Core/Services/Transport/Grpc/Redaction.SwitchChunk.cs
@@ -21,34 +21,41 @@ namespace EventStore.Core.Services.Transport.Grpc {
 				throw RpcExceptions.AccessDenied();
 			}
 
-			await SwitchChunksLock(context).ConfigureAwait(false);
+			await SwitchChunksLock().ConfigureAwait(false);
 			try {
-				await SwitchChunks(requestStream, responseStream).ConfigureAwait(false);
+				await SwitchChunks(context, requestStream, responseStream).ConfigureAwait(false);
 			} finally {
 				await SwitchChunksUnlock().ConfigureAwait(false);
 			}
 		}
 
-		private async Task SwitchChunksLock(ServerCallContext context) {
+		private async Task SwitchChunksLock() {
 			var lockTcs = new TaskCompletionSource<RedactionMessage.SwitchChunkLockCompleted>();
 			_bus.Publish(new RedactionMessage.SwitchChunkLock(
 				new CallbackEnvelope(msg => lockTcs.SetResult(msg as RedactionMessage.SwitchChunkLockCompleted))
 			));
 
 			var completionMsg = await lockTcs.Task.ConfigureAwait(false);
-			if (completionMsg is null)
-				throw new Exception($"Unexpected message type.");
+
+			// it's important not to throw any exceptions beyond this point in this method
+			// (except to indicate that acquiring the lock failed), otherwise we can end up
+			// in a situation where the lock remains permanently acquired.
+
+			if (completionMsg is null) // should never happen
+				return;
 
 			var result = completionMsg.Result;
 			if (result != SwitchChunkLockResult.Success)
 				throw RpcExceptions.RedactionLockFailed();
-
-			await context.WriteResponseHeadersAsync(new Metadata()).ConfigureAwait(false);
 		}
 
 		private async Task SwitchChunks(
+			ServerCallContext context,
 			IAsyncStreamReader<SwitchChunkReq> requestStream,
 			IServerStreamWriter<SwitchChunkResp> responseStream) {
+
+			// write empty headers to indicate that acquiring the lock was successful
+			await context.WriteResponseHeadersAsync(new Metadata()).ConfigureAwait(false);
 
 			await foreach(var request in requestStream.ReadAllAsync().ConfigureAwait(false)) {
 				var tcs = new TaskCompletionSource<RedactionMessage.SwitchChunkCompleted>();
