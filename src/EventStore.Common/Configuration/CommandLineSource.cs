535b048d6266688242a6e8b20bc2f63e36d56e93
matsientst <matt.macchia@gmail.com>
2023-02-27 14:21:20 -0700

    Add fix to allow for command line arguments  --cluster-size=3 and --cluster-size 3 and a fix to check for other delimiters besides ,

--- src/EventStore.Common/Configuration/CommandLineSource.cs
+++ src/EventStore.Common/Configuration/CommandLineSource.cs
@@ -11,14 +11,24 @@ namespace EventStore.Common.Configuration {
 
 			static string NormalizeKeys(string x) => x[0] == '-' && x[1] != '-' ? $"-{x}" : x;
 
-			string NormalizeBooleans(string x, int i) => (x[..2] == "--", x[^1]) switch {
-				(true, '+') => $"{x[..^1]}=true",
-				(true, '-') => $"{x[..^1]}=false",
-				(true, _) => !x.Contains("=") && (i == args.Length - 1 || args[i + 1][..2] == "--")
-					? $"{x}=true"
-					: x,
-				_ => x
-			};
+			string NormalizeBooleans(string x, int i) {
+				if (!x.StartsWith("--"))
+					return x;
+
+				if (x.EndsWith('+'))
+					return $"{x[..^1]}=true";
+
+				if (x.EndsWith('-'))
+					return $"{x[..^1]}=false";
+
+				if (x.Contains('='))
+					return x;
+				
+				if (i != args.Length - 1 && !args[i + 1].StartsWith("--"))
+					return x;
+
+				return $"{x}=true";
+			}
 		}
 
 		public IConfigurationProvider Build(IConfigurationBuilder builder) => new CommandLine(_args);
--- src/EventStore.Common/Configuration/ConfigurationRootExtensions.cs
+++ src/EventStore.Common/Configuration/ConfigurationRootExtensions.cs
@@ -4,8 +4,21 @@ using Microsoft.Extensions.Configuration;
 #nullable enable
 namespace EventStore.Common.Configuration {
 	public static class ConfigurationRootExtensions {
-		public static string[] GetCommaSeparatedValueAsArray(this IConfigurationRoot configurationRoot, string key) =>
-			configurationRoot.GetValue<string?>(key)?.Split(',', StringSplitOptions.RemoveEmptyEntries) ??
-			Array.Empty<string>();
+		private static string[] INVALID_DELIMITERS = new[] { ";", "\t" };
+
+		public static string[] GetCommaSeparatedValueAsArray(this IConfigurationRoot configurationRoot, string key) {
+			string? value = configurationRoot.GetValue<string?>(key);
+			if (string.IsNullOrEmpty(value)) {
+				return Array.Empty<string>();
+			}
+
+			foreach (var invalidDelimiter in INVALID_DELIMITERS) {
+				if (value.Contains(invalidDelimiter)) {
+					throw new ArgumentException($"Invalid delimiter {invalidDelimiter} for {key}");
+				}
+			}
+
+			return value.Split(',', StringSplitOptions.RemoveEmptyEntries);
+		}
 	}
 }
