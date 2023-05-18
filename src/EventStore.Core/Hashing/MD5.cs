c779969dc09d3e56d951a91eeb5bc83e548324fb
shaan1337 <sniper111@gmail.com>
2023-05-18 09:36:20 +0400

    Integrate FIPS MD5 plugin

---
+++ src/EventStore.Core/Hashing/MD5.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Security.Cryptography;
+using EventStore.Common.Utils;
+using EventStore.Plugins.MD5;
+
+namespace EventStore.Core.Hashing;
+
+public class MD5 {
+	private static IMD5Provider _provider = new NetMD5Provider();
+
+	public static HashAlgorithm Create() => _provider.Create();
+
+	public static void UseProvider(IMD5Provider md5Provider) {
+		Ensure.NotNull(md5Provider, nameof(md5Provider));
+
+		try {
+			using var _ = md5Provider.Create();
+		} catch (Exception ex) {
+			throw new AggregateException("Failed to use the specified MD5 provider.", ex);
+		}
+
+		_provider = md5Provider;
+	}
+}
