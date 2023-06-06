7fd1efcadf097a249efba054c95f4047711dbdcc
Pascal van Buijtene <pascal.van.buijtene@eventstore.com>
2023-06-06 12:36:56 +0200

    Show mini node logs on error

---
+++ src/EventStore.Core.Tests/Helpers/MiniNodeLogging.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serilog;
+using Serilog.Sinks.InMemory;
+
+namespace EventStore.Core.Tests.Helpers; 
+
+public static class MiniNodeLogging {
+	public static void Setup() {
+		Serilog.Log.Logger = new LoggerConfiguration()
+			.WriteTo.InMemory()
+			.CreateLogger();
+	}
+	
+	public static void WriteLogs() {
+		foreach (var e in InMemorySink.Instance.LogEvents) {
+			Console.Error.WriteLine($"MiniNode: {e.RenderMessage()}");
+			if (e.Exception != null) {
+				Console.Error.WriteLine($"MiniNode: {e.Exception}");
+			}
+		}
+	}
+
+	public static void Clear() {
+		InMemorySink.Instance.Dispose();
+	}
+}
