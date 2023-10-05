67b1c9848edadff1fc059fa0ada7fa412eda6e43
Timothy Coleman <timothy.coleman@gmail.com>
2023-10-05 17:27:35 +0100

    Allow use of an Assembly that is already loaded into another context
    Also sorted out context unloading in the tests on windows.
    Long term I think a better approach might be to construct the resolver with the path to the plugin dll
    so that it can resolve its dependencies, but this would require identifying which dll is the plugin dll
    (perhaps by requiring the directory to have the same name) but also would require changing the way
    our plugins are built.

---
+++ src/EventStore.Core.XUnit.Tests/PluginLoaderTests.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+using System.Threading.Tasks;
+using EventStore.Core.XUnit.Tests;
+using Xunit;
+
+namespace EventStore.PluginHosting.Tests;
+
+public class PluginLoaderTests : IAsyncLifetime {
+	private readonly DirectoryFixture<PluginLoaderTests> _fixture = new();
+
+	public async Task InitializeAsync() {
+		await _fixture.InitializeAsync();
+	}
+
+	public async Task DisposeAsync() {
+		for (int i = 0; i < 3; i++) {
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+		}
+		await _fixture.DisposeAsync();
+	}
+
+	[Fact]
+	public void root_plugin_folder_does_not_exist() {
+		var rootPluginDirectory = new DirectoryInfo(Path.Combine(_fixture.Directory, "nonexistent"));
+		Assert.False(rootPluginDirectory.Exists);
+
+		using var sut = new PluginLoader(rootPluginDirectory);
+		Assert.Empty(sut.Load<ICloneable>());
+	}
+
+	[Fact]
+	public void root_plugin_folder_does_exist_but_is_empty() {
+		var rootPluginDirectory = new DirectoryInfo(_fixture.Directory);
+		Assert.True(rootPluginDirectory.Exists);
+
+		using var sut = new PluginLoader(rootPluginDirectory);
+		Assert.Empty(sut.Load<ICloneable>());
+	}
+
+	[Fact]
+	public void sub_plugin_folder_does_exist_but_is_empty() {
+		var rootPluginDirectory = new DirectoryInfo(_fixture.Directory);
+		var subPluginDirectory = rootPluginDirectory.CreateSubdirectory(Guid.NewGuid().ToString());
+		Assert.True(subPluginDirectory.Exists);
+
+		using var sut = new PluginLoader(rootPluginDirectory);
+		Assert.Empty(sut.Load<ICloneable>());
+	}
+
+	[Fact]
+	public async Task plugin_exists_in_root_plugin_folder() {
+		var rootPluginDirectory = new DirectoryInfo(_fixture.Directory);
+		await BuildFakePlugin(rootPluginDirectory);
+
+		using var sut = new PluginLoader(rootPluginDirectory);
+		Assert.Single(sut.Load<ICloneable>());
+	}
+
+	[Fact]
+	public async Task plugin_exists_in_sub_plugin_folder() {
+		var rootPluginDirectory = new DirectoryInfo(_fixture.Directory);
+		var subPluginDirectory = rootPluginDirectory.CreateSubdirectory(Guid.NewGuid().ToString());
+		await BuildFakePlugin(subPluginDirectory);
+
+		using var sut = new PluginLoader(rootPluginDirectory);
+		Assert.Single(sut.Load<ICloneable>());
+	}
+
+	[Fact]
+	public async Task plugin_exists_in_sub_sub_plugin_folder_should_not_get_loaded() {
+		var rootPluginDirectory = new DirectoryInfo(_fixture.Directory);
+		var subPluginDirectory = rootPluginDirectory.CreateSubdirectory(Guid.NewGuid().ToString());
+		var subSubPluginDirectory = subPluginDirectory.CreateSubdirectory(Guid.NewGuid().ToString());
+		await BuildFakePlugin(subSubPluginDirectory);
+
+		using var sut = new PluginLoader(rootPluginDirectory);
+		Assert.Empty(sut.Load<ICloneable>());
+	}
+
+	private static async Task BuildFakePlugin(DirectoryInfo outputFolder) {
+		var tcs = new TaskCompletionSource<bool>();
+		using var process = new Process {
+			StartInfo = new ProcessStartInfo {
+				FileName = "dotnet",
+				Arguments = $"publish --configuration {BuildConfiguration} --framework=net6.0 --output {outputFolder.FullName}",
+				WorkingDirectory = PluginSourceDirectory,
+				UseShellExecute = false,
+				RedirectStandardError = true,
+				RedirectStandardOutput = true
+			},
+			EnableRaisingEvents = true
+		};
+
+		process.Exited += (_, e) => tcs.SetResult(default);
+		process.Start();
+
+		var stdout = process.StandardOutput.ReadToEndAsync();
+		var stderr = process.StandardError.ReadToEndAsync();
+
+		await Task.WhenAll(tcs.Task, stdout, stderr);
+
+		if (process.ExitCode != 0) {
+			throw new Exception(stdout.Result);
+		}
+	}
+
+	private static string PluginSourceDirectory => Path.Combine(Environment.CurrentDirectory, "FakePlugin");
+
+	private const string BuildConfiguration =
+#if DEBUG
+		"Debug";
+#else
+		"Release";
+#endif
+}
--- src/EventStore.PluginHosting/PluginLoader.cs
+++ src/EventStore.PluginHosting/PluginLoader.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Runtime.Loader;
 
 namespace EventStore.PluginHosting {
-	public class PluginLoader : IDisposable {
+	public sealed class PluginLoader : IDisposable {
 		private readonly DirectoryInfo _rootPluginDirectory;
 		private readonly PluginLoadContext[] _contexts;
 
@@ -23,20 +23,18 @@ namespace EventStore.PluginHosting {
 			}
 		}
 
-		public PluginLoader(DirectoryInfo rootPluginDirectory) {
-			if (rootPluginDirectory == null) {
-				throw new ArgumentNullException(nameof(rootPluginDirectory));
-			}
-			_rootPluginDirectory = rootPluginDirectory;
-			_contexts = PluginDirectories.Select(directory => new PluginLoadContext(directory)).ToArray();
+		public PluginLoader(DirectoryInfo rootPluginDirectory, Assembly shared = null) {
+			_rootPluginDirectory = rootPluginDirectory ?? throw new ArgumentNullException(nameof(rootPluginDirectory));
+			_contexts = PluginDirectories.Select(directory => new PluginLoadContext(directory, shared)).ToArray();
 		}
 
-		public IEnumerable<T> Load<T>() where T : class =>
-			from loadContext in _contexts
-			from pluginType in loadContext.Assemblies.SelectMany(assembly => assembly.GetExportedTypes())
-				.Where(typeof(T).IsAssignableFrom)
-				.ToArray()
-			select (T)Activator.CreateInstance(pluginType);
+		public IEnumerable<T> Load<T>() where T : class {
+			foreach (var loadContext in _contexts)
+				foreach (var assembly in loadContext.Assemblies)
+					foreach (var pluginType in assembly.GetExportedTypes())
+						if (typeof(T).IsAssignableFrom(pluginType))
+							yield return (T)Activator.CreateInstance(pluginType);
+		}
 
 		public void Dispose() {
 			foreach (var context in _contexts) {
@@ -46,19 +44,32 @@ namespace EventStore.PluginHosting {
 
 		private class PluginLoadContext : AssemblyLoadContext {
 			private static readonly string[] Shared = {"Serilog", "YamlDotNet", "EventStore.Plugins"};
+			private readonly Assembly _shared;
 			private readonly AssemblyDependencyResolver _resolver;
 
-			public PluginLoadContext(DirectoryInfo directory) : base(true) {
+			public PluginLoadContext(DirectoryInfo directory, Assembly shared) : base(true) {
 				_resolver = new AssemblyDependencyResolver(directory.FullName);
 				foreach (var library in directory.GetFiles("*.dll")
 					.Where(file => !Shared.Contains(Path.GetFileNameWithoutExtension(file.Name)))) {
 					LoadFromAssemblyPath(library.FullName);
 				}
+				_shared = shared;
 			}
 
 			protected override Assembly Load(AssemblyName assemblyName) {
+				if (assemblyName.Name == _shared?.GetName().Name) {
+					// dont load the dependency at all, use this one from another context
+					return _shared;
+				}
+
 				var path = _resolver.ResolveAssemblyToPath(assemblyName);
-				return path != null ? LoadFromAssemblyPath(path) : null;
+				if (path != null) {
+					// load the dependency into this context
+					return LoadFromAssemblyPath(path);
+				} else {
+					// load the dependency into the default context
+					return null;
+				}
 			}
 		}
 	}
