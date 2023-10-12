6fb40e5821dbafbd34102cc7f7ac13f197c5eaa6
shaan1337 <sniper111@gmail.com>
2023-10-12 16:56:55 +0400

    Cache client certificate authentication results so that authentication requests on already established TLS connections will succeed after certificate reloads (even when the reserved CN has changed)

--- src/EventStore.Core/Services/Transport/Http/Authentication/ClientCertificateAuthenticationProvider.cs
+++ src/EventStore.Core/Services/Transport/Http/Authentication/ClientCertificateAuthenticationProvider.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using EventStore.Common.Utils;
 using EventStore.Core.Services.UserManagement;
+using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.AspNetCore.Http;
 using Serilog;
 
@@ -16,10 +18,62 @@ namespace EventStore.Core.Services.Transport.Http.Authentication {
 		}
 
 		public bool Authenticate(HttpContext context, out HttpAuthenticationRequest request) {
-			request = null;
-			var clientCertificate = context.Connection.ClientCertificate;
-			if (clientCertificate is null) return false;
+			return AuthenticateCached(context, out request);
+		}
 
+		private bool AuthenticateCached(HttpContext context, out HttpAuthenticationRequest request) {
+			// we cache the authentication result as the same TLS connection may be used for multiple HTTP requests.
+			// performance aside, the cache also ensures that authentication requests that were successful in the past
+			// will succeed in the future for the same TLS connection even if certificates are rotated.
+
+			request = null;
+
+			// if the connection doesn't have a client certificate, take a shortcut
+			var clientCertificate = context.Connection.ClientCertificate;
+			if (clientCertificate is null)
+				return false;
+
+			bool authenticated;
+
+			var connectionItems = context.Features.Get<IConnectionItemsFeature>()?.Items;
+			const string connectionItemsKey = "ClientCertificateAuthenticationStatus";
+			if (TryGetDictionaryValue(connectionItems, connectionItemsKey, out var wasAuthenticated)) {
+				authenticated = (bool) wasAuthenticated;
+			} else {
+				authenticated = AuthenticateUncached(context, clientCertificate);
+				TrySetDictionaryValue(connectionItems, connectionItemsKey, authenticated);
+			}
+
+			if (!authenticated)
+				return false;
+
+			request = new HttpAuthenticationRequest(context, "system", "");
+			request.Authenticated(SystemAccounts.System);
+
+			return true;
+		}
+
+		private static bool TryGetDictionaryValue(IDictionary<object, object> dictionary, string key, out object value) {
+			if (dictionary == null) {
+				value = null;
+				return false;
+			}
+
+			lock (dictionary) {
+				return dictionary.TryGetValue(key, out value);
+			}
+		}
+
+		private static bool TrySetDictionaryValue(IDictionary<object, object> dictionary, string key, object value) {
+			if (dictionary == null)
+				return false;
+
+			lock (dictionary) {
+				return dictionary.TryAdd(key, value);
+			}
+		}
+
+		private bool AuthenticateUncached(HttpContext context, X509Certificate2 clientCertificate) {
 			var reservedNodeCN = _getCertificateReservedNodeCommonName();
 			bool hasReservedNodeCN;
 			try {
@@ -49,8 +103,6 @@ namespace EventStore.Core.Services.Transport.Http.Authentication {
 				return false;
 			}
 			
-			request = new HttpAuthenticationRequest(context, "system", "");
-			request.Authenticated(SystemAccounts.System);
 			return true;
 		}
 	}
