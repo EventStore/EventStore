3558c078b69f1b070394861242c1cc021bffd922
RahimaM <rahima.malik@eventstore.com>
2023-10-18 18:33:11 +0100

    Revert "update streams"
    This reverts commit 959674d72d3ff167ed4a8157d8399dffd49007c5.

--- docs/docs/.vuepress/.temp/internal/clientAppEnhances.js
+++
@@ -1,9 +0,0 @@
-import clientAppEnhance0 from 'C:/Users/RahimaM/Desktop/EventStore/docs/node_modules/@vuepress/theme-default/lib/client/clientAppEnhance.js'
-import clientAppEnhance1 from 'C:/Users/RahimaM/Desktop/EventStore/docs/node_modules/@vuepress/plugin-medium-zoom/lib/client/clientAppEnhance.js'
-import clientAppEnhance2 from 'C:/Users/RahimaM/Desktop/EventStore/docs/node_modules/@vuepress/plugin-theme-data/lib/client/clientAppEnhance.js'
-
-export const clientAppEnhances = [
-  clientAppEnhance0,
-  clientAppEnhance1,
-  clientAppEnhance2,
-]
--- docs/docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from 'C:/Users/RahimaM/Desktop/EventStore/docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
