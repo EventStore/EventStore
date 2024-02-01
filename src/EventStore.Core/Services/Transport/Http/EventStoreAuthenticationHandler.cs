8dcf784a2fd47888342fe59a1e2a9972a73d6ca0
Timothy Coleman <timothy.coleman@gmail.com>
2024-02-01 12:43:26 +0000

    Use conventional UseAuthentication/UseAuthorization middleware
    So that the rest of the pipeline can access auth in the conventional way.
    
    For simplicity, this is currently driven by our bespoke AuthenticationMiddleware
    rather than replacing it
    
    Co-authored-by: Pascal van Buijtene <pascal.van.buijtene@eventstore.com>

---
+++ src/EventStore.Core/Services/Transport/Http/EventStoreAuthenticationHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace EventStore.Core.Services.Transport.Http;
+
+// This runs after Services.Transport.Http.AuthenticationMiddleware runs and makes use of the
+// ClaimsPrinciple that it has left in the context.
+// todo: figure out why AuthenticationMiddleware is as complicated as it is and whether it
+// should be replaced with an AuthenticationHandler (or several)
+public class EventStoreAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions> {
+	public EventStoreAuthenticationHandler(
+		IOptionsMonitor<AuthenticationSchemeOptions> options,
+		// we could pass the logger through instead of NullLoggerFactory but we have debug logging
+		// on by default and it will log successes unless we change the level in the config:
+		// "EventStore.Core.Services.Transport.Http.EventStoreAuthenticationHandler": "Information"
+		// instead we should rationalize the log levels, turn off debug logging, and then pass this through.
+		// ILoggerFactory logger,
+		UrlEncoder encoder)
+		: base(options, NullLoggerFactory.Instance, encoder) {
+	}
+
+	protected override Task<AuthenticateResult> HandleAuthenticateAsync() {
+		if (!Context.User.Identity.IsAuthenticated)
+			return Task.FromResult(AuthenticateResult.Fail("Not authenticated"));
+
+		var ticket = new AuthenticationTicket(
+			principal: Context.User,
+			authenticationScheme: "es auth");
+
+		return Task.FromResult(AuthenticateResult.Success(ticket));
+	}
+}
