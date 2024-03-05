4eba6721c9138732cb0cafe62912a3f16fe2d041
Timothy Coleman <timothy.coleman@gmail.com>
2024-03-05 13:54:18 +0000

    simplify

--- src/EventStore.Core/Configuration/OptionMetadata.cs
+++ src/EventStore.Core/Configuration/OptionMetadata.cs
@@ -12,29 +12,25 @@ namespace EventStore.Core;
 
 public record OptionMetadata(
 	string Key,
-	string FullKey,
 	string Name,
 	string Description,
 	string SectionName,
 	string[] AllowedValues,
 	bool IsSensitive,
 	bool IsEnvironmentOnly,
-	string? ErrorMessage,
+	string? EnvironmentOnlyMessage,
 	int Sequence) {
-	public override string ToString() => FullKey;
 
 	public static OptionMetadata FromPropertyInfo(PropertyInfo property, int sequence) {
-		var sectionName = property.DeclaringType?.Name.Replace("Options", "") ?? String.Empty;
+		var sectionName = property.DeclaringType?.Name.Replace("Options", "") ?? "";
 		var key = EventStoreConfigurationKeys.Normalize(property.Name);
-		var fullKey =
-			$"EventStore:{sectionName}:{EventStoreConfigurationKeys.StripConfigurationPrefix(property.Name)}";
 
-		var description = property.GetCustomAttribute<DescriptionAttribute>()?.Description ?? String.Empty;
+		var description = property.GetCustomAttribute<DescriptionAttribute>()?.Description ?? "";
 		var isSensitive = property.GetCustomAttribute<SensitiveAttribute>() != null;
 
 		var environmentOnlyAttribute = property.GetCustomAttribute<EnvironmentOnlyAttribute>();
 		var isEnvironmentOnly = environmentOnlyAttribute != null;
-		var errorMessage = environmentOnlyAttribute?.Message;
+		var environmentOnlyMessage = environmentOnlyAttribute?.Message;
 
 		string[] allowedValues = property.PropertyType.IsEnum
 			? property.PropertyType.GetEnumNames()
@@ -42,14 +38,13 @@ public record OptionMetadata(
 
 		return new(
 			Key: key,
-			FullKey: fullKey,
 			Name: property.Name,
 			Description: description,
 			SectionName: sectionName,
 			AllowedValues: allowedValues,
 			IsSensitive: isSensitive,
 			IsEnvironmentOnly: isEnvironmentOnly,
-			ErrorMessage: errorMessage,
+			EnvironmentOnlyMessage: environmentOnlyMessage,
 			Sequence: sequence
 		);
 	}
