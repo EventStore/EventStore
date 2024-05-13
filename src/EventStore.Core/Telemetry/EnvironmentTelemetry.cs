ad645b99ebff7214febead5bdecc45a2c2cd7b37
Sérgio Silveira <sdsilveira@gmail.com>
2024-05-13 15:59:19 +0200

    trying to fix CVE-2019-0820 Regular Expression Denial of Service in System.Text.RegularExpressions

--- src/EventStore.Core/Telemetry/EnvironmentTelemetry.cs
+++ src/EventStore.Core/Telemetry/EnvironmentTelemetry.cs
@@ -13,18 +13,4 @@ public readonly record struct EnvironmentTelemetry {
             Container =  ContainerInfo.Collect(),
             Machine   =  MachineInfo.Collect(options)
         };
-
-    // public static EnvironmentTelemetry Collect(ClusterVNodeOptions options) =>
-    //     new()  {
-    //         Arch      = RuntimeInformation.Host.Architecture.ToString(),
-    //         Container =  new ContainerInfo (
-    //             RuntimeInformation.IsRunningInContainer,
-    //             RuntimeInformation.IsRunningInKubernetes
-    //         ),
-    //         Machine   =  new MachineInfo(
-    //             Environment.ProcessorCount,
-    //             RuntimeStats.GetTotalMemory(),
-    //             DriveStats.GetDriveInfo(options.Database.Db).TotalBytes
-    //         )
-    //     };
 }
