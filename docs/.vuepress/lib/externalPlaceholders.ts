f9b54c57424a1475277f9942a74fb7899bd8151d
cdevarenne <claude.devarenne@gmail.com>
2024-06-19 13:56:21 -0700

    remove docs directory as we are moving to a mono-repo for all of documentation

--- docs/.vuepress/lib/externalPlaceholders.ts
+++
@@ -1,6 +0,0 @@
-const externalPlaceholders = ["@samples", "@clients"];
-
-export function isKnownPlaceholder(placeholder: string): boolean {
-    const known = externalPlaceholders.find(x => x == placeholder);
-    return known !== undefined;
-}
