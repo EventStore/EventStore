c605ab92c402d47cca72922dae6cbce7369de3a3
Kunal Dhingra <kdhingra55@gmail.com>
2024-06-26 12:05:16 -0400

    Separate projections metrics into multiple instruments
    Also
    - No longer keeps registering additional handlers
    - Progress is now a float so it can be values between 0 and 1
    
    Co-authored-by: Timothy Coleman <timothy.coleman@gmail.com>

---
+++ src/EventStore.Projections.Core/Metrics/ProjectionEventsProcessedMetric.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using EventStore.Core.Metrics;
+using EventStore.Projections.Core.Services;
+
+namespace EventStore.Projections.Core.Metrics;
+
+public class ProjectionEventsProcessedMetric {
+	private readonly ObservableUpDownMetricMulti<long> _statsMetric;
+
+	public ProjectionEventsProcessedMetric(Meter meter, string name) {
+		_statsMetric = new ObservableUpDownMetricMulti<long>(meter, name);
+	}
+
+	public void Register(Func<ProjectionStatistics[]> getCurrentStatsList) {
+		_statsMetric.Register(GetMeasurements);
+
+		IEnumerable<Measurement<long>> GetMeasurements() {
+			var currentStatsList = getCurrentStatsList();
+			foreach (var statistics in currentStatsList) {
+				yield return new(statistics.EventsProcessedAfterRestart, [
+					new("projection", statistics.Name)
+				]);
+			}
+		}
+	}
+}
