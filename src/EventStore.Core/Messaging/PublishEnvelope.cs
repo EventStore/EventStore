d812a2464addd74b60fdafb091ae2cc92cf997b9
Roman Sakno <roman.sakno@eventstore.com>
2024-09-09 20:34:25 +0300

    Added debugger support

--- src/EventStore.Core/Messaging/PublishEnvelope.cs
+++ src/EventStore.Core/Messaging/PublishEnvelope.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using EventStore.Core.Bus;
 
 namespace EventStore.Core.Messaging;
@@ -5,4 +6,7 @@ namespace EventStore.Core.Messaging;
 public class PublishEnvelope(IPublisher publisher) : IEnvelope {
 	public void ReplyWith<T>(T message) where T : Message
 		=> publisher.Publish(message);
+
+	[ExcludeFromCodeCoverage]
+	private object DebugView => publisher;
 }
