70ac64273fcd9f32e219909efca9c4d30e627d7a
Roman Sakno <roman.sakno@eventstore.com>
2024-09-20 10:37:54 +0300

    Added test for parallel loop with async support

--- src/EventStore.Core.XUnit.Tests/Scavenge/ParallelLoopTests.cs
+++ src/EventStore.Core.XUnit.Tests/Scavenge/ParallelLoopTests.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
+using DotNext.Threading;
 using EventStore.Core.TransactionLog.Scavenging;
 using Xunit;
 
 namespace EventStore.Core.XUnit.Tests.Scavenge {
 	public class ParallelLoopTests {
-		void Run(
+		static void Run(
 			int[] source,
 			int[] completionOrder,
 			int[] expectedCheckpoints,
@@ -76,11 +78,86 @@ namespace EventStore.Core.XUnit.Tests.Scavenge {
 			Assert.Equal(expectedCheckpoints, emittedCheckpoints);
 		}
 
+		static async Task RunAsync(
+			int[] source,
+			int[] completionOrder,
+			int[] expectedCheckpoints,
+			int degreeOfParallelism = 2) {
+
+			if (source.Any(x => x % 10 != 0))
+				throw new Exception("use multiples of 10");
+
+			// maps from the item to the mres it should wait for
+			var selfTriggers = new Dictionary<int, AsyncManualResetEvent>();
+			// maps from item to the mres it should trigger when it completes
+			var nextTriggers = new Dictionary<int, AsyncManualResetEvent>();
+
+			var prev = default(int?);
+			foreach (var item in completionOrder) {
+				selfTriggers[item] = new AsyncManualResetEvent(initialState: false);
+				if (prev is not null)
+					nextTriggers[prev.Value] = selfTriggers[item];
+
+				prev = item;
+			}
+
+			if (completionOrder.Length > 0) {
+				nextTriggers[completionOrder.Last()] = new AsyncManualResetEvent(initialState: false);
+				selfTriggers[completionOrder[0]].Set();
+			}
+
+			// make sure only one item completes at a time, to force the sut to always pick the same slot
+			var serializer = new AsyncManualResetEvent(initialState: true);
+			var emittedCheckpoints = new List<int>();
+			var completedItems = new List<int>();
+
+			var loopThread = Thread.CurrentThread.ManagedThreadId;
+			await ParallelLoop.RunWithTrailingCheckpointAsync(
+				source: source,
+				degreeOfParallelism: degreeOfParallelism,
+				getCheckpointInclusive: x => x,
+				getCheckpointExclusive: x => {
+					var chunkStartNumber = x == 10
+						? x - 10
+						: x - 9;
+					if (chunkStartNumber == 00)
+						return null;
+					return chunkStartNumber - 1;
+				},
+				process: async (slot, x, token) => {
+					Assert.NotEqual(loopThread, Thread.CurrentThread.ManagedThreadId);
+
+					// wait until we are complete
+					await selfTriggers[x].WaitAsync(token);
+					completedItems.Add(x);
+
+					await serializer.WaitAsync(token);
+					serializer.Reset();
+
+					// complete the next in line
+					nextTriggers[x].Set();
+				},
+				emitCheckpoint: checkpoint => {
+					emittedCheckpoints.Add(checkpoint);
+				},
+				onConsiderEmit: () => serializer.Set());
+
+
+			Assert.Equal(completionOrder, completedItems);
+			Assert.Equal(expectedCheckpoints, emittedCheckpoints);
+		}
+
 		[Fact]
 		public void process_out_of_order() => Run(
 			source: new int[] { 10, 20, 30, 40, 50 },
 			completionOrder: new int[] { 20, 30, 40, 10, 50 },
 			expectedCheckpoints: new int[] { 40, 50 });
+
+		[Fact]
+		public async Task process_out_of_order_async() => await RunAsync(
+			source: [10, 20, 30, 40, 50],
+			completionOrder: [20, 30, 40, 10, 50],
+			expectedCheckpoints: [40, 50]);
 
 		[Fact]
 		public void process_very_out_of_order() => Run(
