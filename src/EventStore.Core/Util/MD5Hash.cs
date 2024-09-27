f52c951bda16177be1032e3283cdd9172696ca70
Timothy Coleman <timothy.coleman@gmail.com>
2024-09-27 10:07:35 +0100

    Apply license header to source files

--- src/EventStore.Core/Util/MD5Hash.cs
+++ src/EventStore.Core/Util/MD5Hash.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Event Store Ltd and/or licensed to Event Store Ltd under one or more agreements.
+// Event Store Ltd licenses this file to you under the Event Store License v2 (see LICENSE.md).
+
 using System;
 using System.IO;
 using System.Security.Cryptography;
