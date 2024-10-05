d5aab5e6f14319067ba9cd019e815b3d6cb48a2c
Timothy Coleman <timothy.coleman@gmail.com>
2024-10-05 17:47:17 +0100

    fix source generator tests

--- src/EventStore.SourceGenerators.Tests/Messaging/Cases/Message.cs
+++ src/EventStore.SourceGenerators.Tests/Messaging/Cases/Message.cs
@@ -1,34 +1,34 @@
 using System;
 
-namespace EventStore.SourceGenerators.Tests.Messaging;
-
-[AttributeUsage(AttributeTargets.Class)]
-public class BaseMessageAttribute : Attribute {
-	public BaseMessageAttribute() {
-	}
-}
-
-[AttributeUsage(AttributeTargets.Class)]
-public class DerivedMessageAttribute : Attribute {
-	public DerivedMessageAttribute() {
+namespace EventStore.SourceGenerators.Tests.Messaging {
+	[AttributeUsage(AttributeTargets.Class)]
+	public class BaseMessageAttribute : Attribute {
+		public BaseMessageAttribute() {
+		}
 	}
 
-	public DerivedMessageAttribute(object messageGroup) {
+	[AttributeUsage(AttributeTargets.Class)]
+	public class DerivedMessageAttribute : Attribute {
+		public DerivedMessageAttribute() {
+		}
+
+		public DerivedMessageAttribute(object messageGroup) {
+		}
 	}
-}
 
-[BaseMessage]
-public abstract partial class Message {
-}
+	[BaseMessage]
+	public abstract partial class Message {
+	}
 
-enum TestMessageGroup {
-	None,
-	Abstract,
-	AbstractWithGroup,
-	FileScopedNamespace,
-	Impartial,
-	ImpartialNested,
-	Nested,
-	NestedDerived,
-	Simple,
+	enum TestMessageGroup {
+		None,
+		Abstract,
+		AbstractWithGroup,
+		FileScopedNamespace,
+		Impartial,
+		ImpartialNested,
+		Nested,
+		NestedDerived,
+		Simple,
+	}
 }
