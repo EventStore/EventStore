158833937ca8d5d46d30dc3510ea1e53c571f9b9
Timothy Coleman <timothy.coleman@gmail.com>
2024-11-08 07:18:59 +0000

    add chunkprioritizer tests

--- src/EventStore.Core/Services/Archiver/ChunkPrioritizer.cs
+++ src/EventStore.Core/Services/Archiver/ChunkPrioritizer.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 namespace EventStore.Core.Services.Archiver;
 
+// prioritizes earlier chunks
 public class ChunkPrioritizer : IComparer<Commands.ArchiveChunk> {
 	public int Compare(Commands.ArchiveChunk x, Commands.ArchiveChunk y) {
 		int cmp = x!.ChunkStartNumber.CompareTo(y!.ChunkStartNumber);
