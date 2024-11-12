a681f017a65493156b9ece1ffce7ed1fe3436b5b
shaan1337 <sniper111@gmail.com>
2024-11-12 09:34:33 +0400

    ChunkPrioritizer: #nullable enable

--- src/EventStore.Core/Services/Archiver/ChunkPrioritizer.cs
+++ src/EventStore.Core/Services/Archiver/ChunkPrioritizer.cs
@@ -1,13 +1,15 @@
 // Copyright (c) Event Store Ltd and/or licensed to Event Store Ltd under one or more agreements.
 // Event Store Ltd licenses this file to you under the Event Store License v2 (see LICENSE.md).
 
+#nullable enable
+
 using System.Collections.Generic;
 
 namespace EventStore.Core.Services.Archiver;
 
 // prioritizes earlier chunks
 public class ChunkPrioritizer : IComparer<Commands.ArchiveChunk> {
-	public int Compare(Commands.ArchiveChunk x, Commands.ArchiveChunk y) {
+	public int Compare(Commands.ArchiveChunk? x, Commands.ArchiveChunk? y) {
 		int cmp = x!.ChunkStartNumber.CompareTo(y!.ChunkStartNumber);
 
 		if (cmp != 0)
