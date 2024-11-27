1b7d7e2a1ec1f591e9afee2d9d29e8a84c11bb97
Timothy Coleman <timothy.coleman@gmail.com>
2024-11-27 08:12:25 +0000

    even less granular

--- src/EventStore.Core/Services/UserManagement/SystemAccounts.cs
+++ src/EventStore.Core/Services/UserManagement/SystemAccounts.cs
@@ -11,15 +11,17 @@ namespace EventStore.Core.Services.UserManagement {
 		public static readonly ClaimsPrincipal System = new ClaimsPrincipal(new ClaimsIdentity(Claims, "system"));
 		public static readonly ClaimsPrincipal Anonymous = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]{new Claim(ClaimTypes.Anonymous, ""), }));
 
-		public static readonly string SystemChaserName = "system-chaser";
+		// we may want to make the granularity here configurable, but as a starting point we only
+		// separate scavenge, because its the only part the user has direct control over
+		public static readonly string SystemChaserName = "system";
 		public static readonly string SystemEpochManagerName = "system";
 		public static readonly string SystemName = "system";
 		public static readonly string SystemIndexCommitterName = "system";
-		public static readonly string SystemPersistentSubscriptionsName = "system-persistent-subscriptions";
+		public static readonly string SystemPersistentSubscriptionsName = "system";
 		public static readonly string SystemRedactionName = "system";
-		public static readonly string SystemReplicationName = "system-replication";
+		public static readonly string SystemReplicationName = "system";
 		public static readonly string SystemScavengeName = "system-scavenge";
-		public static readonly string SystemSubscriptionsName = "system-subscriptions";
+		public static readonly string SystemSubscriptionsName = "system";
 		public static readonly string SystemTelemetryName = "system";
 		public static readonly string SystemWriterName = "system";
 	}
