3718df00afc98dba33275ab75b06c250921830ef
Timothy Coleman <timothy.coleman@gmail.com>
2024-12-16 15:09:23 +0000

    Add archive retention options

--- src/EventStore.Core/Services/Archive/ArchiveOptions.cs
+++ src/EventStore.Core/Services/Archive/ArchiveOptions.cs
@@ -8,6 +8,7 @@ public class ArchiveOptions {
 	public StorageType StorageType { get; init; } = StorageType.Unspecified;
 	public FileSystemOptions FileSystem { get; init; } = new();
 	public S3Options S3 { get; init; } = new();
+	public RetentionOptions RetainAtLeast { get; init; } = new();
 }
 
 public enum StorageType {
@@ -24,4 +25,10 @@ public class S3Options {
 	public string AwsCliProfileName { get; init; } = "default";
 	public string Bucket { get; init; } = "";
 	public string Region { get; init; } = "";
+}
+
+public class RetentionOptions {
+	public long Days { get; init; }
+	// number of bytes in the logical log
+	public long LogicalBytes { get; init; }
 }
