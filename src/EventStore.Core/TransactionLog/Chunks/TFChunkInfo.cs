140ed43e9cdba8fddc970e7e9d432162a581237f
Timothy Coleman <timothy.coleman@gmail.com>
2025-01-06 09:23:10 +0000

    rename MissingVersion.Start to ChunkNumber

--- src/EventStore.Core/TransactionLog/Chunks/TFChunkInfo.cs
+++ src/EventStore.Core/TransactionLog/Chunks/TFChunkInfo.cs
@@ -6,4 +6,4 @@ namespace EventStore.Core.TransactionLog.Chunks;
 public record TFChunkInfo(string fileName);
 public record LatestVersion(string fileName, int start, int end) : TFChunkInfo(fileName);
 public record OldVersion(string fileName, int start) : TFChunkInfo(fileName);
-public record MissingVersion(string fileName, int start) : TFChunkInfo(fileName);
+public record MissingVersion(string fileName, int chunkNumber) : TFChunkInfo(fileName);
