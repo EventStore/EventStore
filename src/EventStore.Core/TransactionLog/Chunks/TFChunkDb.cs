4368090557219c0014c7a216fd51023957e72a20
Roman Sakno <roman.sakno@eventstore.com>
2025-01-07 14:55:02 +0200

    Fixed Qodana warnings

--- src/EventStore.Core/TransactionLog/Chunks/TFChunkDb.cs
+++ src/EventStore.Core/TransactionLog/Chunks/TFChunkDb.cs
@@ -7,10 +7,8 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using DotNext.Buffers;
 using EventStore.Common.Utils;
 using EventStore.Core.Exceptions;
-using EventStore.Core.TransactionLog.Chunks.TFChunk;
 using EventStore.Core.Transforms;
 using ILogger = Serilog.ILogger;
 
