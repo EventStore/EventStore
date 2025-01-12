a9c50b964b4034a8d10bc1cc5c5c15fe1d724d36
Alexey Zimarev <alex@zimarev.com>
2025-01-12 13:23:52 +0100

    Add stream id cache

--- src/EventStore.Core/Duck/DuckDb.cs
+++ src/EventStore.Core/Duck/DuckDb.cs
@@ -8,6 +8,7 @@ using Dapper;
 using DuckDB.NET.Data;
 using EventStore.Core.Index;
 using EventStore.Core.Metrics;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace EventStore.Core.Duck;
 
@@ -25,18 +26,31 @@ public static class DuckDb {
 
 	public static IReadOnlyList<IndexEntry> GetRange(string streamName, ulong streamId, long fromEventNumber, int maxCount) {
 		using var _ = TempIndexMetrics.MeasureIndex("get_range");
-		const string query =
-			"select event_number, log_position from idx_all inner join streams on idx_all.stream=streams.id where streams.name=$stream and event_number>=$start order by event_number limit $count";
+		const string query = "select event_number, log_position from idx_all where stream=$stream and event_number>=$start order by event_number limit $count";
 
 		while (true) {
 			try {
-				var result = Connection.Query<IndexRecord>(query, new { stream = streamName, start = fromEventNumber, count = maxCount });
+				var stream = GetStreamId(streamName);
+				var result = Connection.Query<IndexRecord>(query, new { stream, start = fromEventNumber, count = maxCount });
 				return result.Select(x => new IndexEntry(streamId, x.event_number, x.log_position)).ToList();
 			} catch (Exception e) {
 				Console.WriteLine(e);
 			}
 		}
 	}
+
+	static long GetStreamId(string streamName) {
+		return _cache.GetOrCreate(streamName, GetFromDb);
+
+		static long GetFromDb(ICacheEntry arg) {
+			const string sql = "select id from streams where name=$name";
+			arg.SlidingExpiration = TimeSpan.FromMinutes(10);
+			return Connection.Query<long>(sql, new { name = arg.Key }).SingleOrDefault();
+		}
+	}
+
+
+	static readonly MemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
 
 	class IndexRecord {
 		public int event_number { get; set; }
