1030aeebaf4ef7a801f1f467577fa4c82f091c73
Alexey Zimarev <alex@zimarev.com>
2025-01-12 11:59:06 +0100

    Add try-catch

--- src/EventStore.Core/Duck/DuckDb.cs
+++ src/EventStore.Core/Duck/DuckDb.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Event Store Ltd and/or licensed to Event Store Ltd under one or more agreements.
 // Event Store Ltd licenses this file to you under the Event Store License v2 (see LICENSE.md).
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Dapper;
@@ -20,14 +21,21 @@ public static class DuckDb {
 		Connection.Close();
 	}
 
-	public static DuckDBConnection Connection;
+	static DuckDBConnection Connection;
 
 	public static IReadOnlyList<IndexEntry> GetRange(string streamName, ulong streamId, long fromEventNumber, int maxCount) {
 		using var _ = TempIndexMetrics.MeasureIndex("get_range");
-		const string query = "select event_number, log_position from idx_all inner join streams on idx_all.stream=streams.id where streams.name=$stream and event_number>=$start order by event_number limit $count";
+		const string query =
+			"select event_number, log_position from idx_all inner join streams on idx_all.stream=streams.id where streams.name=$stream and event_number>=$start order by event_number limit $count";
 
-		var result = Connection.Query<IndexRecord>(query, new { stream = streamName, start = fromEventNumber, count = maxCount });
-		return result.Select(x => new IndexEntry(streamId, x.event_number, x.log_position)).ToList();
+		while (true) {
+			try {
+				var result = Connection.Query<IndexRecord>(query, new { stream = streamName, start = fromEventNumber, count = maxCount });
+				return result.Select(x => new IndexEntry(streamId, x.event_number, x.log_position)).ToList();
+			} catch (Exception e) {
+				Console.WriteLine(e);
+			}
+		}
 	}
 
 	class IndexRecord {
