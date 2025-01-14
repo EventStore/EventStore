668b11f58e7afbf3c15eba05f76181cc7bf8270f
Alexey Zimarev <alex@zimarev.com>
2025-01-14 11:25:58 +0100

    Increase the number of external threads

--- src/EventStore.Core/Duck/DuckDb.cs
+++ src/EventStore.Core/Duck/DuckDb.cs
@@ -30,6 +30,7 @@ public static class DuckDb {
 		Connection = new("Data Source=./data/file.db");
 		Connection.Open();
 		Connection.Execute("SET threads = 10;");
+		Connection.Execute("SET external_threads = 10;");
 		Connection.Execute("SET memory_limit = '8GB';");
 
 		Categories = Connection.Query<ReferenceRecord>("select * from category").ToDictionary(x => x.name, x => x.id);
