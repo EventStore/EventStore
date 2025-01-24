7213231794b8d7cc0653d54c0aeb0010e71da21c
shaan1337 <sniper111@gmail.com>
2025-01-24 10:49:59 +0400

    Fix bug in ticks conversion: Stopwatch.Frequency is not necessarily a multiple of TimeSpan.TicksPerSecond In particular, on Windows, it'll have the frequency of the high resolution timer if present. On linux, it's fixed to 1 billion.

--- src/EventStore.Core.XUnit.Tests/Metrics/InstantTests.cs
+++ src/EventStore.Core.XUnit.Tests/Metrics/InstantTests.cs
@@ -9,10 +9,27 @@ namespace EventStore.Core.XUnit.Tests.Metrics;
 
 public class InstantTests {
 	[Fact]
-	public void can_measure_elapsed() {
+	public void can_measure_elapsed_seconds() {
 		var x = Instant.FromSeconds(4);
 		var y = Instant.FromSeconds(6);
 		Assert.Equal(2, y.ElapsedSecondsSince(x));
+	}
+
+	[Theory]
+	[InlineData(4, 4, 0)]
+	[InlineData(4, 6, 2_000_000)]
+	[InlineData(123, 1_000_000_000, 999_999_877_000_000)]
+	public void can_measure_elapsed_time(int startSecs, int endSecs, long elapsedMicroseconds) {
+		var x = Instant.FromSeconds(startSecs);
+		var y = Instant.FromSeconds(endSecs);
+		Assert.Equal(elapsedMicroseconds, y.ElapsedTimeSince(x).TotalMicroseconds);
+	}
+
+	[Fact]
+	public void rounds_up_elapsed_time() {
+		var x = Instant.Now;
+		var y = new Instant(x.Ticks + 1);
+		Assert.True(y.ElapsedTimeSince(x).Ticks > 0);
 	}
 
 	[Fact]
