ecee61efa1cad288c6dc79fe588ffab438866931
Alexey Zimarev <alex@zimarev.com>
2025-01-27 22:10:37 +0100

    Remove excessive logging

--- src/EventStore.Core/Services/Transport/Http/KestrelToInternalBridgeMiddleware.cs
+++ src/EventStore.Core/Services/Transport/Http/KestrelToInternalBridgeMiddleware.cs
@@ -23,12 +23,9 @@ public class KestrelToInternalBridgeMiddleware(IUriRouter uriRouter, bool logHtt
 	}
 
 	private static bool TryMatch(HttpContext context, IUriRouter uriRouter, bool logHttpRequests, string advertiseAsAddress, int advertiseAsPort) {
-		Log.Debug("Trying to match {Path}", context.Request.Path);
 		if (context.IsGrpc() || context.Request.Path.StartsWithSegments("/ui")) {
-			Log.Debug("Ignoring endpoint {Path}", context.Request.Path);
 			return true;
 		}
-		Log.Debug("Request to {Path} using {ContentType} is neither gRPC or UI", context.Request.Path, context.Request.ContentType);
 
 		var tcs = new TaskCompletionSource<bool>();
 		var httpEntity = new HttpEntity(context, logHttpRequests, advertiseAsAddress, advertiseAsPort, () => tcs.TrySetResult(true));
