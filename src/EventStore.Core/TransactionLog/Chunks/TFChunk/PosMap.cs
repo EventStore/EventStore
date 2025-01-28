1c8557f28637dfc6b86ccfea004b283897b72dab
Roman Sakno <roman.sakno@eventstore.com>
2025-01-28 13:39:29 +0200

    Fixed Qodana warnings

--- src/EventStore.Core/TransactionLog/Chunks/TFChunk/PosMap.cs
+++ src/EventStore.Core/TransactionLog/Chunks/TFChunk/PosMap.cs
@@ -9,7 +9,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using DotNext.Buffers;
 using DotNext.Buffers.Binary;
-using DotNext.IO;
 
 namespace EventStore.Core.TransactionLog.Chunks.TFChunk;
 
