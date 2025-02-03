75f99c36b73dd39088093cae2d941bf2ef3ce412
Tim Coleman <timothy.coleman@gmail.com>
2025-02-03 14:54:34 +0100

    Validate against attempts to set metadata for the "" stream (#4799) (#4806)
    "" has never been a valid stream name. Attempting to set the metadata for it results in an attempt to write to the stream "$$" which, until now, has been a valid stream name.
    
    However, writing to $$ involves checking on the "" stream, to see if it is soft deleted. This results in the storage writer exiting which shuts down the server to avoid a 'sick but not dead' scenario
    
    This PR makes "$$" an invalid stream name, and so the attempt to write to it is rejected at an early stage.

--- src/EventStore.Core/Messages/ClientMessage.cs
+++ src/EventStore.Core/Messages/ClientMessage.cs
@@ -179,10 +179,14 @@ namespace EventStore.Core.Messages {
 				string eventStreamId, long expectedVersion, Event[] events, ClaimsPrincipal user,
 				IReadOnlyDictionary<string, string> tokens = null, CancellationToken cancellationToken = default)
 				: base(internalCorrId, correlationId, envelope, requireLeader, user, tokens) {
-				Ensure.NotNullOrEmpty(eventStreamId, "eventStreamId");
+
+				if (SystemStreams.IsInvalidStream(eventStreamId))
+					throw new ArgumentOutOfRangeException(nameof(eventStreamId));
+
 				if (expectedVersion < Data.ExpectedVersion.StreamExists ||
 				    expectedVersion == Data.ExpectedVersion.Invalid)
 					throw new ArgumentOutOfRangeException(nameof(expectedVersion));
+
 				Ensure.NotNull(events, "events");
 
 				EventStreamId = eventStreamId;
