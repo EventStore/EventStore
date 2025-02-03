06bd5860c803bbd7c40970783f31d3fe24edf6c2
Tim Coleman <timothy.coleman@gmail.com>
2025-02-03 13:03:26 +0100

    Validate against attempts to set metadata for the "" stream (#4799)
    "" has never been a valid stream name. Attempting to set the metadata for it results in an attempt to write to the stream "$$" which, until now, has been a valid stream name.
    
    However, writing to $$ involves checking on the "" stream, to see if it is soft deleted. This results in the storage writer exiting which shuts down the server to avoid a 'sick but not dead' scenario
    
    This PR makes "$$" an invalid stream name, and so the attempt to write to it is rejected at an early stage.

--- src/EventStore.Core/Services/SystemNames.cs
+++ src/EventStore.Core/Services/SystemNames.cs
@@ -33,6 +33,11 @@ namespace EventStore.Core.Services {
 			return streamId.Length != 0 && streamId[0] == '$';
 		}
 
+		// "" is an invalid name, and so metadata cannot be set for it
+		public static bool IsInvalidStream(string streamId) {
+			return string.IsNullOrEmpty(streamId) || streamId == "$$";
+		}
+
 		public static string MetastreamOf(string streamId) {
 			return "$$" + streamId;
 		}
