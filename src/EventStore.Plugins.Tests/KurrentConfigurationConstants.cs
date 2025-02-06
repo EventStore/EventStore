1cf7a2d036110de389dda11d87bfef668f964f3c
Timothy Coleman <timothy.coleman@gmail.com>
2025-02-06 06:30:21 +0000

    Import OTLP Exporter plugin into main repo

---
+++ src/EventStore.Plugins.Tests/KurrentConfigurationConstants.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Event Store Ltd and/or licensed to Event Store Ltd under one or more agreements.
+// Event Store Ltd licenses this file to you under the Event Store License v2 (see LICENSE.md).
+
+namespace EventStore.Plugins.Tests;
+
+public static class KurrentConfigurationConstants {
+	public const string Prefix = "KurrentDB";
+}
