600edd4e0407ca0d6c1b668933f2b2f3634016f0
Timothy Coleman <timothy.coleman@gmail.com>
2025-02-11 14:58:06 +0000

    Whitespace

--- src/EventStore.Projections.Core.Tests/Services/paused_at_event_distribution_point.cs
+++ src/EventStore.Projections.Core.Tests/Services/paused_at_event_distribution_point.cs
@@ -1,4 +1,3 @@
 // Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
 // Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
-
