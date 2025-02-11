895480f5984807e34b29227be5e35915d8a6264d
Timothy Coleman <timothy.coleman@gmail.com>
2025-02-11 14:44:35 +0000

    Remove Kurrent header from 3rd party files

--- src/EventStore.Core/Services/Transport/Http/GrpcProtocolHelpers.cs
+++ src/EventStore.Core/Services/Transport/Http/GrpcProtocolHelpers.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
-// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
-
 #pragma warning disable IDE0073 // The file header does not match the required text
 // From https://github.com/grpc/grpc-dotnet
 
