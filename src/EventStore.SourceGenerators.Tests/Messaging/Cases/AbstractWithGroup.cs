021cb862548e80771ab944630e65431933e8e399
Timothy Coleman <timothy.coleman@gmail.com>
2025-02-11 14:57:56 +0000

    Remove header from source generator test cases

--- src/EventStore.SourceGenerators.Tests/Messaging/Cases/AbstractWithGroup.cs
+++ src/EventStore.SourceGenerators.Tests/Messaging/Cases/AbstractWithGroup.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
-// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
-
 namespace EventStore.SourceGenerators.Tests.Messaging.AbstractWithGroup {
 	[DerivedMessage(TestMessageGroup.AbstractWithGroup)]
 	public abstract partial class A : Message {
--- src/EventStore.SourceGenerators.Tests/Messaging/Cases/NestedDerived.cs
+++ src/EventStore.SourceGenerators.Tests/Messaging/Cases/NestedDerived.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
-// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
-
 namespace EventStore.SourceGenerators.Tests.Messaging.NestedDerived {
 	[DerivedMessage(TestMessageGroup.NestedDerived)]
 	partial class B : Message {
