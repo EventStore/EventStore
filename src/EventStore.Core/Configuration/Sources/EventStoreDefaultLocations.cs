2ae095aeaae7f4430a2bbe98a4a7447051efd8f1
William Chong <william-chong@outlook.com>
2025-02-24 09:41:24 +0400

    feat: solution filter for connectors plugin

--- src/EventStore.Core/Configuration/Sources/EventStoreDefaultLocations.cs
+++ src/EventStore.Core/Configuration/Sources/EventStoreDefaultLocations.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Event Store Ltd and/or licensed to Event Store Ltd under one or more agreements.
 // Event Store Ltd licenses this file to you under the Event Store License v2 (see LICENSE.md).
 
-# nullable enable
+#nullable enable
 
 using System.Collections.Generic;
 using System.IO.Abstractions;
