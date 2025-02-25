877ddb3aba2315cad1b6c778e60293272fc2c49d
Alexey Zimarev <alex@zimarev.com>
2025-02-25 19:08:14 +0100

    Removed excessive temp logging

--- src/KurrentDB/Services/AuthStateProvider.cs
+++ src/KurrentDB/Services/AuthStateProvider.cs
@@ -12,25 +12,20 @@ namespace KurrentDB.Services;
 
 public class AuthStateProvider : RevalidatingServerAuthenticationStateProvider {
 	AuthenticationState _authenticationState;
-	readonly ILogger<AuthStateProvider> _log;
 
 	public AuthStateProvider(AuthService service, ILoggerFactory loggerFactory) : base(loggerFactory) {
 		_authenticationState = new(service.CurrentUser);
-		_log = loggerFactory.CreateLogger<AuthStateProvider>();
 		service.UserChanged += newUser => {
-			_log.LogInformation("User changed to {@newUser}", newUser.Identity?.Name);
 			_authenticationState = new(newUser);
 			NotifyAuthenticationStateChanged(Task.FromResult(_authenticationState));
 		};
 	}
 
 	public override Task<AuthenticationState> GetAuthenticationStateAsync() {
-		_log.LogInformation("Returning authentication state {@user}", _authenticationState.User.Identity?.Name);
 		return Task.FromResult(_authenticationState);
 	}
 
 	protected override Task<bool> ValidateAuthenticationStateAsync(AuthenticationState authenticationState, CancellationToken cancellationToken) {
-		_log.LogInformation("Revalidating auth state for {@user}", authenticationState.User.Identity?.Name);
 		return Task.FromResult(authenticationState.User.Identity?.IsAuthenticated == true);
 	}
 
