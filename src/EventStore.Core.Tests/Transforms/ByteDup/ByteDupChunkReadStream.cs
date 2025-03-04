667da46448e1f9970e0fbd3f6e91ba31bf0659df
Roman Sakno <roman.sakno@eventstore.com>
2025-03-04 16:07:19 +0200

    Fixed compiler errors

--- src/EventStore.Core.Tests/Transforms/ByteDup/ByteDupChunkReadStream.cs
+++ src/EventStore.Core.Tests/Transforms/ByteDup/ByteDupChunkReadStream.cs
@@ -11,11 +11,11 @@ public class ByteDupChunkReadStream(ChunkDataReadStream stream)
 	: ChunkDataReadStream(stream.ChunkFileStream) {
 	private const int HeaderSize = 128;
 
-	public override int Read(byte[] buffer, int offset, int count) {
-		var buf = new byte[count * 2];
-		int numRead = base.Read(buf, 0, buf.Length);
-		for (int i = 0; i < count; i++)
-			buffer[i + offset] = buf[i * 2];
+	public override int Read(Span<byte> buffer) {
+		var buf = new byte[buffer.Length * 2];
+		int numRead = base.Read(buf);
+		for (int i = 0; i < buffer.Length; i++)
+			buffer[i] = buf[i * 2];
 
 		return numRead / 2;
 	}
