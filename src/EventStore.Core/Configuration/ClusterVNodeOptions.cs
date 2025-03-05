36947376ac32af251c3d227d01df918293e3c18e
Timothy Coleman <timothy.coleman@gmail.com>
2025-03-05 11:31:39 +0000

    pedantic :)

--- src/EventStore.Core/Configuration/ClusterVNodeOptions.cs
+++ src/EventStore.Core/Configuration/ClusterVNodeOptions.cs
@@ -157,7 +157,7 @@ public partial record ClusterVNodeOptions {
 		             "May not exceed 16MB.")]
 		public int MaxAppendSize { get; init; } = 1_024 * 1_024;
 
-		[Description("The maximum size of an individual event in an append request received over gRPC or HTTP, in bytes. ")]
+		[Description("The maximum size of an individual event in an append request received over gRPC or HTTP, in bytes.")]
 		public int MaxAppendEventSize { get; init; } = int.MaxValue;
 
 		[Description("Disable Authentication, Authorization and TLS on all TCP/HTTP interfaces.")]
