36be884695e414474380816e91c899b5272b9646
Timothy Coleman <timothy.coleman@gmail.com>
2025-03-12 07:55:54 +0000

    Refactor: pass ArchiveOptions into the calculator
    To save having another location where they are resolved out of the IConfiguration

--- src/EventStore.Core/ClusterVNode.cs
+++ src/EventStore.Core/ClusterVNode.cs
@@ -1249,7 +1249,7 @@ public class ClusterVNode<TStreamId> :
 
 		scavengerFactory = new ScavengerFactory((message, scavengerLogger, logger) => {
 			// currently on the main queue
-			var optionsCalculator = new ScavengeOptionsCalculator(options, message);
+			var optionsCalculator = new ScavengeOptionsCalculator(options, archiveOptions, message);
 
 			var throttle = new Throttle(
 				logger: logger,
