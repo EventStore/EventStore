f0ba13720742a6d87a4bd67b1b443d6df93d3195
YoEight <yo.eight@gmail.com>
2025-03-19 13:24:36 +0000

    Refactor Docker CI

---
+++ docker/test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+tests_directory=/build/published-tests
+settings=/build/ci/ci.runsettings
+output_directory=/build/test-results
+
+tests=$(find "$tests_directory" -maxdepth 1 -type d -name "*.Tests")
+
+for test in $tests; do
+    proj=$(basename "$test")
+
+    dotnet test \
+      --blame \
+      --blame-hang-timeout 5min \
+      --settings "$settings" \
+      --logger:"GitHubActions;report-warnings=false" \
+      --logger:html \
+      --logger:trx \
+      --logger:"console;verbosity=normal" \
+      --results-directory "$output_directory/$proj" "$test/$proj.dll"
+
+    exit_code=$?
+    html_files=$(find "$output_directory/$proj" -name "*.html")
+
+    for html in $html_files; do
+      cat "$html" > "$output_directory/test-results.html"
+    done
+
+    exit $exit_code
+done
